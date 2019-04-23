14dd81e0387bf6978b1ad4d69cf2e3a8900c79f8
Igor Egorov <igor@soramitsu.co.jp>
2019-04-23 18:13:07 +0300

    Fix batch validator for use in block loader
    Batch validator has to consider "ordered" batches
    with missing transactions as valid.
    
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

---
+++ test/module/shared_model/validators/batch_validator_test.cpp
@@ -0,0 +1,8 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "validators/transaction_batch_validator.hpp"
+
+// TODO igor-egorov 23.04.2019 IR-454 Create tests for batch validator
