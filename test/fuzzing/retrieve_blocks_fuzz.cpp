7a1268e4a80aaa7a87c68ffa8317dc640321bfbf
Konstantin Munichev <toobwn@gmail.com>
2019-05-17 20:39:22 +0300

    Fix build for all fuzzing targets
    Signed-off-by: Konstantin Munichev <toobwn@gmail.com>

--- test/fuzzing/retrieve_blocks_fuzz.cpp
+++ test/fuzzing/retrieve_blocks_fuzz.cpp
@@ -14,7 +14,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, std::size_t size) {
     return 0;
   }
 
-  iroha::network::proto::BlocksRequest request;
+  iroha::network::proto::BlockRequest request;
   if (protobuf_mutator::libfuzzer::LoadProtoInput(true, data, size, &request)) {
     grpc::ServerContext context;
     iroha::MockServerWriter<iroha::protocol::Block> serverWriter;
