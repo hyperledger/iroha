e9edc1924116e8c352b73b85ef4088b63ac6a8b7
Bulat Saifullin <bulat@saifullin.ru>
2019-06-05 14:36:12 +0300

    packer memory add
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>

--- .packer/win/scripts/SetUpWinRM.ps1
+++ .packer/win/scripts/SetUpWinRM.ps1
@@ -25,7 +25,7 @@ write-host "(host) setting up WinRM"
 
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="0"}'
 cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
