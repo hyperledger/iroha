6dd789d3205c0772da5a18855c7aae3a8071cd13
Mikhail Boldyrev <miboldyrev@gmail.com>
2019-06-07 11:10:19 +0300

    added ametsuchi::TransactionExecutor
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

---
+++ irohad/ametsuchi/tx_executor.hpp
@@ -0,0 +1,38 @@
+#ifndef IROHA_AMETSUCHI_TX_EXECUTOR_HPP
+#define IROHA_AMETSUCHI_TX_EXECUTOR_HPP
+
+#include "ametsuchi/command_executor.hpp"
+#include "common/result.hpp"
+
+namespace shared_model {
+  namespace interface {
+    class Command;
+    class Transaction;
+  }  // namespace interface
+}  // namespace shared_model
+
+namespace iroha {
+  namespace ametsuchi {
+
+    struct TxExecutionError {
+      CommandError command_error;
+      size_t command_index;
+    };
+
+    class TransactionExecutor {
+      public:
+       explicit TransactionExecutor(
+           std::shared_ptr<CommandExecutor> command_executor);
+
+       iroha::expected::Result<void, TxExecutionError> execute(
+           const shared_model::interface::Transaction &transaction,
+           bool do_validation) const;
+
+      private:
+       std::shared_ptr<CommandExecutor> command_executor_;
+    };
+
+  }  // namespace ametsuchi
+}  // namespace iroha
+
+#endif  // IROHA_AMETSUCHI_TX_EXECUTOR_HPP
