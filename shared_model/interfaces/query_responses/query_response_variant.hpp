346522f415cabb0d04e692cd0f32bc2b3e9ec396
Igor Egorov <igor@soramitsu.co.jp>
2019-06-10 18:07:41 +0300

    Fix qry resp proto test
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- shared_model/interfaces/query_responses/query_response_variant.hpp
+++ shared_model/interfaces/query_responses/query_response_variant.hpp
@@ -18,11 +18,12 @@ namespace boost {
       const shared_model::interface::ErrorQueryResponse &,
       const shared_model::interface::SignatoriesResponse &,
       const shared_model::interface::TransactionsResponse &,
-      const shared_model::interface::TransactionsPageResponse &,
-      const shared_model::interface::PendingTransactionsPageResponse &,
       const shared_model::interface::AssetResponse &,
       const shared_model::interface::RolesResponse &,
-      const shared_model::interface::RolePermissionsResponse &>;
+      const shared_model::interface::RolePermissionsResponse &,
+      const shared_model::interface::TransactionsPageResponse &,
+      const shared_model::interface::PendingTransactionsPageResponse &,
+      const shared_model::interface::BlockResponse &>;
 }
 
 #endif  // IROHA_SHARED_MODEL_QUERY_RESPONSE_VARIANT_HPP
