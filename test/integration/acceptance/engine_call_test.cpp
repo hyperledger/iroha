4b0089da485927f89d431dcab190e2b50822fc9b
Andrei Lebedev <lebdron@gmail.com>
2019-08-20 18:10:07 +0300

    Create evm role, domain, and account in engine_call_test
    Signed-off-by: Andrei Lebedev <lebdron@gmail.com>
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>
    Signed-off-by: Eugene Kovalev <eugene.a.kovalev@gmail.com>
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/integration/acceptance/engine_call_test.cpp
+++ test/integration/acceptance/engine_call_test.cpp
@@ -7,51 +7,58 @@ using namespace shared_model;
 using namespace common_constants;
 
 class EngineCall : public AcceptanceFixture {
-  public:
-    // TODO(IvanTyulyandin): add permissions for engine_call
-    auto makeUserWithPerms(const interface::RolePermissionSet &perms =
-                           shared_model::interface::RolePermissionSet()) {
-      return AcceptanceFixture::makeUserWithPerms(perms);
+ public:
+  // TODO(IvanTyulyandin): add permissions for engine_call
+  auto makeUserWithPerms(const interface::RolePermissionSet &perms =
+                             shared_model::interface::RolePermissionSet()) {
+    return AcceptanceFixture::makeUserWithPerms(perms);
+  }
+
+  const crypto::Keypair kEvmKeypair =
+      crypto::DefaultCryptoAlgorithmType::generateKeypair();
+
+  /*
+
+  code is bytecode from the following Solidity code using online Remix IDE with
+  compiler version 0.4.0
+
+  pragma solidity ^0.4.0;
+
+  contract C {
+    uint256 a;
+    function setA(uint256 _a) {
+      a = _a;
     }
 
-/*
-
-code is bytecode from the following Solidity code using online Remix IDE with compiler version 0.4.0
-
-pragma solidity ^0.4.0;
-
-contract C {
-  uint256 a;
-  function setA(uint256 _a) {
-    a = _a;
+    function getA() returns(uint256) {
+      return a;
+    }
   }
 
-  function getA() returns(uint256) {
-    return a;
-  }
-}
+  */
 
-*/
+  std::string code =
+      "606060405260a18060106000396000f360606040526000357c01000000000000000"
+      "0000000000000000000000000000000000000000090048063d46300fd1460435780"
+      "63ee919d5014606857603f565b6002565b34600257605260048050506082565b604"
+      "0518082815260200191505060405180910390f35b34600257608060048080359060"
+      "200190919050506093565b005b600060006000505490506090565b90565b8060006"
+      "00050819055505b5056";
 
-    std::string code = "606060405260a18060106000396000f360606040526000357c01000000000000000"
-           "0000000000000000000000000000000000000000090048063d46300fd1460435780"
-           "63ee919d5014606857603f565b6002565b34600257605260048050506082565b604"
-           "0518082815260200191505060405180910390f35b34600257608060048080359060"
-           "200190919050506093565b005b600060006000505490506090565b90565b8060006"
-           "00050819055505b5056";
+  /*
+    calling setA(uint256), bytes4(keccak256(setA(uint256))) == ee919d50, and
+    append uint256 equal to 1 as the parameter
+  */
 
-/*
-  calling setA(uint256), bytes4(keccak256(setA(uint256))) == ee919d50, and append uint256 equal to 1 as the parameter
-*/
+  std::string inputCallSetter =
+      "ee919d50"
+      "0000000000000000000000000000000000000000000000000000000000000001";
 
-    std::string inputCallSetter = "ee919d50"
-        "0000000000000000000000000000000000000000000000000000000000000001";
+  /*
+    calling getA(), bytes4(keccak256(getA())) == d46300fd
+  */
 
-/*
-  calling getA(), bytes4(keccak256(getA())) == d46300fd
-*/
-
-    std::string inputCallGetter = "d46300fd";
+  std::string inputCallGetter = "d46300fd";
 };
 
 /**
@@ -65,15 +72,23 @@ TEST_F(EngineCall, Basic) {
       .sendTx(makeUserWithPerms())
       .skipProposal()
       .skipBlock()
+      .sendTx(complete(
+          baseTx(kAdminId)
+              .createRole("evm", interface::RolePermissionSet().setAll())
+              .createDomain("evm", "evm")
+              .createAccount("evm", "evm", kEvmKeypair.publicKey()),
+          kAdminKeypair))
+      .skipProposal()
+      .skipBlock()
       .sendTxAwait(
-          complete(baseTx().addSmartContract("caller","Callee", code, "")),
+          complete(baseTx().addSmartContract("caller", "Callee", code, "")),
           [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); })
       .sendTxAwait(
-          complete(baseTx().addSmartContract("caller","Callee", "", inputCallSetter)),
+          complete(baseTx().addSmartContract(
+              "caller", "Callee", "", inputCallSetter)),
           [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); })
       .sendTxAwait(
-          complete(baseTx().addSmartContract("caller","Callee", "", inputCallGetter)),
+          complete(baseTx().addSmartContract(
+              "caller", "Callee", "", inputCallGetter)),
           [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); });
-
 }
-
