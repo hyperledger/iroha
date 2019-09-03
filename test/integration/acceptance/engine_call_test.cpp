f86c969573f11d6750c3299f14f770dd7e3e544b
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2019-09-03 11:15:09 +0300

    Add smart contract creator storage test
    Signed-off-by: Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>
    Signed-off-by: Eugene Kovalev <eugene.a.kovalev@gmail.com>
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/integration/acceptance/engine_call_test.cpp
+++ test/integration/acceptance/engine_call_test.cpp
@@ -4,6 +4,7 @@
 
 using namespace integration_framework;
 using namespace shared_model;
+using namespace shared_model::interface::types;
 using namespace common_constants;
 
 class EngineCall : public AcceptanceFixture {
@@ -16,6 +17,9 @@ class EngineCall : public AcceptanceFixture {
 
   const crypto::Keypair kEvmKeypair =
       crypto::DefaultCryptoAlgorithmType::generateKeypair();
+
+  std::string caller = "caller";
+  std::string callee = "callee";
 
   /*
 
@@ -59,6 +63,43 @@ class EngineCall : public AcceptanceFixture {
   */
 
   std::string inputCallGetter = "d46300fd";
+
+/*
+pragma solidity^0.5.10;
+
+contract test {
+    address creator;
+
+    constructor() public {
+        creator = msg.sender;
+    }
+
+    function getCreator() public view returns (address) {
+        return creator;
+    }
+
+    function getMsgSender() public view returns (address) {
+        return msg.sender;
+    }
+}
+*/
+  std::string creatorStorageCode =  "608060405234801561001057600080fd5b50336000"
+    "806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffff"
+    "ffffffffffffffffffffffffffffffffffff16021790555061012d806100606000396000f3"
+    "fe6080604052348015600f57600080fd5b506004361060325760003560e01c80630ee2cb10"
+    "1460375780637a6ce2e114607f575b600080fd5b603d60c7565b604051808273ffffffffff"
+    "ffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff"
+    "16815260200191505060405180910390f35b608560f0565b604051808273ffffffffffffff"
+    "ffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681"
+    "5260200191505060405180910390f35b60008060009054906101000a900473ffffffffffff"
+    "ffffffffffffffffffffffffffff16905090565b60003390509056fea265627a7a72315820"
+    "336325bf5922e2c7c3f12efcc8283ba81942be490be9e05c0414d5b028b279b464736f6c63"
+    "4300050b0032";
+
+// getCreator()
+  std::string getCreator = "0ee2cb10";
+// getMsgSender()
+  std::string getMsgSender = "7a6ce2e1";
 };
 
 /**
@@ -76,19 +117,51 @@ TEST_F(EngineCall, Basic) {
           baseTx(kAdminId)
               .createRole("evm", interface::RolePermissionSet().setAll())
               .createDomain("evm", "evm")
-              .createAccount("evm", "evm", kEvmKeypair.publicKey()),
+              .createAccount("evm",
+                             "evm",
+                             PublicKeyHexStringView{kEvmKeypair.publicKey()}),
           kAdminKeypair))
       .skipProposal()
       .skipBlock()
       .sendTxAwait(
-          complete(baseTx().addSmartContract("caller", "Callee", code, "")),
+          complete(baseTx().addSmartContract(caller, callee, code, "")),
           [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); })
       .sendTxAwait(
           complete(baseTx().addSmartContract(
-              "caller", "Callee", "", inputCallSetter)),
+              caller, callee, "", inputCallSetter)),
           [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); })
       .sendTxAwait(
           complete(baseTx().addSmartContract(
-              "caller", "Callee", "", inputCallGetter)),
+              caller, callee, "", inputCallGetter)),
+          [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); });
+}
+
+
+TEST_F(EngineCall, CreatorStorageSmartContract) {
+  IntegrationTestFramework(1)
+      .setInitialState(kAdminKeypair)
+      .sendTx(makeUserWithPerms())
+      .skipProposal()
+      .skipBlock()
+      .sendTx(complete(
+          baseTx(kAdminId)
+              .createRole("evm", interface::RolePermissionSet().setAll())
+              .createDomain("evm", "evm")
+              .createAccount("evm",
+                             "evm",
+                             PublicKeyHexStringView{kEvmKeypair.publicKey()}),
+          kAdminKeypair))
+      .skipProposal()
+      .skipBlock()
+      .sendTxAwait(
+          complete(baseTx().addSmartContract(caller, callee, creatorStorageCode, "")),
+          [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); })
+      .sendTxAwait(
+          complete(baseTx().addSmartContract(
+              caller, callee, "", getCreator)),
+          [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); })
+      .sendTxAwait(
+          complete(baseTx().addSmartContract(
+              caller, callee, "", getMsgSender)),
           [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); });
 }
