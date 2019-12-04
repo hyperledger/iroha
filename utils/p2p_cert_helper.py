5000cb142acd1513a84df6acf0fa27d1923426cb
Mikhail Boldyrev <miboldyrev@gmail.com>
2019-12-04 11:09:52 +0300

    updated utils/p2p_cert_helper.py
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- utils/p2p_cert_helper.py
+++ utils/p2p_cert_helper.py
@@ -3,19 +3,21 @@
 import argparse
 import os
 import sys
+import typing
 
 from OpenSSL import crypto
 
-CMD_GEN_SAN = 'gen_san'
 CMD_GEN_REQ = 'gen_req'
+CMD_GEN_CERT = 'gen_self_signed_cert'
+CMD_SIGN_REQ = 'sign_req'
+CMD_EXTRACT_KEYS = 'extract_keys'
 
-SUBJECT_FIELDS = {
-    'country': 'JP',
-    'state': 'Japan',
-    'locality': 'Aizu',
-    'organization': 'One World',
-    'organization-unit': 'example',
-    'common-name': 'some iroha peer',
+OVERRIDEN_SUBJECT_FIELDS = {
+    'country': 'C',
+    'state': 'ST',
+    'locality': 'L',
+    'organization': 'O',
+    'organization-unit': 'OU',
 }
 
 
@@ -26,51 +28,170 @@ def get_iroha_pubkey(args: argparse.Namespace) -> str:
         assert (os.path.isfile(args.iroha_pubkey_path))
         with open(args.iroha_pubkey_path, 'rt') as inp:
             return inp.read().strip()
-    raise (Exception('No iroha public key specified!'))
+    raise Exception('No iroha public key specified!')
 
 
 def get_tls_key(args: argparse.Namespace) -> crypto.PKey:
     if args.tls_key_path is not None:
         with open(args.tls_key_path, 'rt') as inp:
             return crypto.load_privatekey(crypto.FILETYPE_PEM, inp.read())
-    raise (Exception(
-        'No TLS key specified! Please specify private TLS key path.'))
+    elif args.create_and_store_tls_key is not None:
+        key = crypto.PKey()
+        key.generate_key(type=crypto.TYPE_RSA, bits=1024)
+        with open(args.create_and_store_tls_key, 'wb') as out:
+            out.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+        return key
+    raise Exception(
+        'No TLS key specified! '
+        'Please specify either a path to existing private TLS key, '
+        'or a path to store a newly created one.')
 
 
-def make_san(args: argparse.Namespace) -> str:
-    def split_key(key):
-        split_key = list()
-        tail = key
-        while tail:
-            split_key.append(tail[:63])
-            tail = tail[63:]
-        return split_key
+def get_csr(args: argparse.Namespace) -> crypto.X509Req:
+    if args.req is not None:
+        with open(args.req, 'rt') as inp:
+            return crypto.load_certificate_request(crypto.FILETYPE_PEM,
+                                                   inp.read())
+    raise Exception('No CSR path specified!')
 
-    dns_key = 'iroha-node-public-key.' + '.'.join(
-        split_key(get_iroha_pubkey(args)))
-    return f'DNS:iroha, DNS:{dns_key}'
+
+def get_cert(args: argparse.Namespace) -> crypto.X509:
+    if args.cert is not None:
+        with open(args.cert, 'rt') as inp:
+            return crypto.load_certificate(crypto.FILETYPE_PEM, inp.read())
+    raise Exception('No certificate path specified!')
+
+
+def make_iroha_san_extension(pubkey: str) -> crypto.X509Extension:
+    return crypto.X509Extension(b'subjectAltName',
+                                critical=True,
+                                value=b'DNS:iroha' + pubkey.encode())
+
+
+def make_ed15519_pubkey_san_extension(pubkey: str) -> crypto.X509Extension:
+    return crypto.X509Extension(b'subjectAltName',
+                                critical=True,
+                                value=b'otherName:1.3.101.112;INT:0x' +
+                                pubkey.encode())
+
+
+def fill_subject_from_args(subject: crypto.X509Name, args: argparse.Namespace) -> None:
+    # fill default values
+    subject.C = 'JP'
+    subject.ST = 'Japan'
+    subject.L = 'Aizu'
+    subject.O = 'One World'
+    subject.OU = 'example'
+    subject.CN = 'iroha'
+
+    # override from with args
+    for arg, attr_name in OVERRIDEN_SUBJECT_FIELDS.items():
+        override = args.__dict__.get(arg.replace('-', '_'))
+        if override is not None:
+            setattr(subject, attr, override)
 
 
 def make_req(args: argparse.Namespace) -> str:
     req = crypto.X509Req()
-    req.get_subject().C = SUBJECT_FIELDS['country']
-    req.get_subject().ST = SUBJECT_FIELDS['state']
-    req.get_subject().L = SUBJECT_FIELDS['locality']
-    req.get_subject().O = SUBJECT_FIELDS['organization']
-    req.get_subject().OU = SUBJECT_FIELDS['organization-unit']
-    req.get_subject().CN = SUBJECT_FIELDS['common-name']
-    req.add_extensions([
-        crypto.X509Extension(b'subjectAltName',
-                             critical=True,
-                             value=make_san(args).encode())
-    ])
+    fill_subject_from_args(req.get_subject(), args)
+    req.add_extensions(
+        [make_ed15519_pubkey_san_extension(get_iroha_pubkey(args))])
     tls_key = get_tls_key(args)
     req.set_pubkey(tls_key)
     req.sign(tls_key, 'sha1')
     return crypto.dump_certificate_request(crypto.FILETYPE_PEM, req).decode()
 
 
-COMMANDS = {CMD_GEN_SAN: make_san, CMD_GEN_REQ: make_req}
+def make_cert(args: argparse.Namespace) -> str:
+    cert = crypto.X509()
+    fill_subject_from_args(cert.get_subject(), args)
+    cert.add_extensions(
+        [make_ed15519_pubkey_san_extension(get_iroha_pubkey(args))])
+    tls_key = get_tls_key(args)
+    cert.set_serial_number(1)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(365*24*60*60)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(tls_key)
+    cert.sign(tls_key, 'sha1')
+    return crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode()
+
+
+def sign_req(args: argparse.Namespace) -> str:
+    req = get_csr(args)
+    ca_cert = get_cert(args)
+    tls_key = get_tls_key(args)
+    if not ca_cert.get_pubkey().to_cryptography_key().public_numbers() \
+            == tls_key.to_cryptography_key().public_key().public_numbers():
+        raise Exception('Provided TLS key does not match the CA certificate.')
+    cert = crypto.X509()
+    cert.set_subject(req.get_subject())
+    cert.add_extensions(req.get_extensions())
+    cert.set_serial_number(1)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(365*24*60*60)
+    cert.set_issuer(ca_cert.get_subject())
+    cert.set_pubkey(req.get_pubkey())
+    cert.sign(tls_key, 'sha1')
+    return crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode()
+
+def get_keys(args: argparse.Namespace) -> typing.Generator[str, None, None]:
+    def try_load(loader):
+        try:
+            return loader(args)
+        except:
+            return None
+
+    something = try_load(get_cert) or try_load(get_csr)
+    if not something:
+        raise Exception('Please specify either a certificate or a certificate request.')
+
+    if hasattr(something, 'get_extensions'):
+        extensions = something.get_extensions()
+    elif all(hasattr(something, a) for a in ('get_extension_count', 'get_extension')):
+        extensions = (something.get_extension(i) for i in range(something.get_extension_count()))
+
+    import asn1, binascii
+
+    container_tag = asn1.Tag(0, 32, 128)
+
+    for extension in filter(lambda e: e.get_short_name() == b'subjectAltName',
+                            extensions):
+        san_seq_decoder = asn1.Decoder()
+        san_seq_decoder.start(extension.get_data())
+        tag, value = san_seq_decoder.read()
+        if tag.nr != 16: # ASN1 Sequence
+            continue
+        san_seq_decoder.start(value)
+        while san_seq_decoder.peek():
+            tag, value = san_seq_decoder.read()
+            if tag != container_tag:
+                continue
+            # descend into container
+            san_el_decoder = asn1.Decoder()
+            san_el_decoder.start(value)
+            tag, value = san_el_decoder.read()
+            if tag.nr != 6 or value != '1.3.101.112': # check ed25519 OID
+                continue
+            # descend into container
+            _, value = san_el_decoder.read()
+            san_el_decoder.start(value)
+            tag, value = san_el_decoder.read()
+            if tag.nr != 2: # ASN1 Integer
+                continue
+            yield binascii.hexlify(value.to_bytes(32, 'big')).decode()
+
+
+def get_keys_as_string(args: argparse.Namespace) -> str:
+    return '\n'.join(get_keys(args)) + '\n'
+
+
+COMMANDS = {
+    CMD_GEN_REQ: make_req,
+    CMD_GEN_CERT: make_cert,
+    CMD_SIGN_REQ: sign_req,
+    CMD_EXTRACT_KEYS: get_keys_as_string
+}
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -86,33 +207,31 @@ if __name__ == '__main__':
                         help='Path to TLS private key in PEM format.',
                         required=False)
     parser.add_argument(
+        '--create-and-store-tls-key',
+        help='Path to store a newly created TLS private key in PEM format.',
+        required=False)
+    parser.add_argument(
+        '--req',
+        help='Path to certificate request in PEM format.',
+        required=False)
+    parser.add_argument(
+        '--cert',
+        help='Path to certificate in PEM format.',
+        required=False)
+    parser.add_argument(
         '-O',
         '--output-file',
         help=
         'Output file path. If not provided, contents will be written to stdout.',
         required=False)
-    for field in SUBJECT_FIELDS:
+    for field in OVERRIDEN_SUBJECT_FIELDS:
         parser.add_argument(f'--{field}',
-                            help=f'certificate subject {field}',
+                            help=f'certificate subject {OVERRIDEN_SUBJECT_FIELDS[field]}',
                             required=False)
-    parser.add_argument('command', choices=COMMANDS)
+    parser.add_argument('command', choices=COMMANDS, help="Action.")
     args = parser.parse_args()
-
-    SUBJECT_FIELDS['common-name'] = 'iroha peer {}'.format(
-        get_iroha_pubkey(args)[:5])
 
     output_dest = args.output_file and open(args.output_file,
                                             'wt') or sys.stdout
 
-    SUBJECT_FIELDS.update({
-        field: arg
-        for field, arg in ((field, getattr(args, field.replace('-', '_')))
-                           for field in SUBJECT_FIELDS) if arg is not None
-    })
-
-    if args.command in COMMANDS:
-        output_dest.write(COMMANDS[args.command](args))
-    else:
-        print('No command specified!', file=sys.stderr)
-        print('Available commands are: ' + ','.join(COMMANDS), file=sys.stderr)
-        parser.print_help(file=sys.stderr)
+    output_dest.write(COMMANDS[args.command](args))
