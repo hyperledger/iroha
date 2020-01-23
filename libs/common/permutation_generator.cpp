52f7abc134907cb08cbfa22b399302de3e1506c4
Mikhail Boldyrev <miboldyrev@gmail.com>
2020-01-23 11:23:09 +0300

    lib permutation generator
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

---
+++ libs/common/permutation_generator.cpp
@@ -0,0 +1,73 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "common/permutation_generator.hpp"
+
+#include <algorithm>
+#include <numeric>
+#include <random>
+
+using namespace iroha;
+
+namespace {
+  Seeder::ValueType kInitialSeedValue = 0;
+}
+
+RandomEngine iroha::makeSeededPrng(const char *seed_start, size_t seed_length) {
+  return Seeder{}.feed(seed_start, seed_length).makePrng();
+}
+
+RandomEngine iroha::makeSeededPrng(const unsigned char *seed_start,
+                                   size_t seed_length) {
+  return Seeder{}
+      .feed(reinterpret_cast<const char *>(seed_start), seed_length)
+      .makePrng();
+}
+
+Seeder::Seeder() : current_seed_(kInitialSeedValue) {}
+
+RandomEngine Seeder::makePrng() const {
+  return RandomEngine{current_seed_};
+}
+
+Seeder &Seeder::feed(const char *seed_start, size_t seed_length) {
+  const ValueType *full_numbers_start =
+      reinterpret_cast<const ValueType *>(seed_start);
+  const ValueType *full_numbers_end =
+      full_numbers_start + seed_length / sizeof(ValueType);
+
+  for (; full_numbers_start < full_numbers_end; ++full_numbers_start) {
+    feed(*full_numbers_start);
+  }
+
+  const char *tail_start = reinterpret_cast<const char *>(full_numbers_end);
+  const char *seed_end = seed_start + seed_length;
+  if (tail_start < seed_end) {
+    ValueType tail = 0;
+    for (; tail_start < seed_end; ++tail_start) {
+      tail |= *tail_start;
+      tail <<= 8;
+    }
+    feed(tail);
+  }
+  return *this;
+}
+
+inline Seeder &Seeder::feed(ValueType value) {
+  // like CBC
+  current_seed_ = RandomEngine{current_seed_ ^ value}();
+  return *this;
+}
+
+void iroha::generatePermutation(std::vector<size_t> &permutation,
+                                RandomEngine prng,
+                                size_t size) {
+  permutation.resize(size);
+  std::iota(permutation.begin(), permutation.end(), 0);
+
+  for (auto it = permutation.begin(); it != permutation.end(); ++it) {
+    std::iter_swap(it, permutation.begin() + prng() % size);
+  }
+}
--- test/module/libs/common/permutation_generator_test.cpp
+++ test/module/libs/common/permutation_generator_test.cpp
@@ -3,28 +3,117 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "common/permutation_generator.hpp"
+
+#include <algorithm>
+#include <numeric>
 #include <random>
 #include <vector>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-namespace {
-  std::vector<size_t> generate_permutation(const std::string &hash_seed,
-                                           size_t size) {
-    std::vector<size_t> permutation;
-    permutation.resize(size);
-    std::iota(permutation.begin(), permutation.end(), 0);
+using namespace iroha;
 
-    std::seed_seq seed(hash_seed.begin(), hash_seed.end());
-    std::default_random_engine gen(seed);
+static const char kSeedString[] = "sector prise on the wheel";
 
-    std::shuffle(permutation.begin(), permutation.end(), gen);
-    return permutation;
+struct TestSeeder : public Seeder {
+  ValueType getCurrentSeed() {
+    return current_seed_;
   }
-}  // namespace
+};
 
-TEST(ShufflerTest, Portable) {
-  auto generated_now = generate_permutation("sector prise on the wheel", 10);
-  std::vector<size_t> generated_before{{2, 6, 1, 7, 3, 8, 9, 0, 5, 4}};
+TEST(ShufflerTest, StdSeedSeqPortable) {
+  std::vector<size_t> generated_now(10, 0);
+  std::seed_seq(kSeedString, kSeedString + sizeof(kSeedString))
+      .generate(generated_now.begin(), generated_now.end());
+
+  std::vector<size_t> generated_before{{3500580016,
+                                        2487152681,
+                                        890682050,
+                                        2703780814,
+                                        180547361,
+                                        4244110869,
+                                        995692298,
+                                        2794135049,
+                                        28909055,
+                                        3881973278}};
   EXPECT_EQ(generated_now, generated_before);
+}
+
+TEST(PermutationTest, SeederPortable) {
+  TestSeeder seeder;
+  seeder.feed(kSeedString, sizeof(kSeedString));
+
+  const Seeder::ValueType old_seed = 18321097469817268575ull;
+  EXPECT_EQ(seeder.getCurrentSeed(), old_seed);
+}
+
+TEST(PermutationTest, PrngPortable) {
+  auto prng = makeSeededPrng(kSeedString, sizeof(kSeedString));
+  EXPECT_EQ(prng(), 18411566977080829358ull);
+  EXPECT_EQ(prng(), 1542867733836066154ull);
+  EXPECT_EQ(prng(), 14957448015533108087ull);
+  EXPECT_EQ(prng(), 9726285800433590021ull);
+  EXPECT_EQ(prng(), 7000960477383894054ull);
+  EXPECT_EQ(prng(), 13774377099739194617ull);
+  EXPECT_EQ(prng(), 9855892305214809794ull);
+  EXPECT_EQ(prng(), 4375420897913288132ull);
+  EXPECT_EQ(prng(), 3961499137579268468ull);
+  EXPECT_EQ(prng(), 563129626573376221ull);
+}
+
+TEST(PermutationTest, StdShufflePortable) {
+  std::vector<size_t> generated_now(10, 0);
+  std::iota(generated_now.begin(), generated_now.end(), 0);
+
+  auto prng = makeSeededPrng(kSeedString, sizeof(kSeedString));
+  std::shuffle(generated_now.begin(), generated_now.end(), prng);
+
+  const std::vector<size_t> generated_before{{5, 3, 0, 8, 9, 7, 1, 2, 6, 4}};
+  EXPECT_EQ(generated_now, generated_before);
+}
+
+TEST(PermutationTest, PermutationPortable) {
+  std::vector<size_t> generated_now;
+  generatePermutation(
+      generated_now, makeSeededPrng(kSeedString, sizeof(kSeedString)), 10);
+  const std::vector<size_t> generated_before{{8, 9, 5, 4, 6, 2, 1, 7, 0, 3}};
+  EXPECT_EQ(generated_now, generated_before);
+}
+
+using PermutationTestParametricSeed =
+    ::testing::TestWithParam<Seeder::ValueType>;
+
+std::vector<Seeder::ValueType> generateRandomNumbers(size_t number) {
+  std::vector<Seeder::ValueType> random_numbers;
+  random_numbers.reserve(number);
+
+  std::random_device r;
+  std::default_random_engine e1(r());
+
+  std::generate_n(
+      std::back_inserter(random_numbers), number, [&] { return e1(); });
+
+  return random_numbers;
+}
+
+INSTANTIATE_TEST_SUITE_P(Instance,
+                         PermutationTestParametricSeed,
+                         ::testing::ValuesIn(generateRandomNumbers(100)));
+
+TEST_P(PermutationTestParametricSeed, PermutationValid) {
+  static const size_t kSize = 1234;
+
+  std::vector<size_t> permutation;
+  generatePermutation(permutation, Seeder{}.feed(GetParam()).makePrng(), kSize);
+
+  EXPECT_EQ(permutation.size(), kSize);
+
+  std::vector<size_t> ascending_ints;
+  ascending_ints.resize(kSize);
+  std::iota(ascending_ints.begin(), ascending_ints.end(), 0);
+  EXPECT_THAT(
+      permutation,
+      ::testing::WhenSorted(::testing::ElementsAreArray(ascending_ints)));
 }
