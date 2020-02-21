24cf0f5fd6b12d7f037ae6a5471fe778baa653c9
Mikhail Boldyrev <miboldyrev@gmail.com>
2020-02-21 10:54:34 +0300

    enabled eternal voting
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/module/irohad/consensus/yac/mock_yac_timer.hpp
+++ test/module/irohad/consensus/yac/mock_yac_timer.hpp
@@ -6,9 +6,11 @@
 #ifndef IROHA_MOCK_YAC_TIMER_HPP
 #define IROHA_MOCK_YAC_TIMER_HPP
 
-#include <gmock/gmock.h>
-
 #include "consensus/yac/timer.hpp"
+
+#include <atomic>
+
+#include <gmock/gmock.h>
 
 namespace iroha {
   namespace consensus {
@@ -17,7 +19,32 @@ namespace iroha {
       class MockTimer : public Timer {
        public:
         void invokeAfterDelay(std::function<void()> handler) override {
-          handler();
+          int32_t invoke_times{invoke_times_.load()};
+          while (invoke_times > 0
+                 and not std::atomic_compare_exchange_weak(
+                         &invoke_times_, &invoke_times, invoke_times - 1))
+            ;
+          if (invoke_times != 0) {
+            handler();
+          }
+        }
+
+        /**
+         * Toggle invoking the handler by @a invokeAfterDelay.
+         * @param invoke_is_enabled invoke eternally if true, otherwise stop
+         * invoking.
+         */
+        void setInvokeEnabled(bool invoke_is_enabled) {
+          invoke_times_.store(invoke_is_enabled ? -1 : 0);
+        }
+
+        /**
+         * Set number of times of invoking the handler by @a invokeAfterDelay.
+         * @param times handler will be invoked @a times times and then stop.
+         */
+        void setInvokeThisMoreTimes(int32_t times) {
+          assert(times > 0);
+          invoke_times_.store(times);
         }
 
         MOCK_METHOD0(deny, void());
@@ -29,6 +56,9 @@ namespace iroha {
         MockTimer &operator=(const MockTimer &rhs) {
           return *this;
         }
+
+       private:
+        std::atomic<int32_t> invoke_times_{-1};
       };
 
     }  // namespace yac
