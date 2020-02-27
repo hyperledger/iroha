9002ac9ff7bd982a86efe79656b0b1a3fe5bbc7e
Bulat Saifullin <bulat@saifullin.ru>
2020-02-27 14:22:28 +0300

    Runtime vcpkg for Mac and Windows
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>

--- .jenkinsci/builders/x64-win-build-steps.groovy
+++ .jenkinsci/builders/x64-win-build-steps.groovy
@@ -11,14 +11,24 @@
 def buildSteps(int parallelism, List compilerVersions, String buildType, boolean coverage, boolean testing, String testList,
        boolean packageBuild, boolean useBTF, List environment) {
   withEnv(environment) {
-    scmVars = checkout scm
+    def utils
+    stage('Prepare Windows environment') {
+      scmVars = checkout scm
+      utils = load ".jenkinsci/utils/utils.groovy"
+
+      win_local_vcpkg_hash = bat(script: "python .jenkinsci\\helpers\\hash.py vcpkg", returnStdout: true).trim().readLines()[-1].trim()
+      win_vcpkg_path = "C:\\vcpkg-${win_local_vcpkg_hash}"
+      win_vcpkg_toolchain_file = "${win_vcpkg_path}\\scripts\\buildsystems\\vcpkg.cmake"
+
+      utils.build_vcpkg(win_vcpkg_path,win_vcpkg_toolchain_file)
+    }
     for (compiler in compilerVersions) {
       stage ("build ${compiler}"){
-        bat '''
-cmake -H.\\ -B.\\build -DCMAKE_TOOLCHAIN_FILE=C:\\vcpkg\\scripts\\buildsystems\\vcpkg.cmake -G "Visual Studio 16 2019" -A x64 -T host=x64 &&^
+        bat """
+cmake -H.\\ -B.\\build -DCMAKE_TOOLCHAIN_FILE=${win_vcpkg_toolchain_file} -G "Visual Studio 16 2019" -A x64 -T host=x64 &&^
 cmake --build .\\build --target irohad &&^
 cmake --build .\\build --target iroha-cli
-        '''
+        """
       }
     }
   }
