7b8a95f92d51e0ef44eebd22b9e39bcc4ec3c319
武宮誠 <takemiya@soramitsu.co.jp>
2020-03-08 22:56:03 +0100

    Add default initializer to structs
    Signed-off-by: 武宮誠 <takemiya@soramitsu.co.jp>

--- src/cache/mst_cache.rs
+++ src/cache/mst_cache.rs
@@ -1,17 +1,9 @@
 use std::fmt;
 use crate::model::model;
 
+#[derive(Default)]
 pub struct MSTCache {
     waiting_mst_tx: Vec<model::Transaction>
-}
-
-impl MSTCache {
-    // constructor
-    pub fn new() -> MSTCache {
-        MSTCache {
-            waiting_mst_tx: Vec::new()
-        }
-    }
 }
 
 impl fmt::Display for MSTCache {
