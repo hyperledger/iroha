6f4a3fb5f8c45073fc5391688a83b90ab3322aa2
kamilsa <kamilsa16@gmail.com>
2020-03-10 09:58:14 +0300

    Format
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- src/networking/torii.rs
+++ src/networking/torii.rs
@@ -1,11 +1,12 @@
 use crate::cache;
-use std::net::TcpListener;
 use std::io::prelude::*;
+use std::net::TcpListener;
 
 // Start a simple TCP server
-pub fn start_torii(mut mst_cache: &cache::mst_cache::MSTCache,
-                   mut pending_tx_cache: &cache::pending_tx_cache::PendingTxCache) {
-
+pub fn start_torii(
+    mut mst_cache: &cache::mst_cache::MSTCache,
+    mut pending_tx_cache: &cache::pending_tx_cache::PendingTxCache,
+) {
     //TODO: make port configurable from config file
     let listener = TcpListener::bind("127.0.0.1:1337").expect("could not start server");
 
@@ -18,7 +19,9 @@ pub fn start_torii(mut mst_cache: &cache::mst_cache::MSTCache,
                 stream.read_to_string(&mut text).expect("read failed");
                 println!("got '{}'", text);
             }
-            Err(e) => { println!("connection failed {}", e); }
+            Err(e) => {
+                println!("connection failed {}", e);
+            }
         }
     }
 }
