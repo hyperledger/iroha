8edb0fcaffb2813d4b23ee56429709ef8cf533b9
kamilsa <kamilsa16@gmail.com>
2020-03-10 09:58:14 +0300

    Format
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- src/cache/mst_cache.rs
+++ src/cache/mst_cache.rs
@@ -1,13 +1,13 @@
-use std::fmt;
 use crate::model::model;
+use std::fmt;
 
 #[derive(Default)]
 pub struct MSTCache {
-    waiting_mst_tx: Vec<model::Transaction>
+    waiting_mst_tx: Vec<model::Transaction>,
 }
 
 impl fmt::Display for MSTCache {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.waiting_mst_tx)//TODO:
+        write!(f, "{:?}", self.waiting_mst_tx) //TODO:
     }
 }
