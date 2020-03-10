1e30e21e49e456e78da4af5a65ee41a8e0d8ee9b
kamilsa <kamilsa16@gmail.com>
2020-03-10 09:58:14 +0300

    Format
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- src/cache/pending_tx_cache.rs
+++ src/cache/pending_tx_cache.rs
@@ -1,19 +1,19 @@
-use std::fmt;
 use crate::model::model;
+use std::fmt;
 
 #[derive(Default)]
 pub struct PendingTxCache {
-    pending_tx: Vec<model::Transaction>
+    pending_tx: Vec<model::Transaction>,
 }
 
 impl PendingTxCache {
-	pub fn add_tx(&mut self, tx: model::Transaction) {
-		self.pending_tx.push(tx);
-	}
+    pub fn add_tx(&mut self, tx: model::Transaction) {
+        self.pending_tx.push(tx);
+    }
 }
 
 impl fmt::Display for PendingTxCache {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self.pending_tx)//TODO:
+        write!(f, "{:?}", self.pending_tx) //TODO:
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,34 @@
 use std::thread;
 use std::time::Duration;
 
-mod networking;
 mod cache;
 mod model;
+mod networking;
 
 fn main() {
-
-	println!("Hyperledgerいろは2にようこそ！");
+    println!("Hyperledgerいろは2にようこそ！");
 
     // Setup data structures
-	let block_time_ms = 1000; //TODO: read from config file
+    let block_time_ms = 1000; //TODO: read from config file
 
-	// move block creation to another thread
-    thread::spawn(move || {
-        loop {
-            thread::sleep(Duration::from_millis(block_time_ms));
-            println!("building block");
-        }
+    // move block creation to another thread
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_millis(block_time_ms));
+        println!("building block");
     });
 
-	// Initialize world state view (reads WSV if it already exists; don't worry, we will audit it
-	// later in the background to make sure it hasn't been tampered with)
+    // Initialize world state view (reads WSV if it already exists; don't worry, we will audit it
+    // later in the background to make sure it hasn't been tampered with)
 
-	// spawn auditor thread from block store to WSV
-	//TODO:
+    // spawn auditor thread from block store to WSV
+    //TODO:
 
-	// Set up in-memory transaction caches
-	let mst_cache = cache::mst_cache::MSTCache::default();
-	let pending_tx_cache = cache::pending_tx_cache::PendingTxCache::default();
+    // Set up in-memory transaction caches
+    let mst_cache = cache::mst_cache::MSTCache::default();
+    let pending_tx_cache = cache::pending_tx_cache::PendingTxCache::default();
 
-	println!("{}", mst_cache);
-	println!("{}", pending_tx_cache);
+    println!("{}", mst_cache);
+    println!("{}", pending_tx_cache);
 
     networking::torii::start_torii(&mst_cache, &pending_tx_cache);
 }
