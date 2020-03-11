b3550fea8936219f787d2fc41bd44d363e713349
Andrei Lebedev <lebdron@gmail.com>
2020-03-11 14:24:57 +0300

    vcpkg: update ed25519 patch, ignore whitespaces in patches (cherry picked from commit 133e27f)
    Signed-off-by: Andrei Lebedev <lebdron@gmail.com>

--- vcpkg/build_iroha_deps.sh
+++ vcpkg/build_iroha_deps.sh
@@ -6,7 +6,7 @@ iroha_vcpkg_path="${2:-$(pwd)/iroha/vcpkg}"
 
 git clone https://github.com/microsoft/vcpkg $vcpkg_path
 git -C $vcpkg_path checkout $(cat "$iroha_vcpkg_path"/VCPKG_COMMIT_SHA)
-for i in "$iroha_vcpkg_path"/patches/*.patch; do git -C $vcpkg_path apply $i; done;
+for i in "$iroha_vcpkg_path"/patches/*.patch; do git -C $vcpkg_path apply --ignore-whitespace $i; done;
 $vcpkg_path/bootstrap-vcpkg.sh
 $vcpkg_path/vcpkg install $(cat "$iroha_vcpkg_path"/VCPKG_DEPS_LIST | cut -d':' -f1 | tr '\n' ' ')
 $vcpkg_path/vcpkg install --head $(cat "$iroha_vcpkg_path"/VCPKG_HEAD_DEPS_LIST | cut -d':' -f1 | tr '\n' ' ')
