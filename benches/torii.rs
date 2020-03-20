f7f2611a30574814166c49a03c7c2c06bb703fee
Nikita Puzankov <humb1t@yandex.ru>
2020-03-20 17:21:00 +0400

    External configuration via environemnt variables.
    Signed-off-by: Nikita Puzankov <humb1t@yandex.ru>

--- benches/torii.rs
+++ benches/torii.rs
@@ -1,6 +1,7 @@
 use criterion::*;
 use iroha::{
     client::query::Query,
+    config::Configuration,
     consensus::sumeragi::Sumeragi,
     model::{block::Blockchain, tx::Transaction},
     networking::torii::Torii,
@@ -9,17 +10,21 @@ use iroha::{
 use std::io::prelude::*;
 use std::time::Duration;
 
-const TORII_URL: &str = "127.0.0.1:1337";
 const QUERY_REQUEST_HEADER: &[u8; 16] = b"GET / HTTP/1.1\r\n";
 const COMMAND_REQUEST_HEADER: &[u8; 25] = b"POST /commands HTTP/1.1\r\n";
 const _PUT: &[u8; 16] = b"PUT / HTTP/1.1\r\n";
 const OK: &[u8; 19] = b"HTTP/1.1 200 OK\r\n\r\n";
 
 fn query_requests(criterion: &mut Criterion) {
-    std::thread::spawn(|| {
-        let mut torii = Torii::new(Sumeragi::new(Blockchain::new(
-            futures::executor::block_on(Kura::strict_init()).expect("Failed to init Kura."),
-        )));
+    let config = Configuration::from_path("config.json").expect("Failed to load configuration.");
+    let torii_url = config.torii_url.to_string();
+    std::thread::spawn(move || {
+        let mut torii = Torii::new(
+            &torii_url,
+            Sumeragi::new(Blockchain::new(
+                futures::executor::block_on(Kura::strict_init()).expect("Failed to init Kura."),
+            )),
+        );
         torii.start();
     });
     std::thread::sleep(std::time::Duration::from_millis(50));
@@ -29,10 +34,11 @@ fn query_requests(criterion: &mut Criterion) {
     let mut query_request = QUERY_REQUEST_HEADER.to_vec();
     query_request.append(&mut query);
     group.throughput(Throughput::Bytes(query_request.len() as u64));
+    let torii_url = config.torii_url.to_string();
     group.bench_function("query", |b| {
         b.iter(|| {
             let mut stream =
-                std::net::TcpStream::connect(TORII_URL).expect("Failet connect to the server.");
+                std::net::TcpStream::connect(&torii_url).expect("Failed connect to the server.");
             stream
                 .set_read_timeout(Some(Duration::new(2, 0)))
                 .expect("Failed to set read timeout");
@@ -54,10 +60,15 @@ fn query_requests(criterion: &mut Criterion) {
 }
 
 fn command_requests(criterion: &mut Criterion) {
-    std::thread::spawn(|| {
-        let mut torii = Torii::new(Sumeragi::new(Blockchain::new(
-            futures::executor::block_on(Kura::strict_init()).expect("Failed to init Kura."),
-        )));
+    let config = Configuration::from_path("config.json").expect("Failed to load configuration.");
+    let torii_url = config.torii_url.to_string();
+    std::thread::spawn(move || {
+        let mut torii = Torii::new(
+            &torii_url,
+            Sumeragi::new(Blockchain::new(
+                futures::executor::block_on(Kura::strict_init()).expect("Failed to init Kura."),
+            )),
+        );
         torii.start();
     });
     std::thread::sleep(std::time::Duration::from_millis(50));
@@ -67,10 +78,11 @@ fn command_requests(criterion: &mut Criterion) {
     let mut transaction_request = COMMAND_REQUEST_HEADER.to_vec();
     transaction_request.append(&mut transaction);
     group.throughput(Throughput::Bytes(transaction_request.len() as u64));
+    let torii_url = config.torii_url.to_string();
     group.bench_function("commands", |b| {
         b.iter(|| {
             let mut stream =
-                std::net::TcpStream::connect(TORII_URL).expect("Failet connect to the server.");
+                std::net::TcpStream::connect(&torii_url).expect("Failet connect to the server.");
             stream
                 .set_read_timeout(Some(Duration::new(2, 0)))
                 .expect("Failed to set read timeout");
