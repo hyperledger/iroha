6fb2d37b4eb26aa8305e5482ce85a5fc755975e6
Bulat Saifullin <bulat@saifullin.ru>
2020-03-30 17:52:00 +0300

    update docker entrypoint.sh
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>

--- docker/release/entrypoint.sh
+++ docker/release/entrypoint.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
+set -e
 
-echo key=$KEY
-echo $PWD
-if [ -n "$IROHA_POSTGRES_HOST" ]; then
-  echo "NOTE: IROHA_POSTGRES_HOST should match 'host' option in config file"
-  PG_PORT=${IROHA_POSTGRES_PORT:-5432}
-  /wait-for-it.sh -h $IROHA_POSTGRES_HOST -p $PG_PORT -t 30 -- true
-else
-  echo "WARNING: IROHA_POSTGRES_HOST is not defined.
-    Do not wait for Postgres to become ready. Iroha may fail to start up"
+# if first arg looks like a flag, assume we want to run irohad server
+if [ "${1:0:1}" = '-' ]; then
+  set -- irohad "$@"
 fi
-irohad --genesis_block genesis.block --config config.docker --keypair_name $KEY
+
+if [ "$1" = 'irohad' ]; then
+  echo key=$KEY
+  echo $PWD
+  if [ -n "$IROHA_POSTGRES_HOST" ]; then
+    echo "NOTE: IROHA_POSTGRES_HOST should match 'host' option in config file"
+    PG_PORT=${IROHA_POSTGRES_PORT:-5432}
+    /wait-for-it.sh -h $IROHA_POSTGRES_HOST -p $PG_PORT -t 30 -- true
+  else
+    echo "WARNING: IROHA_POSTGRES_HOST is not defined.
+      Do not wait for Postgres to become ready. Iroha may fail to start up"
+  fi
+	exec "$@" --genesis_block genesis.block --config config.docker --keypair_name $KEY
+fi
+
+exec "$@"
