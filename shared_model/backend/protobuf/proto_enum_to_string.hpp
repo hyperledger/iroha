91d33362d6f39d065760302734a4421833bbd9d8
Mikhail Boldyrev <miboldyrev@gmail.com>
2020-04-22 20:55:27 -0500

    logging daemon status
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- shared_model/backend/protobuf/proto_enum_to_string.hpp
+++ shared_model/backend/protobuf/proto_enum_to_string.hpp
@@ -10,43 +10,15 @@
 
 #include <google/protobuf/generated_enum_reflection.h>
 
-#define IROHA_DEFINE_PROTO_ENUM_TO_STRING_FWD(EnumType) \
-  namespace iroha {                                     \
-    namespace to_string {                               \
-      std::string toString(const EnumType &val);        \
-    }                                                   \
-  }
-
 #define IROHA_DEFINE_PROTO_ENUM_TO_STRING(EnumType)                       \
   namespace iroha {                                                       \
     namespace to_string {                                                 \
-      std::string toString(const EnumType &val) {                         \
+      inline std::string toString(const EnumType &val) {                  \
         const ::google::protobuf::EnumDescriptor *const descriptor =      \
             ::google::protobuf::GetEnumDescriptor<EnumType>();            \
         return ::google::protobuf::internal::NameOfEnum(descriptor, val); \
       }                                                                   \
     }                                                                     \
-  }
-
-#define IROHA_DEFINE_IFACE_ENUM_TO_PROTO_STRING_FWD(IfaceEnumType, map) \
-  namespace iroha {                                                     \
-    namespace to_string {                                               \
-      std::string toString(const IfaceEnumType &val);                   \
-    }                                                                   \
-  }
-
-#define IROHA_DEFINE_IFACE_ENUM_TO_PROTO_STRING(IfaceEnumType, map) \
-  namespace iroha {                                                 \
-    namespace to_string {                                           \
-      std::string toString(const IfaceEnumType &val) {              \
-        auto it = map.find(val);                                    \
-        if (it == map.end()) {                                      \
-          assert(it != map.end());                                  \
-          return "<unknown>";                                       \
-        }                                                           \
-        return ::iroha::to_string::toString(it->second);            \
-      }                                                             \
-    }                                                               \
   }
 
 #endif
