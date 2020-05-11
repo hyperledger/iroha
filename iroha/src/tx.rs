3ca272e65e997e10d0d0fd423ecd5a0d5e54329c
Nikita Puzankov <humb1t@yandex.ru>
2020-05-11 10:52:46 +0400

    Fixed validation bug, sumeragi loop cycle synced with block_build_time_ms configuration parameter.
    Signed-off-by: Nikita Puzankov <humb1t@yandex.ru>

--- iroha/src/tx.rs
+++ iroha/src/tx.rs
@@ -110,15 +110,14 @@ impl Transaction {
         }
     }
 
-    pub fn validate(self, world_state_view: &WorldStateView) -> Result<Transaction, String> {
+    pub fn validate(self, world_state_view: &mut WorldStateView) -> Result<Transaction, String> {
         if let Transaction::Signed {
             request,
             signatures,
         } = self
         {
-            let mut world_state_view = world_state_view.clone();
             for instruction in &request.instructions {
-                instruction.invoke(&mut world_state_view)?;
+                instruction.invoke(world_state_view)?;
             }
             Ok(Transaction::Valid {
                 request,
