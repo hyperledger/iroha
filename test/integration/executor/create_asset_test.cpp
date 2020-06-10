76fea572319d2b06952836b27260bcb6c56fec83
PippityPoppity <58883461+PippityPoppity@users.noreply.github.com>
2020-06-10 07:16:11 +0530

    Added ExecutorItf create asset test
    Signed-off-by: Ashutosh Tiwari <atiwarixs@gmail.com>

---
+++ test/integration/executor/create_asset_test.cpp
@@ -0,0 +1,125 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "integration/executor/executor_fixture.hpp"
+
+#include <gtest/gtest.h>
+#include "common/result.hpp"
+#include "framework/common_constants.hpp"
+#include "framework/crypto_literals.hpp"
+#include "integration/executor/command_permission_test.hpp"
+#include "integration/executor/executor_fixture_param_provider.hpp"
+#include "module/shared_model/mock_objects_factories/mock_command_factory.hpp"
+#include "module/shared_model/mock_objects_factories/mock_query_factory.hpp"
+
+using namespace common_constants;
+using namespace executor_testing;
+using namespace framework::expected;
+using namespace shared_model::interface::types;
+
+using shared_model::interface::permissions::Grantable;
+using shared_model::interface::permissions::Role;
+
+static const AssetNameType kAssetName{"new_asset"};
+constexpr PrecisionType kAssetPrecision(1);
+
+const AssetIdType &getNewId() {
+  static const AssetIdType kNewId{kAssetName + "#" + kDomain};
+  return kAssetId;
+}
+
+class CreateAssetTest : public ExecutorTestBase {
+ public:
+ 	void checkAsset(
+      AssetIdType const &asset_id = kAssetId, 
+	  PrecisionType precision= kAssetPrecision) {
+    auto account_id_val = account_id.value_or(getNewId());
+    ASSERT_NO_FATAL_FAILURE(GetAssetInfo(account_id_val, {precision}););
+  }
+
+  void checkNoSuchAsset(
+      		AssetIdType const &asset_id = kAssetId)) {
+    auto asset_val = asset_id.value_or(getNewId());
+    checkQueryError<shared_model::interface::NoAssetErrorResponse>(
+        getItf().executeQuery(
+            *getItf().getMockQueryFactory()->constructCreateAsset(
+                asset_id)),
+        0);
+  }
+
+  iroha::ametsuchi::CommandResult createAsset(
+      const AccountIdType &issuer,
+      const AssetNameType &target_name = kAssetName,
+      const DomainIdType &target_domain = kDomain,
+      constexpr PrecisionType  &precision = kAssetPrecision,
+      bool validation_enabled = true) {
+    return getItf().executeCommandAsAccount(
+        *getItf().getMockCommandFactory()->constructCreateAsset(
+            target_name, target_domain, precision),
+        issuer,
+        validation_enabled);
+  }
+//
+  iroha::ametsuchi::CommandResult createDefaultAsset(
+      const AccountIdType &issuer, bool validation_enabled = true) {
+    return createAsset(
+        issuer, kAssetName, kDomain, kAssetPrecision, validation_enabled);
+  }
+};
+
+using CreateAssetBasicTest = BasicExecutorTest<CreateAssetTest>;
+
+/**
+ * @given a user with all related permissions
+ * @when executes CreateAsset command with nonexistent domain
+ * @then the command does not succeed and the asset is not added
+ */
+TEST_P(CreateAssetBasicTest, NoDomain) {
+  checkCommandError(checkNoSuchAsset(kAdminId, kAssetName, "no_such_domain"), 3);
+  checkNoSuchAsset(kAssetName + "@no_such_domain");
+}
+
+/**
+ * @given a user with all related permissions
+ * @when executes CreateAsset command with already taken name
+ * @then the command does not succeed and the original asset is not changed
+ */
+TEST_P(CreateAssetBasicTest, NameExists) {
+  ASSERT_NO_FATAL_FAILURE(
+      getItf().createAssetWithPerms(kAssetName, kDomain, kAssetPrecision, {}));
+  ASSERT_NO_FATAL_FAILURE(checkVerifiedProposal( 
+  			[](auto &vproposal) {
+            ASSERT_EQ(vproposal->transactions().size(), 0););
+
+  checkCommandError(createDefaultAsset(kAdminId), 4);
+  checkNoSuchAsset();
+}
+
+
+INSTANTIATE_TEST_SUITE_P(Base,
+                         CreateAssetBasicTest,
+                         executor_testing::getExecutorTestParams(),
+                         executor_testing::paramToString);
+
+using CreateAssetPermissionTest =
+    command_permission_test::CommandPermissionTest<CreateAssetTest>;
+
+TEST_P(CreateAssetPermissionTest, CommandPermissionTest) {
+  ASSERT_NO_FATAL_FAILURE(getItf().createDomain(kDomain));
+
+  if (checkResponse(createDefaultAsset(getActor(), getValidationEnabled()))) {
+	checkAsset();
+  } else {
+    checkNoSuchAsset();
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(Common,
+                         CreateAssetPermissionTest,
+                         command_permission_test::getParams(boost::none,
+                                                            boost::none,
+                                                            Domain::kCreateAsset,
+                                                            boost::none),
+                         command_permission_test::paramToString);
+
