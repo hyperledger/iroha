6d7308c013ee797bea5783ae20db7c15fdec100f
Vladislav Markushin <negigic@gmail.com>
2020-06-22 22:42:53 +0500

    Bridge: manual external transfers handling
    Signed-off-by: Vladislav Markushin <negigic@gmail.com>

--- iroha/src/bridge.rs
+++ iroha/src/bridge.rs
@@ -1,5 +1,6 @@
 //! This module contains functionality related to `Bridge`.
 
+use crate::asset::Bytes;
 use crate::prelude::*;
 use iroha_derive::Io;
 use parity_scale_codec::{Decode, Encode};
@@ -95,6 +96,16 @@ impl Identifiable for Bridge {
     type Id = BridgeId;
 }
 
+/// An entity used for storing data of any third-party transaction.
+#[cfg_attr(test, derive(PartialEq, Eq))]
+#[derive(Debug, Encode, Decode)]
+pub struct ExternalTransaction {
+    /// External transaction identifier. Not always can be calculated from the `payload`.
+    pub hash: String,
+    /// External transaction payload.
+    pub payload: Bytes,
+}
+
 #[inline]
 fn bridge_asset_definition_id() -> <AssetDefinition as Identifiable>::Id {
     AssetDefinitionId::new("bridge_asset", "bridge")
@@ -103,6 +114,18 @@ fn bridge_asset_definition_id() -> <AssetDefinition as Identifiable>::Id {
 #[inline]
 fn bridge_external_assets_asset_definition_id() -> <AssetDefinition as Identifiable>::Id {
     AssetDefinitionId::new("bridge_external_assets_asset", "bridge")
+}
+
+#[inline]
+fn bridge_incoming_external_transactions_asset_definition_id(
+) -> <AssetDefinition as Identifiable>::Id {
+    AssetDefinitionId::new("bridge_incoming_external_transactions_asset", "bridge")
+}
+
+#[inline]
+fn bridge_outgoing_external_transactions_asset_definition_id(
+) -> <AssetDefinition as Identifiable>::Id {
+    AssetDefinitionId::new("bridge_outgoing_external_transactions_asset", "bridge")
 }
 
 /// This module provides structures for working with external assets.
@@ -124,7 +147,7 @@ pub mod asset {
         /// External asset's name in the Iroha.
         pub name: String,
         /// External asset ID.
-        pub external_id: Id,
+        pub id: Id,
         /// The number of digits that come after the decimal place.
         /// Used in a value representation.
         pub decimals: u8,
@@ -141,7 +164,6 @@ pub mod isi {
     use super::*;
     use crate::account::query::*;
     use crate::bridge::asset::*;
-    use crate::isi::prelude::*;
 
     /// Constructor of Iroha Special Instruction for bridge registration.
     pub fn register_bridge(
@@ -178,6 +200,7 @@ pub mod isi {
                     },
                 }
                 .into(),
+                // TODO: add incoming transfer event listener
             ])),
             Some(Box::new(Instruction::Fail(
                 "Account not found.".to_string(),
@@ -188,9 +211,9 @@ pub mod isi {
     /// Constructor of Iroha Special Instruction for external asset registration.
     pub fn register_external_asset(external_asset: &ExternalAsset) -> Instruction {
         let domain_id = &external_asset.bridge_id.definition_id.name;
-        let account = Account::new(BRIDGE_ACCOUNT_NAME, domain_id);
+        let account_id = AccountId::new(BRIDGE_ACCOUNT_NAME, domain_id);
         let asset_definition = AssetDefinition::new(AssetDefinitionId::new(
-            &external_asset.name,
+            &external_asset.id,
             &external_asset.bridge_id.definition_id.name,
         ));
         Instruction::Sequence(vec![
@@ -200,10 +223,10 @@ pub mod isi {
             }
             .into(),
             Mint {
-                object: (external_asset.name.clone(), external_asset.encode()),
+                object: (external_asset.id.clone(), external_asset.encode()),
                 destination_id: AssetId {
                     definition_id: bridge_external_assets_asset_definition_id(),
-                    account_id: account.id,
+                    account_id,
                 },
             }
             .into(),
@@ -216,12 +239,71 @@ pub mod isi {
         client_public_key: PublicKey,
     ) -> Instruction {
         let domain_id = &bridge_definition_id.name;
-        let account = Account::new(BRIDGE_ACCOUNT_NAME, domain_id);
+        let account_id = AccountId::new(BRIDGE_ACCOUNT_NAME, domain_id);
         Add {
             object: client_public_key,
-            destination_id: account.id,
+            destination_id: account_id,
         }
         .into()
+    }
+
+    /// Constructor of Iroha Special Instruction for registering incoming transfer and minting
+    /// the external asset to the recipient.
+    pub fn handle_incoming_transfer(
+        bridge_definition_id: &<BridgeDefinition as Identifiable>::Id,
+        external_asset_id: &<ExternalAsset as Identifiable>::Id,
+        quantity: u32,
+        big_quantity: u128,
+        recipient: <Account as Identifiable>::Id,
+        transaction: &ExternalTransaction,
+    ) -> Instruction {
+        let domain_id = &bridge_definition_id.name;
+        let account_id = AccountId::new(BRIDGE_ACCOUNT_NAME, domain_id);
+        let asset_id = AssetId {
+            definition_id: AssetDefinitionId::new(&external_asset_id, &bridge_definition_id.name),
+            account_id: recipient,
+        };
+        Instruction::Sequence(vec![
+            Mint::new(quantity, asset_id.clone()).into(),
+            Mint::new(big_quantity, asset_id).into(),
+            Mint::new(
+                (transaction.hash.clone(), transaction.encode()),
+                AssetId {
+                    definition_id: bridge_incoming_external_transactions_asset_definition_id(),
+                    account_id,
+                },
+            )
+            .into(),
+        ])
+    }
+
+    /// Constructor of Iroha Special Instruction for registering outgoing transfer and deminting
+    /// received asset.
+    pub fn handle_outgoing_transfer(
+        bridge_definition_id: &<BridgeDefinition as Identifiable>::Id,
+        external_asset_id: &<ExternalAsset as Identifiable>::Id,
+        quantity: u32,
+        big_quantity: u128,
+        transaction: &ExternalTransaction,
+    ) -> Instruction {
+        let domain_id = &bridge_definition_id.name;
+        let account_id = AccountId::new(BRIDGE_ACCOUNT_NAME, domain_id);
+        let asset_id = AssetId {
+            definition_id: AssetDefinitionId::new(&external_asset_id, &bridge_definition_id.name),
+            account_id: account_id.clone(),
+        };
+        Instruction::Sequence(vec![
+            Demint::new(quantity, asset_id.clone()).into(),
+            Demint::new(big_quantity, asset_id).into(),
+            Mint::new(
+                (transaction.hash.clone(), transaction.encode()),
+                AssetId {
+                    definition_id: bridge_outgoing_external_transactions_asset_definition_id(),
+                    account_id,
+                },
+            )
+            .into(),
+        ])
     }
 
     #[cfg(test)]
@@ -270,17 +352,18 @@ pub mod isi {
                 };
                 let bridge_domain_name = "bridge".to_string();
                 let mut bridge_asset_definitions = HashMap::new();
-                let bridge_asset_definition_id = bridge_asset_definition_id();
-                bridge_asset_definitions.insert(
-                    bridge_asset_definition_id.clone(),
-                    AssetDefinition::new(bridge_asset_definition_id.clone()),
-                );
-                let bridge_external_assets_asset_definition_id =
-                    bridge_external_assets_asset_definition_id();
-                bridge_asset_definitions.insert(
-                    bridge_external_assets_asset_definition_id.clone(),
-                    AssetDefinition::new(bridge_external_assets_asset_definition_id.clone()),
-                );
+                let asset_definition_ids = [
+                    bridge_asset_definition_id(),
+                    bridge_external_assets_asset_definition_id(),
+                    bridge_incoming_external_transactions_asset_definition_id(),
+                    bridge_outgoing_external_transactions_asset_definition_id(),
+                ];
+                for asset_definition_id in &asset_definition_ids {
+                    bridge_asset_definitions.insert(
+                        asset_definition_id.clone(),
+                        AssetDefinition::new(asset_definition_id.clone()),
+                    );
+                }
                 let bridge_domain = Domain {
                     name: bridge_domain_name.clone(),
                     accounts: HashMap::new(),
@@ -387,7 +470,7 @@ pub mod isi {
             let external_asset = ExternalAsset {
                 bridge_id: BridgeId::new(&bridge_definition.id.name),
                 name: "DOT Token".to_string(),
-                external_id: "DOT".to_string(),
+                id: "DOT".to_string(),
                 decimals: 12,
             };
             register_external_asset(&external_asset)
@@ -397,7 +480,7 @@ pub mod isi {
             let query_result = bridge_query
                 .execute(&world_state_view)
                 .expect("failed to query a bridge");
-            let decoded_external_asset = decode_external_asset(&query_result, &external_asset.name)
+            let decoded_external_asset = decode_external_asset(&query_result, &external_asset.id)
                 .expect("failed to decode an external asset");
             assert_eq!(decoded_external_asset, external_asset);
         }
@@ -433,6 +516,115 @@ pub mod isi {
             let clients = get_clients(&query_result).expect("failed to get bridge clients");
             assert_eq!(clients, &[bridge_owner_public_key]);
         }
+
+        #[test]
+        fn test_external_transfer_should_pass() {
+            let mut testkit = TestKit::new();
+            let bridge_owner_public_key = KeyPair::generate()
+                .expect("Failed to generate KeyPair.")
+                .public_key;
+            let bridge_owner_account =
+                Account::with_signatory("bridge_owner", "Company", bridge_owner_public_key.clone());
+            let recipient_public_key = KeyPair::generate()
+                .expect("Failed to generate KeyPair.")
+                .public_key;
+            let recipient_account =
+                Account::with_signatory("recepient", "Company", recipient_public_key.clone());
+            let bridge_definition = BridgeDefinition {
+                id: BridgeDefinitionId::new(BRIDGE_NAME),
+                kind: BridgeKind::IClaim,
+                owner_account_id: bridge_owner_account.id.clone(),
+            };
+            let world_state_view = &mut testkit.world_state_view;
+            let accounts = [&bridge_owner_account, &recipient_account];
+            for account in &accounts {
+                world_state_view
+                    .peer()
+                    .domains
+                    .get_mut("Company")
+                    .unwrap()
+                    .register_account((*account).clone())
+                    .execute(testkit.root_account_id.clone(), world_state_view)
+                    .expect("failed to register bridge owner account");
+            }
+            register_bridge(world_state_view.read_peer().id.clone(), &bridge_definition)
+                .execute(testkit.root_account_id.clone(), world_state_view)
+                .expect("failed to register bridge");
+            let external_asset = ExternalAsset {
+                bridge_id: BridgeId::new(&bridge_definition.id.name),
+                name: "DOT Token".to_string(),
+                id: "DOT".to_string(),
+                decimals: 12,
+            };
+            register_external_asset(&external_asset)
+                .execute(testkit.root_account_id.clone(), world_state_view)
+                .expect("failed to register external asset");
+            let external_incoming_transaction = ExternalTransaction {
+                hash: "0x9e58e3c750a53475f8613f100c1ccfd81083fb5c8cc8d5ed149b8e877bd1123f".into(),
+                payload: b"77bd1123fd5ed1750a53475f8619e58e3ccfd81083fb5c8cc849b8e83f100c1c"
+                    .to_vec(),
+            };
+            handle_incoming_transfer(
+                &bridge_definition.id,
+                &external_asset.id,
+                10,
+                20,
+                recipient_account.id.clone(),
+                &external_incoming_transaction,
+            )
+            .execute(testkit.root_account_id.clone(), world_state_view)
+            .expect("failed to handle incoming transaction");
+            let asset_definition_id =
+                AssetDefinitionId::new(&external_asset.id, &bridge_definition.id.name);
+            let asset = world_state_view
+                .read_asset(&AssetId {
+                    definition_id: asset_definition_id.clone(),
+                    account_id: recipient_account.id.clone(),
+                })
+                .expect("failed to read asset")
+                .clone();
+            assert_eq!(asset.quantity, 10);
+            assert_eq!(asset.big_quantity, 20);
+            let bridge_account_id = AccountId::new(BRIDGE_ACCOUNT_NAME, BRIDGE_NAME);
+            recipient_account
+                .transfer_asset_to(asset.clone(), bridge_account_id.clone())
+                .execute(testkit.root_account_id.clone(), world_state_view)
+                .expect("failed to transfer asset to the bridge");
+            let external_outgoing_transaction = ExternalTransaction {
+                hash: "0xb8e877bd1123fd5ed1c849f8613f100c1c750a534759e58e3ccfd81083fb5c8c".into(),
+                payload: b"3475fa5cc849b8e83f100c1c8619e58e3ccfd81083fb5c877bd1123fd5ed1750"
+                    .to_vec(),
+            };
+            handle_outgoing_transfer(
+                &bridge_definition.id,
+                &external_asset.id,
+                asset.quantity,
+                asset.big_quantity,
+                &external_outgoing_transaction,
+            )
+            .execute(testkit.root_account_id.clone(), world_state_view)
+            .expect("failed to handle outgoing transaction");
+            let asset = world_state_view
+                .read_asset(&AssetId {
+                    definition_id: asset_definition_id,
+                    account_id: bridge_account_id,
+                })
+                .expect("failed to read asset")
+                .clone();
+            assert_eq!(asset.quantity, 0);
+            assert_eq!(asset.big_quantity, 0);
+            let query_result = query_bridge(BridgeId::new(&bridge_definition.id.name))
+                .execute(&world_state_view)
+                .expect("failed to query a bridge");
+            let transactions: Vec<_> = decode_incoming_external_transactions(&query_result)
+                .expect("failed to decode incoming transactions")
+                .collect();
+            assert_eq!(transactions, &[external_incoming_transaction]);
+            let transactions: Vec<_> = decode_outgoing_external_transactions(&query_result)
+                .expect("failed to decode outgoing transactions")
+                .collect();
+            assert_eq!(transactions, &[external_outgoing_transaction]);
+        }
     }
 }
 
@@ -466,15 +658,13 @@ pub mod query {
             .account
             .assets
             .iter()
-            .filter(|(id, _)| id.definition_id == bridge_asset_definition_id())
-            .filter_map(|(_, asset)| {
+            .find(|(id, _)| id.definition_id == bridge_asset_definition_id())
+            .and_then(|(_, asset)| {
                 asset
                     .store
                     .get(BRIDGE_ASSET_BRIDGE_DEFINITION_PARAMETER_KEY)
-                    .cloned()
+                    .and_then(|data| BridgeDefinition::decode(&mut data.as_slice()).ok())
             })
-            .filter_map(|data| BridgeDefinition::decode(&mut data.as_slice()).ok())
-            .next()
     }
 
     /// A helper function for decoding information about external asset from the query result.
@@ -495,10 +685,14 @@ pub mod query {
             .account
             .assets
             .iter()
-            .filter(|(id, _)| id.definition_id == bridge_external_assets_asset_definition_id())
-            .filter_map(|(_, asset)| asset.store.get(asset_name).cloned())
-            .filter_map(|data| ExternalAsset::decode(&mut data.as_slice()).ok())
-            .next()
+            .find(|(id, _)| id.definition_id == bridge_external_assets_asset_definition_id())
+            .and_then(|(_, asset)| {
+                asset
+                    .store
+                    .get(asset_name)
+                    .cloned()
+                    .and_then(|data| ExternalAsset::decode(&mut data.as_slice()).ok())
+            })
     }
 
     /// A helper function for retrieving information about bridge clients.
@@ -508,5 +702,59 @@ pub mod query {
             _ => return None,
         };
         Some(&account_result.account.read_signatories())
+    }
+
+    fn decode_external_transactions<'a>(
+        query_result: &'a QueryResult,
+        is_incoming: bool,
+    ) -> Option<impl Iterator<Item = ExternalTransaction> + 'a> {
+        let account_assets_result = match query_result {
+            QueryResult::GetAccount(v) => v,
+            _ => return None,
+        };
+        account_assets_result
+            .account
+            .assets
+            .iter()
+            .find(|(id, _)| {
+                let asset_definition_id = if is_incoming {
+                    bridge_incoming_external_transactions_asset_definition_id()
+                } else {
+                    bridge_outgoing_external_transactions_asset_definition_id()
+                };
+                id.definition_id == asset_definition_id
+            })
+            .map(|(_, asset)| {
+                asset
+                    .store
+                    .values()
+                    .filter_map(|data| ExternalTransaction::decode(&mut data.as_slice()).ok())
+            })
+    }
+
+    /// A helper function for decoding information about incoming external transactions
+    /// from the query result.
+    ///
+    /// Each `ExternalTransaction` is encoded and stored in the bridge external assets asset
+    /// (`bridge_incoming_external_transactions_asset_definition_id`) store and indexed by a
+    /// transaction hash. The given query result may not contain the above values, so this
+    /// function can fail, returning `None`.
+    pub fn decode_incoming_external_transactions<'a>(
+        query_result: &'a QueryResult,
+    ) -> Option<impl Iterator<Item = ExternalTransaction> + 'a> {
+        decode_external_transactions(query_result, true)
+    }
+
+    /// A helper function for decoding information about outgoing external transactions
+    /// from the query result.
+    ///
+    /// Each `ExternalTransaction` is encoded and stored in the bridge external assets asset
+    /// (`bridge_outgoing_external_transactions_asset_definition_id`) store and indexed by a
+    /// transaction hash. The given query result may not contain the above values, so this
+    /// function can fail, returning `None`.
+    pub fn decode_outgoing_external_transactions<'a>(
+        query_result: &'a QueryResult,
+    ) -> Option<impl Iterator<Item = ExternalTransaction> + 'a> {
+        decode_external_transactions(query_result, false)
     }
 }
