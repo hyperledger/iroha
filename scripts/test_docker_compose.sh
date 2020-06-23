e792e3b5fcadfdaa2c50ea3b1ecca7fd9df15735
Egor Ivkov <e.o.ivkov@gmail.com>
2020-06-23 11:43:25 +0300

    Docker compose test in pipeline
    Signed-off-by: Egor Ivkov <e.o.ivkov@gmail.com>

---
+++ scripts/test_docker_compose.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd test_docker
+./iroha_client_cli domain add --name="Soramitsu"
+sleep 2
+./iroha_client_cli account register --name="Alice" --domain="Soramitsu" --key="[101, 170, 80, 164, 103, 38, 73, 61, 223, 133, 83, 139, 247, 77, 176, 84, 117, 15, 22, 28, 155, 125, 80, 226, 40, 26, 61, 248, 40, 159, 58, 53]"
+sleep 2
+./iroha_client_cli asset register --name="XOR" --domain="Soramitsu"
+sleep 2
+./iroha_client_cli asset mint --account_id="Alice@Soramitsu" --id="XOR#Soramitsu" --quantity="100"
+sleep 2
+./iroha_client_cli asset get --account_id="Alice@Soramitsu" --id="XOR#Soramitsu" | grep -q 'quantity: 100'
