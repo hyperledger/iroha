9eb31cc9fc71650c53be22c69af3985500113c2c
Egor Ivkov <e.o.ivkov@gmail.com>
2020-07-06 16:11:16 +0300

    Synchronize blocks in batches
    Signed-off-by: Egor Ivkov <e.o.ivkov@gmail.com>

--- iroha/src/block_sync.rs
+++ iroha/src/block_sync.rs
@@ -1,6 +1,6 @@
 //! This module contains structures and messages for synchronization of blocks between peers.
 
-use self::message::*;
+use self::{config::BlockSyncConfiguration, message::*};
 use crate::{
     block::ValidBlock,
     kura::Kura,
@@ -16,7 +16,8 @@ enum State {
     /// Not synchronizing now.
     Idle,
     /// Synchronization is in progress: validating and committing blocks.
-    InProgress(Vec<ValidBlock>),
+    /// Contains a vector of blocks left to commit and an id of the peer from which the blocks were requested.
+    InProgress(Vec<ValidBlock>, PeerId),
 }
 
 /// Structure responsible for block synchronization between peers.
@@ -26,22 +27,24 @@ pub struct BlockSynchronizer {
     peer_id: PeerId,
     state: State,
     gossip_period: Duration,
+    batch_size: u64,
 }
 
 impl BlockSynchronizer {
     /// Constructs `BlockSync`
-    pub fn new(
+    pub fn from_configuration(
+        config: &BlockSyncConfiguration,
         kura: Arc<RwLock<Kura>>,
         sumeragi: Arc<RwLock<Sumeragi>>,
         peer_id: PeerId,
-        gossip_period: Duration,
     ) -> BlockSynchronizer {
         Self {
             kura,
             peer_id,
             sumeragi,
             state: State::Idle,
-            gossip_period,
+            gossip_period: Duration::from_millis(config.gossip_period_ms),
+            batch_size: config.batch_size,
         }
     }
 
@@ -57,7 +60,7 @@ impl BlockSynchronizer {
             loop {
                 task::sleep(gossip_period).await;
                 let message =
-                    Message::LatestBlock((kura.read().await.latest_block_hash(), peer_id.clone()));
+                    Message::LatestBlock(kura.read().await.latest_block_hash(), peer_id.clone());
                 futures::future::join_all(
                     sumeragi
                         .read()
@@ -74,7 +77,7 @@ impl BlockSynchronizer {
 
     /// Continues the synchronization if it was ongoing. Should be called after `WSV` update.
     pub async fn continue_sync(&mut self) {
-        if let State::InProgress(blocks) = self.state.clone() {
+        if let State::InProgress(blocks, peer_id) = self.state.clone() {
             if let Some((block, blocks)) = blocks.split_first() {
                 let mut network_topology = self.sumeragi.read().await.network_topology.clone();
                 network_topology.shift_peers_by_n(block.header.number_of_view_changes);
@@ -87,7 +90,7 @@ impl BlockSynchronizer {
                         .len()
                         >= network_topology.min_votes_for_commit()
                 {
-                    self.state = State::InProgress(blocks.to_vec());
+                    self.state = State::InProgress(blocks.to_vec(), peer_id);
                     self.sumeragi
                         .write()
                         .await
@@ -98,6 +101,15 @@ impl BlockSynchronizer {
                 }
             } else {
                 self.state = State::Idle;
+                if let Err(e) = Message::LatestBlock(
+                    self.kura.read().await.latest_block_hash(),
+                    self.peer_id.clone(),
+                )
+                .send_to(&peer_id)
+                .await
+                {
+                    eprintln!("Failed to request next batch of blocks. {}", e)
+                }
             }
         }
     }
@@ -115,22 +127,22 @@ pub mod message {
     #[derive(Io, Decode, Encode, Debug, Clone)]
     pub enum Message {
         /// Gossip about latest block.
-        LatestBlock((Hash, PeerId)),
+        LatestBlock(Hash, PeerId),
         /// Request for blocks after the block with `Hash` for the peer with `PeerId`.
-        GetBlocksAfter((Hash, PeerId)),
-        /// The response to `GetBlocksAfter`. Contains the requested blocks.
-        ShareBlocks(Vec<ValidBlock>),
+        GetBlocksAfter(Hash, PeerId),
+        /// The response to `GetBlocksAfter`. Contains the requested blocks and the id of the peer who shared them.
+        ShareBlocks(Vec<ValidBlock>, PeerId),
     }
 
     impl Message {
         /// Handles the incoming message.
         pub async fn handle(&self, block_sync: &mut BlockSynchronizer) {
             match self {
-                Message::LatestBlock((hash, peer)) => {
+                Message::LatestBlock(hash, peer) => {
                     let latest_block_hash = block_sync.kura.read().await.latest_block_hash();
                     if *hash != latest_block_hash {
                         if let Err(err) =
-                            Message::GetBlocksAfter((latest_block_hash, block_sync.peer_id.clone()))
+                            Message::GetBlocksAfter(latest_block_hash, block_sync.peer_id.clone())
                                 .send_to(peer)
                                 .await
                         {
@@ -138,18 +150,30 @@ pub mod message {
                         }
                     }
                 }
-                Message::GetBlocksAfter((hash, peer)) => {
-                    //TODO: send batches of configurable size instead of all blocks
-                    if let Some(blocks) = block_sync.kura.read().await.blocks_after(*hash) {
-                        if let Err(err) = Message::ShareBlocks(blocks.to_vec()).send_to(peer).await
-                        {
-                            eprintln!("Failed to send blocks: {:?}", err)
+                Message::GetBlocksAfter(hash, peer) => {
+                    if block_sync.batch_size > 0 {
+                        if let Some(blocks) = block_sync.kura.read().await.blocks_after(*hash) {
+                            if let Some(blocks_batch) =
+                                blocks.chunks(block_sync.batch_size as usize).next()
+                            {
+                                if let Err(err) = Message::ShareBlocks(
+                                    blocks_batch.to_vec(),
+                                    block_sync.peer_id.clone(),
+                                )
+                                .send_to(peer)
+                                .await
+                                {
+                                    eprintln!("Failed to send blocks: {:?}", err)
+                                }
+                            }
                         }
+                    } else {
+                        eprintln!("Error: not sending any blocks as batch_size is equal to zero.")
                     }
                 }
-                Message::ShareBlocks(blocks) => {
+                Message::ShareBlocks(blocks, peer_id) => {
                     if let State::Idle = block_sync.state.clone() {
-                        block_sync.state = State::InProgress(blocks.clone());
+                        block_sync.state = State::InProgress(blocks.clone(), peer_id.clone());
                         block_sync.continue_sync().await;
                     }
                 }
@@ -172,5 +196,55 @@ pub mod message {
                 )),
             }
         }
+    }
+}
+
+/// This module contains all configuration related logic.
+pub mod config {
+    use iroha_derive::*;
+    use serde::Deserialize;
+    use std::env;
+
+    const BATCH_SIZE: &str = "BLOCK_SYNC_BATCH_SIZE";
+    const GOSSIP_PERIOD_MS: &str = "BLOCK_SYNC_GOSSIP_PERIOD_MS";
+    const DEFAULT_BATCH_SIZE: u64 = 4;
+    const DEFAULT_GOSSIP_PERIOD_MS: u64 = 10000;
+
+    /// Configuration for `BlockSynchronizer`.
+    #[derive(Clone, Deserialize, Debug)]
+    #[serde(rename_all = "UPPERCASE")]
+    pub struct BlockSyncConfiguration {
+        /// The time between peer sharing its latest block hash with other peers in milliseconds.
+        #[serde(default = "default_gossip_period")]
+        pub gossip_period_ms: u64,
+        /// The number of blocks, which can be send in one message.
+        /// Underlying network (`iroha_network`) should support transferring messages this large.
+        #[serde(default = "default_batch_size")]
+        pub batch_size: u64,
+    }
+
+    impl BlockSyncConfiguration {
+        /// Load environment variables and replace predefined parameters with these variables
+        /// values.
+        #[log]
+        pub fn load_environment(&mut self) -> Result<(), String> {
+            if let Ok(batch_size) = env::var(BATCH_SIZE) {
+                self.batch_size = serde_json::from_str(&batch_size)
+                    .map_err(|e| format!("Failed to parse batch size: {}", e))?;
+            }
+            if let Ok(gossip_period_ms) = env::var(GOSSIP_PERIOD_MS) {
+                self.gossip_period_ms = serde_json::from_str(&gossip_period_ms)
+                    .map_err(|e| format!("Failed to parse gossip period: {}", e))?;
+            }
+            Ok(())
+        }
+    }
+
+    fn default_batch_size() -> u64 {
+        DEFAULT_BATCH_SIZE
+    }
+
+    fn default_gossip_period() -> u64 {
+        DEFAULT_GOSSIP_PERIOD_MS
     }
 }
