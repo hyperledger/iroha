f9892ec7d9c2ac3f0e9bcddbe895bc345aa0fb67
utsav <utsavjn48@gmail.com>
2020-07-08 21:30:35 +0530

    Client Iface and proto class: CallModel command
    Signed-off-by: utsav <utsavjn48@gmail.com>
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- irohad/ametsuchi/impl/postgres_command_executor.cpp
+++ irohad/ametsuchi/impl/postgres_command_executor.cpp
@@ -26,6 +26,7 @@
 #include "interfaces/commands/add_signatory.hpp"
 #include "interfaces/commands/append_role.hpp"
 #include "interfaces/commands/call_engine.hpp"
+#include "interfaces/commands/call_model.hpp"
 #include "interfaces/commands/command.hpp"
 #include "interfaces/commands/compare_and_set_account_detail.hpp"
 #include "interfaces/commands/create_account.hpp"
@@ -1897,6 +1898,15 @@ namespace iroha {
       executor.use("precision", precision);
 
       return executor.execute();
+    }
+
+    CommandResult PostgresCommandExecutor::operator()(
+        const shared_model::interface::CallModel &command,
+        const shared_model::interface::types::AccountIdType &creator_account_id,
+        const std::string &tx_hash,
+        shared_model::interface::types::CommandIndexType,
+        bool do_validation) {
+      return {};
     }
 
     CommandResult PostgresCommandExecutor::operator()(
