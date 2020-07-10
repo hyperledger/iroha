5367f9675b186ae96ba3dbd1929e5480c85a80d9
Mikhail Boldyrev <miboldyrev@gmail.com>
2020-07-10 14:38:29 -0500

    added shared_model::interface::DataModelId
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

---
+++ shared_model/interfaces/common_objects/impl/data_model_id.cpp
@@ -0,0 +1,20 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "interfaces/common_objects/data_model_id.hpp"
+
+#include <ciso646>
+#include <functional>
+
+using namespace shared_model::interface;
+
+bool DataModelId::operator==(DataModelId const &rhs) const {
+  return name == rhs.name and version == rhs.version;
+}
+
+std::size_t DataModelId::Hasher::operator()(DataModelId const &id) const {
+  std::hash<std::string> hasher;
+  return hasher(id.name) + hasher(id.version);
+}
