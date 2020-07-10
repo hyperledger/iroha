4768e953680496445c3ac80e0fe3037f6257de8b
Nikita Puzankov <humb1t@yandex.ru>
2020-07-10 11:03:36 +0400

    IR-802 Subscription to blocks statuses changes.
    Signed-off-by: Nikita Puzankov <humb1t@yandex.ru>

--- iroha_client/tests/subscribe_to_changes.rs
+++ iroha_client/tests/subscribe_to_changes.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use async_std::{prelude::*, task};
-    use iroha::{config::Configuration, isi, prelude::*};
+    use iroha::{config::Configuration, event::*, isi, prelude::*};
     use iroha_client::{client::Client, config::Configuration as ClientConfiguration};
     use std::{thread, time::Duration};
     use tempfile::TempDir;
@@ -9,12 +9,26 @@ mod tests {
     const CONFIGURATION_PATH: &str = "tests/test_config.json";
 
     #[async_std::test]
-    #[ignore]
-    async fn client_subscribe_to_changes_request_should_receive_changes() {
-        thread::spawn(create_and_start_iroha);
+    async fn client_subscribe_to_block_changes_request_should_receive_block_change() {
+        thread::spawn(|| {
+            let temp_dir = TempDir::new().expect("Failed to create TempDir.");
+            let mut configuration = Configuration::from_path(CONFIGURATION_PATH)
+                .expect("Failed to load configuration.");
+            configuration.torii_configuration.torii_url = "127.0.0.1:1337".to_string();
+            configuration.torii_configuration.torii_connect_url = "127.0.0.1:8889".to_string();
+            configuration
+                .kura_configuration
+                .kura_block_store_path(temp_dir.path());
+            let iroha = Iroha::new(configuration.clone());
+            task::block_on(iroha.start()).expect("Failed to start Iroha.");
+            //Prevents temp_dir from clean up untill the end of the tests.
+            #[allow(clippy::empty_loop)]
+            loop {}
+        });
         thread::sleep(Duration::from_millis(300));
-        let configuration =
+        let mut configuration =
             Configuration::from_path(CONFIGURATION_PATH).expect("Failed to load configuration.");
+        configuration.torii_configuration.torii_connect_url = "127.0.0.1:8889".to_string();
         let mut iroha_client = Client::with_maintenance(
             &ClientConfiguration::from_iroha_configuration(&configuration),
         );
@@ -22,22 +36,6 @@ mod tests {
             .subscribe_to_block_changes()
             .await
             .expect("Failed to execute request.");
-        if let Some(change) = stream.next().await {
-            println!("Change received {:?}", change);
-        } else {
-            panic!("Failed to receive change.");
-        }
-    }
-
-    fn create_and_start_iroha() {
-        let temp_dir = TempDir::new().expect("Failed to create TempDir.");
-        let mut configuration =
-            Configuration::from_path(CONFIGURATION_PATH).expect("Failed to load configuration.");
-        configuration
-            .kura_configuration
-            .kura_block_store_path(temp_dir.path());
-        let iroha = Iroha::new(configuration.clone());
-        task::block_on(iroha.start()).expect("Failed to start Iroha.");
         let domain_name = "global";
         let asset_definition_id = AssetDefinitionId::new("xor", domain_name);
         let create_asset = isi::Register {
@@ -48,8 +46,76 @@ mod tests {
             &configuration,
         ));
         task::block_on(iroha_client.submit(create_asset.into())).expect("Failed to prepare state.");
-        //Prevents temp_dir from clean up untill the end of the tests.
-        #[allow(clippy::empty_loop)]
-        loop {}
+        while let Some(change) = stream.next().await {
+            println!("Change received {:?}", change);
+            match change {
+                Occurrence::Created(entity)
+                | Occurrence::Updated(entity)
+                | Occurrence::Deleted(entity) => match entity {
+                    Entity::Block(_) => {
+                        println!("Entity changed: {:?}", entity);
+                        return ();
+                    }
+                    _ => println!("Received not expected change: {:?}", entity),
+                },
+            }
+        }
+        panic!("Failed to receive change.");
+    }
+
+    #[async_std::test]
+    async fn client_subscribe_to_transaction_changes_request_should_receive_transaction_change() {
+        thread::spawn(|| {
+            let temp_dir = TempDir::new().expect("Failed to create TempDir.");
+            let mut configuration = Configuration::from_path(CONFIGURATION_PATH)
+                .expect("Failed to load configuration.");
+            configuration.torii_configuration.torii_url = "127.0.0.1:1338".to_string();
+            configuration.torii_configuration.torii_connect_url = "127.0.0.1:8890".to_string();
+            configuration
+                .kura_configuration
+                .kura_block_store_path(temp_dir.path());
+            let iroha = Iroha::new(configuration.clone());
+            task::block_on(iroha.start()).expect("Failed to start Iroha.");
+            //Prevents temp_dir from clean up untill the end of the tests.
+            #[allow(clippy::empty_loop)]
+            loop {}
+        });
+        thread::sleep(Duration::from_millis(300));
+        let mut configuration =
+            Configuration::from_path(CONFIGURATION_PATH).expect("Failed to load configuration.");
+        configuration.torii_configuration.torii_url = "127.0.0.1:1338".to_string();
+        configuration.torii_configuration.torii_connect_url = "127.0.0.1:8890".to_string();
+        let mut iroha_client = Client::with_maintenance(
+            &ClientConfiguration::from_iroha_configuration(&configuration),
+        );
+        let mut stream = iroha_client
+            .subscribe_to_transaction_changes()
+            .await
+            .expect("Failed to execute request.");
+        let domain_name = "global";
+        let asset_definition_id = AssetDefinitionId::new("xor", domain_name);
+        let create_asset = isi::Register {
+            object: AssetDefinition::new(asset_definition_id),
+            destination_id: domain_name.to_string(),
+        };
+        let mut iroha_client = Client::new(&ClientConfiguration::from_iroha_configuration(
+            &configuration,
+        ));
+        task::block_on(iroha_client.submit(create_asset.into())).expect("Failed to prepare state.");
+        while let Some(change) = stream.next().await {
+            println!("Change received {:?}", change);
+            match change {
+                Occurrence::Created(entity)
+                | Occurrence::Updated(entity)
+                | Occurrence::Deleted(entity) => match entity {
+                    Entity::Transaction(_) => {
+                        println!("Entity changed: {:?}", entity);
+                        return ();
+                    }
+                    _ => println!("Received not expected change: {:?}", entity),
+                },
+            }
+        }
+        panic!("Failed to receive change.");
     }
 }
