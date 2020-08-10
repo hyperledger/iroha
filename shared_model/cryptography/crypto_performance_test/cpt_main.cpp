306b9e649cee1e2f55c55f6bdba085da1997f77d
a.stepanov <a.stepanov@innopolis.ru>
2020-08-10 23:22:24 +0300

    crypto_performance_test refactoring
    Signed-off-by: Alexey Stepanov <alexeystep.karss@gmail.com>

--- shared_model/cryptography/crypto_performance_test/cpt_main.cpp
+++ shared_model/cryptography/crypto_performance_test/cpt_main.cpp
@@ -16,28 +16,19 @@ const std::string blobMsg     = "Sign and verify test message";
 const std::string blobWrgMsg  = "Wrong test message";
 
 
-void inline printResults(unsigned numberOfRuns, double totalTime){
-  std::cout << "  number of runs: " << numberOfRuns << std::endl;
-  std::cout << "  total: " << totalTime << " [ms]" << std::endl;  
-  std::cout << "  avg: " << (numberOfRuns ? (totalTime / numberOfRuns) : 0) << " [ms]" << std::endl;
-}
-
 template<class CryptoProvider>
 auto generateKeypairTest(unsigned numOfRuns){
-  std::cout << "generate keypair test:" << std::endl;
   auto start = std::chrono::steady_clock::now();
   for(unsigned i = 0; i < numOfRuns; ++i)
     CryptoProvider::generateKeypair();
   auto end = std::chrono::steady_clock::now();
 
   auto total = std::chrono::duration<double>(end - start).count();
-  printResults(numOfRuns, total);
   return total;
 }
 
 template<class CryptoProvider>
 auto signTest(unsigned numOfRuns){
-  std::cout << "sign test:" << std::endl;
   auto kp = CryptoProvider::generateKeypair();
   auto blob = shared_model::crypto::Blob(blobMsg);
 
@@ -47,13 +38,11 @@ auto signTest(unsigned numOfRuns){
   auto end = std::chrono::steady_clock::now();
   
   auto total = std::chrono::duration<double>(end - start).count();
-  printResults(numOfRuns, total);
   return total;
 }
 
 template<class CryptoProvider>
 auto verifyTest(unsigned numOfRuns){
-  std::cout << "verify test:" << std::endl;
   auto kp = CryptoProvider::generateKeypair();
   auto blob = shared_model::crypto::Blob(blobMsg);
   auto sign = CryptoProvider::sign(blob, kp);
@@ -71,43 +60,17 @@ auto verifyTest(unsigned numOfRuns){
   auto end = std::chrono::steady_clock::now();
 
   auto total = std::chrono::duration<double>(end - start).count();
-  printResults(numOfRuns, total);
   return total;
 }
 
-void PerfTest(){
-  auto gostResults = std::make_tuple(
-    generateKeypairTest<gostCryptoProvider>(generateKeypairNum),
-    signTest<gostCryptoProvider>(signNum),
-    verifyTest<gostCryptoProvider>(verifyNum));
-  auto edResults = std::make_tuple(
-    generateKeypairTest<edCryptoProvider>(generateKeypairNum),
-    signTest<edCryptoProvider>(signNum),
-    verifyTest<edCryptoProvider>(verifyNum));
-
-  std::cout << "=== Test Results ===" << std::endl;
-  std::cout << "Alg\t\tkeypair test\t\tsign test\t\tverify test" << std::endl;
-  std::cout << "GOST3410\t\t" << 
-      std::get<0>(gostResults) / generateKeypairNum << "\t\t" <<
-      std::get<1>(gostResults) / signNum << "\t\t" <<
-      std::get<2>(gostResults) / verifyNum << std::endl;
-  std::cout << "ED25519 \t\t" <<
-      std::get<0>(edResults) / generateKeypairNum << "\t\t" <<
-      std::get<1>(edResults) / signNum << "\t\t" <<
-      std::get<2>(edResults) / verifyNum << std::endl;
-}
-
 template<class CryptoProvider>
-bool integrityTest(){
+auto integrityTest(){
   auto kp = CryptoProvider::generateKeypair();
 
   auto message = shared_model::crypto::Blob(blobMsg);
   auto wrongMessage = shared_model::crypto::Blob(blobWrgMsg);
 
   auto sign = CryptoProvider::sign(message, kp);
-
-  std::cout << "PBK:\n" << kp.publicKey().t << std::endl;
-  std::cout << "Sign:\n" << sign << std::endl;
   
   // signature convertations
   auto blobSign = shared_model::crypto::Blob::fromHexString(sign);
@@ -123,35 +86,36 @@ bool integrityTest(){
   auto res1 = CryptoProvider::verify(signByteRange, message, kpbrange);
   auto res2 = CryptoProvider::verify(signByteRange, wrongMessage, kpbrange);
 
-  std::cout << (res1 ? "Good" : "Bad") << std::endl;
-  std::cout << (!res2 ? "Good" : "Bad") << std::endl;
-
-  return res1 && !res2;
+  return std::make_pair(res1, !res2);
 }
 
-int main(){
-  std::cout << "GOST 34.10:" << std::endl;
-  auto gost_start = std::chrono::steady_clock::now();
-  auto gostIntRes = integrityTest<gostCryptoProvider>();
-  auto gost_end = std::chrono::steady_clock::now();
+template<class CryptoProvider>
+void test(const std::string& algName){
+  std::cout << algName << std::endl;
 
-  std::cout << std::endl;
-
-  std::cout << "Ed 25519:" << std::endl;
-  auto ed_start = std::chrono::steady_clock::now();
-  auto edIntRes = integrityTest<edCryptoProvider>();
-  auto ed_end = std::chrono::steady_clock::now();
-
-  std::cout << std::endl << "Time elapsed: " << std::endl;
-  std::cout << "GOST: " << std::chrono::duration<double>(gost_end - gost_start).count() << " [ms]" << std::endl;
-  std::cout << "ED  : " << std::chrono::duration<double>(ed_end - ed_start).count() << " [ms]" << std::endl;
-
-  std::cout << std::endl << "Performance test: " << std::endl;
-  PerfTest();
+  auto intgRes = integrityTest<CryptoProvider>();
+  auto prfRes = std::make_tuple(
+    generateKeypairTest<CryptoProvider>(generateKeypairNum)/generateKeypairNum,
+    signTest<CryptoProvider>(signNum)/signNum,
+    verifyTest<CryptoProvider>(verifyNum)/verifyNum
+  );
   
+  std::cout << "  Right signature test: " << (intgRes.first ? "Passed" : "Failed") << std::endl;
+  std::cout << "  Wrong signature test: " << (intgRes.second ? "Passed" : "Failed") << std::endl;
+  std::cout << "  Performance test (seconds):" << std::endl;
+  std::cout << "  Keypair test\t\tSign test\t\tVerify test" << std::endl;
+  std::cout.precision(10);
+  std::cout << "  " << std::fixed << 
+      std::get<0>(prfRes) / generateKeypairNum << "\t\t" <<
+      std::get<1>(prfRes) / signNum << "\t\t" <<
+      std::get<2>(prfRes) / verifyNum << std::endl;
+}
+
+
+int main(){
+  test<gostCryptoProvider>("GOST 34.10");
   std::cout << std::endl;
-  std::cout << "GOST3410: " << (gostIntRes?"GOOD":"BAD") << std::endl;
-  std::cout << "ED25519: " << (edIntRes?"GOOD":"BAD") << std::endl;
+  test<edCryptoProvider>("Ed25519");
 
   return 0;
 }
