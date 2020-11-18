cad680ea3e5a6c71ab8e2e606ff129f5c7ccf176
Egor Ivkov <e.o.ivkov@gmail.com>
2020-11-18 12:43:41 +0300

    Introduces Permissions Framework
    Signed-off-by: Egor Ivkov <e.o.ivkov@gmail.com>

--- iroha/src/main.rs
+++ iroha/src/main.rs
@@ -1,4 +1,4 @@
-use iroha::{config::Configuration, Iroha};
+use iroha::{config::Configuration, permissions::AllowAll, Iroha};
 use std::{thread, time::Duration};
 
 const CONFIGURATION_PATH: &str = "config.json";
@@ -8,7 +8,7 @@ async fn main() -> Result<(), String> {
     println!("Hyperledgerいろは2にようこそ！");
     let mut configuration = Configuration::from_path(CONFIGURATION_PATH)?;
     configuration.load_environment()?;
-    Iroha::new(configuration).start().await?;
+    Iroha::new(configuration, AllowAll.into()).start().await?;
     loop {
         thread::sleep(Duration::from_secs(10));
     }
