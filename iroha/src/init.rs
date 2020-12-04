2d9c05ee66c5842e4208d499802eb5f035608323
Egor Ivkov <e.o.ivkov@gmail.com>
2020-12-04 22:04:52 +0300

    Introduces genesis account
    Signed-off-by: Egor Ivkov <e.o.ivkov@gmail.com>

--- iroha/src/init.rs
+++ iroha/src/init.rs
@@ -25,22 +25,33 @@ pub fn domains(configuration: &config::InitConfiguration) -> BTreeMap<String, Do
     };
     let mut domains = BTreeMap::new();
     let _ = domains.insert(domain_name, domain);
+    let _ = domains.insert(
+        GENESIS_DOMAIN_NAME.to_string(),
+        GenesisDomain::new(configuration.genesis_account_public_key.clone()).into(),
+    );
     domains
 }
 
 /// This module contains all configuration related logic.
 pub mod config {
-    use iroha_crypto::PublicKey;
+    use iroha_crypto::{PrivateKey, PublicKey};
     use serde::Deserialize;
     use std::env;
 
     const ROOT_PUBLIC_KEY: &str = "IROHA_ROOT_PUBLIC_KEY";
+    const GENESIS_ACCOUNT_PUBLIC_KEY: &str = "IROHA_GENESIS_ACCOUNT_PUBLIC_KEY";
+    const GENESIS_ACCOUNT_PRIVATE_KEY: &str = "IROHA_GENESIS_ACCOUNT_PRIVATE_KEY";
 
     #[derive(Clone, Deserialize, Debug)]
     #[serde(rename_all = "UPPERCASE")]
     pub struct InitConfiguration {
         /// The public key of an initial "root@global" account
         pub root_public_key: PublicKey,
+        /// Genesis account public key, should be supplied to all the peers.
+        pub genesis_account_public_key: PublicKey,
+        /// Genesis account private key, only needed on the peer that submits the genesis block.
+        #[serde(default)]
+        pub genesis_account_private_key: Option<PrivateKey>,
     }
 
     impl InitConfiguration {
@@ -50,6 +61,19 @@ pub mod config {
                     .map_err(|e| {
                     format!("Failed to parse Public Key of root account: {}", e)
                 })?;
+            }
+            if let Ok(genesis_account_public_key) = env::var(GENESIS_ACCOUNT_PUBLIC_KEY) {
+                self.genesis_account_public_key = serde_json::from_value(serde_json::json!(
+                    genesis_account_public_key
+                ))
+                .map_err(|e| format!("Failed to parse Public Key of genesis account: {}", e))?;
+            }
+            if let Ok(genesis_account_private_key) = env::var(GENESIS_ACCOUNT_PRIVATE_KEY) {
+                self.genesis_account_private_key =
+                    serde_json::from_value(serde_json::json!(genesis_account_private_key))
+                        .map_err(|e| {
+                            format!("Failed to parse Private Key of genesis account: {}", e)
+                        })?;
             }
             Ok(())
         }
