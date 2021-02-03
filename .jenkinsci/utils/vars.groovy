c485d0a974115941be4c4f7d92f59a86e603a227
Stepan Lavrentev <lawrentievsv@gmail.com>
2021-02-03 12:24:14 +0300

    build_fix
    Signed-off-by: Stepan Lavrentev <lawrentievsv@gmail.com>

--- .jenkinsci/utils/vars.groovy
+++ .jenkinsci/utils/vars.groovy
@@ -11,8 +11,8 @@
 def compilerMapping () {
   return ['gcc7': ['cxx_compiler':'g++-7', 'cc_compiler':'gcc-7'],
           'gcc9' : ['cxx_compiler':'g++-9', 'cc_compiler':'gcc-9'],
-          'clang6': ['cxx_compiler':'clang++-6.0', 'cc_compiler':'clang-6.0'],
           'clang7': ['cxx_compiler':'clang++-7', 'cc_compiler':'clang-7'],
+          'clang9': ['cxx_compiler':'clang++-9', 'cc_compiler':'clang-9'],
           'appleclang': ['cxx_compiler':'clang++', 'cc_compiler':'clang'],
          ]
   }
---
+++ libs/common/to_string.hpp
@@ -0,0 +1,104 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef IROHA_LIBS_TO_STRING_HPP
+#define IROHA_LIBS_TO_STRING_HPP
+
+#include <memory>
+#include <string>
+
+#include <boost/optional.hpp>
+
+namespace iroha {
+  namespace to_string {
+    namespace detail {
+      const std::string kBeginBlockMarker = "[";
+      const std::string kEndBlockMarker = "]";
+      const std::string kSingleFieldsSeparator = ", ";
+      const std::string kNotSet = "(not set)";
+
+      /// Print pointers and optionals.
+      template <typename T>
+      inline std::string toStringDereferenced(const T &o);
+    }  // namespace detail
+
+    inline std::string toString(const std::string &o) {
+      return o;
+    }
+
+    template <typename T>
+    inline auto toString(const T &o) -> std::enable_if_t<
+        std::is_same<decltype(std::to_string(o)), std::string>::value,
+        std::string> {
+      return std::to_string(o);
+    }
+
+    template <typename T>
+    inline auto toString(const T &o) -> std::enable_if_t<
+        std::is_same<typename std::decay_t<decltype(o.toString())>,
+                     std::string>::value,
+        std::string> {
+      return o.toString();
+    }
+
+    template <typename... T>
+    inline std::string toString(const std::unique_ptr<T...> &o) {
+      return detail::toStringDereferenced(o);
+    }
+
+    template <typename... T>
+    inline std::string toString(const std::shared_ptr<T...> &o) {
+      return detail::toStringDereferenced(o);
+    }
+
+    template <typename T>
+    inline std::string toString(const T *o) {
+      return detail::toStringDereferenced(o);
+    }
+
+    template <typename T>
+    inline auto toString(const T &o) -> std::enable_if_t<
+        boost::optional_detail::is_optional_related<T>::value,
+        std::string> {
+      return detail::toStringDereferenced(o);
+    }
+
+    /// Print a plain collection.
+    template <typename T, typename = decltype(*std::declval<T>().begin())>
+    inline std::string toString(const T &c) {
+      std::string result = detail::kBeginBlockMarker;
+      bool need_field_separator = false;
+      for (auto &o : c) {
+        if (need_field_separator) {
+          result.append(detail::kSingleFieldsSeparator);
+        }
+        result.append(toString(o));
+        need_field_separator = true;
+      }
+      result.append(detail::kEndBlockMarker);
+      return result;
+    }
+
+    namespace detail {
+      /// Print pointers and optionals.
+      template <typename T>
+      inline std::string toStringDereferenced(const T &o) {
+        if (o) {
+          return ::iroha::to_string::toString(*o);
+        } else {
+          return kNotSet;
+        }
+      }
+
+      template <>
+      inline std::string toStringDereferenced<boost::none_t>(
+          const boost::none_t &) {
+        return kNotSet;
+      }
+    }  // namespace detail
+  }    // namespace to_string
+}  // namespace iroha
+
+#endif
