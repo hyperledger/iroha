26a1e13c3f9fcd8a2d9d1829739cb36a45834fb6
Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
2021-02-04 18:13:16 +0300

    update iroha2 commit for docker build
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

--- scripts/setup_docker_test_env.sh
+++ scripts/setup_docker_test_env.sh
@@ -2,6 +2,6 @@
 git clone https://github.com/hyperledger/iroha.git
 cd iroha || exit
 # specific version of iroha2 to test with
-git checkout 1a2bf8e621153497631a805fdea4d9255397c128
-docker-compose -f docker-compose-single.yml up -d --build --force-recreate
+git checkout 55f8ce6b21842a95f63abb1c4c611c305b6beff0
+docker-compose -f docker-compose.yml up -d --build --force-recreate
 sleep 10
