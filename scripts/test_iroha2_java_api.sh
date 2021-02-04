38ed73b894782b38ee1102440fd7817ee7c0f93f
Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
2021-02-04 17:42:12 +0300

    add github action to test api with iroha2-java
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

--- scripts/test_iroha2_java_api.sh
+++ scripts/test_iroha2_java_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir iroha_api_test
 cd iroha_api_test || exit
-git clone https://github.com/Alexey-N-Chernyshov/iroha.git
+git clone https://github.com/hyperledger/iroha.git
 cd iroha || exit
 git checkout iroha2-java
 ./gradlew build
