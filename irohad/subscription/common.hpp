6a558b59502076ed82ab15fa56398c1ba7b49d89
Alexander Lednev <57529355+iceseer@users.noreply.github.com>
2021-03-10 16:57:57 +0300

    Feature/subscription engine (#813)
    * subscription: utils
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * threaded handler
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * threaded handler queue
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * threaded handler queue
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * threaded handler queue
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * refactoring
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * dispatcher
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * dispatcher
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * subscription engine
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * format
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * compile-time murmur2
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * engine refactoring
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * dispatcher in engine
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * TID chained
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * dispatcher checks
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * engine multithreaded
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * format
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * format
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * manager
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * compiled
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * compiled
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * work
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * refactoring
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * format
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * comments
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * test
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * rollback test
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    # Conflicts:
    #	irohad/main/application.cpp
    
    * move args
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * thread safe
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * fixup!
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * refactoring
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * format
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * rw object holder
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * rename
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * SE sync call
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * issue fixes
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * revert sync call
    
    Signed-off-by: iceseer <iceseer@gmail.com>

---
+++ irohad/subscription/common.hpp
@@ -0,0 +1,87 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef IROHA_SUBSCRIPTION_COMMON_HPP
+#define IROHA_SUBSCRIPTION_COMMON_HPP
+
+#include <chrono>
+#include <mutex>
+#include <shared_mutex>
+
+namespace iroha::utils {
+
+  struct NoCopy {
+    NoCopy(NoCopy const &) = delete;
+    NoCopy &operator=(NoCopy const &) = delete;
+    NoCopy() = default;
+  };
+
+  struct NoMove {
+    NoMove(NoMove &&) = delete;
+    NoMove &operator=(NoMove &&) = delete;
+    NoMove() = default;
+  };
+
+  /**
+   * Protected object wrapper. Allow read-write access.
+   * @tparam T object type
+   * Example:
+   * @code
+   * ReadWriteObject<std::string> obj("1");
+   * bool const is_one_att1 = obj.sharedAccess([](auto const &str) { return str == "1"; });
+   * obj.exclusiveAccess([](auto &str) { str = "2"; });
+   * bool const is_one_att2 = obj.sharedAccess([](auto const &str) { return str == "1"; });
+   * std::cout <<
+   *   "Attempt 1: " << is_one_att1 << std::endl <<
+   *   "Attempt 2: " << is_one_att2;
+   * @endcode
+   */
+  template <typename T>
+  struct ReadWriteObject {
+    template <typename... Args>
+    ReadWriteObject(Args &&... args) : t_(std::forward<Args>(args)...) {}
+
+    template <typename F>
+    inline auto exclusiveAccess(F &&f) {
+      std::unique_lock lock(cs_);
+      return std::forward<F>(f)(t_);
+    }
+
+    template <typename F>
+    inline auto sharedAccess(F &&f) const {
+      std::shared_lock lock(cs_);
+      return std::forward<F>(f)(t_);
+    }
+
+   private:
+    T t_;
+    mutable std::shared_mutex cs_;
+  };
+
+  class WaitForSingleObject final : NoMove, NoCopy {
+    std::condition_variable wait_cv_;
+    std::mutex wait_m_;
+    std::atomic_flag flag_;
+
+   public:
+    WaitForSingleObject() {
+      flag_.test_and_set();
+    }
+
+    bool wait(std::chrono::microseconds wait_timeout) {
+      std::unique_lock<std::mutex> _lock(wait_m_);
+      return wait_cv_.wait_for(_lock,
+                               wait_timeout,
+                               [&]() { return !flag_.test_and_set(); });
+    }
+
+    void set() {
+      flag_.clear();
+      wait_cv_.notify_one();
+    }
+  };
+}  // namespace iroha::utils
+
+#endif  // IROHA_SUBSCRIPTION_COMMON_HPP
---
+++ irohad/subscription/subscription_engine.hpp
@@ -0,0 +1,162 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef IROHA_SUBSCRIPTION_SUBSCRIPTION_ENGINE_HPP
+#define IROHA_SUBSCRIPTION_SUBSCRIPTION_ENGINE_HPP
+
+#include <list>
+#include <memory>
+#include <shared_mutex>
+#include <unordered_map>
+
+#include "subscription/common.hpp"
+#include "subscription/dispatcher.hpp"
+#include "subscription/subscriber.hpp"
+
+namespace iroha::subscription {
+
+  /**
+   * @tparam EventKey - the type of a specific event from event set (e. g. a key
+   * from a storage or a particular kind of event from an enumeration)
+   * @tparam Dispatcher - thread handler
+   * @tparam Receiver - the type of an object that is a part of a Subscriber
+   * internal state and can be accessed on every event
+   */
+  template <typename EventKey, typename Dispatcher, typename Receiver>
+  class SubscriptionEngine final
+      : public std::enable_shared_from_this<
+            SubscriptionEngine<EventKey, Dispatcher, Receiver>>,
+        utils::NoMove,
+        utils::NoCopy {
+   public:
+    using EventKeyType = EventKey;
+    using ReceiverType = Receiver;
+    using SubscriberType = Receiver;
+    using SubscriberWeakPtr = std::weak_ptr<SubscriberType>;
+    using DispatcherType = typename std::decay<Dispatcher>::type;
+    using DispatcherPtr = std::shared_ptr<DispatcherType>;
+
+    /// List is preferable here because this container iterators remain
+    /// alive after removal from the middle of the container
+    /// using custom allocator
+    using SubscribersContainer = std::list<std::tuple<typename Dispatcher::Tid,
+                                                      SubscriptionSetId,
+                                                      SubscriberWeakPtr>>;
+    using IteratorType = typename SubscribersContainer::iterator;
+
+   public:
+    explicit SubscriptionEngine(DispatcherPtr const &dispatcher)
+        : dispatcher_(dispatcher) {
+      assert(dispatcher_);
+    }
+    ~SubscriptionEngine() = default;
+
+   private:
+    struct SubscriptionContext final {
+      std::mutex subscribers_list_cs;
+      SubscribersContainer subscribers_list;
+    };
+    using KeyValueContainer =
+        std::unordered_map<EventKeyType, SubscriptionContext>;
+
+    mutable std::shared_mutex subscribers_map_cs_;
+    KeyValueContainer subscribers_map_;
+    DispatcherPtr dispatcher_;
+
+   public:
+    template <typename Dispatcher::Tid kTid>
+    IteratorType subscribe(SubscriptionSetId set_id,
+                           const EventKeyType &key,
+                           SubscriberWeakPtr ptr) {
+      Dispatcher::template checkTid<kTid>();
+      std::unique_lock lock(subscribers_map_cs_);
+      auto &subscribers_context = subscribers_map_[key];
+
+      std::lock_guard l(subscribers_context.subscribers_list_cs);
+      return subscribers_context.subscribers_list.emplace(
+          subscribers_context.subscribers_list.end(),
+          std::make_tuple(kTid, set_id, std::move(ptr)));
+    }
+
+    void unsubscribe(const EventKeyType &key, const IteratorType &it_remove) {
+      std::unique_lock lock(subscribers_map_cs_);
+      auto it = subscribers_map_.find(key);
+      if (subscribers_map_.end() != it) {
+        auto &subscribers_context = it->second;
+        std::lock_guard l(subscribers_context.subscribers_list_cs);
+        subscribers_context.subscribers_list.erase(it_remove);
+        if (subscribers_context.subscribers_list.empty())
+          subscribers_map_.erase(it);
+      }
+    }
+
+    size_t size(const EventKeyType &key) const {
+      std::shared_lock lock(subscribers_map_cs_);
+      if (auto it = subscribers_map_.find(key); it != subscribers_map_.end()) {
+        auto &subscribers_context = it->second;
+        std::lock_guard l(subscribers_context.subscribers_list_cs);
+        return subscribers_context.subscribers_list.size();
+      }
+      return 0ull;
+    }
+
+    size_t size() const {
+      std::shared_lock lock(subscribers_map_cs_);
+      size_t count = 0ull;
+      for (auto &it : subscribers_map_) {
+        auto &subscribers_context = it->second;
+        std::lock_guard l(subscribers_context.subscribers_list_cs);
+        count += subscribers_context.subscribers_list.size();
+      }
+      return count;
+    }
+
+    template <typename... EventParams>
+    void notify(const EventKeyType &key, EventParams const &... args) {
+      notifyDelayed(std::chrono::microseconds(0ull), key, args...);
+    }
+
+    template <typename... EventParams>
+    void notifyDelayed(std::chrono::microseconds timeout,
+                       const EventKeyType &key,
+                       EventParams const &... args) {
+      std::shared_lock lock(subscribers_map_cs_);
+      auto it = subscribers_map_.find(key);
+      if (subscribers_map_.end() == it)
+        return;
+
+      auto &subscribers_container = it->second;
+      std::lock_guard l(subscribers_container.subscribers_list_cs);
+      for (auto it_sub = subscribers_container.subscribers_list.begin();
+           it_sub != subscribers_container.subscribers_list.end();) {
+        auto wsub = std::get<2>(*it_sub);
+        auto id = std::get<1>(*it_sub);
+
+        if (auto sub = wsub.lock()) {
+          dispatcher_->addDelayed(std::get<0>(*it_sub),
+                                  timeout,
+                                  [wsub(std::move(wsub)),
+                                   id(id),
+                                   key(key),
+                                   args = std::make_tuple(args...)]() mutable {
+                                    if (auto sub = wsub.lock())
+                                      std::apply(
+                                          [&](auto &&... args) {
+                                            sub->on_notify(
+                                                id, key, std::move(args)...);
+                                          },
+                                          std::move(args));
+                                  });
+          ++it_sub;
+        } else {
+          it_sub = subscribers_container.subscribers_list.erase(it_sub);
+        }
+      }
+    }
+  };
+
+}  // namespace iroha::subscription
+
+#endif  // IROHA_SUBSCRIPTION_SUBSCRIPTION_ENGINE_HPP
