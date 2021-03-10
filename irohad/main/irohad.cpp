b051d5856a64d2f49becbe57d3fce3afdcffd0d4
Ivan Kuvaldin <kuvaldin@soramitsu.co.jp>
2021-03-10 16:08:49 +0200

    Fix comments
    Signed-off-by: Ivan Kuvaldin <kuvaldin@soramitsu.co.jp>

--- irohad/main/irohad.cpp
+++ irohad/main/irohad.cpp
@@ -262,8 +262,8 @@ int main(int argc, char *argv[]) {
 
     if(FLAGS_metrics_port.size()) {
       maintenance_metrics_init(FLAGS_metrics_addr + ":" + FLAGS_metrics_port);
-    }else if(config.metrics.addr_port.size()){
-      maintenance_metrics_init(config.metrics.addr_port);
+    }else if(config.metrics_addr_port.size()){
+      maintenance_metrics_init(config.metrics_addr_port);
     }
 
     daemon_status_notifier->notify(
