45167e5201fdde38fc86cdbc58de4596705733dd
i1i1 <vanyarybin1@live.ru>
2021-03-12 17:29:44 +0300

    Rewrite iroha_http_server and rework torii errors
    Signed-off-by: i1i1 <vanyarybin1@live.ru>

--- iroha/src/query.rs
+++ iroha/src/query.rs
@@ -3,10 +3,17 @@
 use crate::prelude::*;
 use iroha_data_model::prelude::*;
 use iroha_derive::Io;
-use iroha_error::{Error, Result};
+use iroha_error::{derive::Error, Error, Result};
+use iroha_http_server::http::{
+    HttpRequest, HttpResponseError, StatusCode, HTTP_CODE_BAD_REQUEST,
+    HTTP_CODE_INTERNAL_SERVER_ERROR,
+};
+use iroha_version::{scale::DecodeVersioned, Version};
 use parity_scale_codec::{Decode, Encode};
 
 use std::convert::TryFrom;
+use std::error::Error as StdError;
+use std::fmt;
 
 /// Query Request verified on the Iroha node side.
 #[derive(Debug, Io, Encode, Decode)]
@@ -37,6 +44,88 @@ impl TryFrom<SignedQueryRequest> for VerifiedQueryRequest {
             signature: sr.signature,
             query: sr.query,
         })
+    }
+}
+
+/// Unsupported version error
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+pub struct UnsupportedVersionError {
+    /// Version that we got
+    pub version: u8,
+}
+
+impl UnsupportedVersionError {
+    /// Expected version
+    pub const fn expected_version() -> u8 {
+        1
+    }
+}
+
+impl StdError for UnsupportedVersionError {}
+
+impl fmt::Display for UnsupportedVersionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Unsupported version. Expected version {}, got: {}",
+            Self::expected_version(),
+            self.version
+        )
+    }
+}
+
+/// Decode verified query error
+#[derive(Error, Debug)]
+pub enum AcceptQueryError {
+    /// Transaction has unsupported version
+    #[error("Transaction has unsupported version")]
+    UnsupportedQueryVersion(#[source] UnsupportedVersionError),
+    /// Failed to decode signed query
+    #[error("Failed to decode signed query")]
+    DecodeVersionedSignedQuery(#[source] iroha_version::error::Error),
+    /// Failed to verify query request
+    #[error("Failed to verify query request")]
+    VerifyQuery(iroha_error::Error),
+}
+
+impl HttpResponseError for AcceptQueryError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            Self::UnsupportedQueryVersion(_) | Self::VerifyQuery(_) => {
+                HTTP_CODE_INTERNAL_SERVER_ERROR
+            }
+            Self::DecodeVersionedSignedQuery(_) => HTTP_CODE_BAD_REQUEST,
+        }
+    }
+
+    fn error_body(&self) -> Vec<u8> {
+        self.to_string().into()
+    }
+}
+
+impl TryFrom<&HttpRequest> for VerifiedQueryRequest {
+    type Error = AcceptQueryError;
+
+    fn try_from(request: &HttpRequest) -> Result<Self, Self::Error> {
+        let query = VersionedSignedQueryRequest::decode_versioned(&request.body)
+            .map_err(AcceptQueryError::DecodeVersionedSignedQuery)?;
+        let query: SignedQueryRequest = query
+            .as_v1()
+            .ok_or_else(|| {
+                AcceptQueryError::UnsupportedQueryVersion(UnsupportedVersionError {
+                    version: query.version(),
+                })
+            })?
+            .clone()
+            .into();
+        VerifiedQueryRequest::try_from(query).map_err(AcceptQueryError::VerifyQuery)
+    }
+}
+impl TryFrom<HttpRequest> for VerifiedQueryRequest {
+    type Error = AcceptQueryError;
+
+    fn try_from(request: HttpRequest) -> Result<Self, Self::Error> {
+        Self::try_from(&request)
     }
 }
 
