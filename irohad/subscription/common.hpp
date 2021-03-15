b5d3e45f53d2a545acbbe75a0c0e34845ddbbb12
iceseer <iceseer@gmail.com>
2021-03-15 15:32:13 +0300

    clang fix
    Signed-off-by: iceseer <iceseer@gmail.com>

--- irohad/subscription/common.hpp
+++ irohad/subscription/common.hpp
@@ -10,6 +10,17 @@
 #include <mutex>
 #include <shared_mutex>
 
+#if __clang__
+namespace std {
+
+template <typename To, typename From>
+inline std::shared_ptr<To> reinterpret_pointer_cast(std::shared_ptr<From> const & ptr) noexcept {
+    return std::shared_ptr<To>(ptr, reinterpret_cast<To *>(ptr.get()));
+}
+
+}
+#endif
+
 namespace iroha::utils {
 
   struct NoCopy {
