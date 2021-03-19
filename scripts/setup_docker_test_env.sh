dd39f0a30b5f493bb935335bd8d73f6ba615e22e
Egor Ivkov <e.o.ivkov@gmail.com>
2021-03-19 18:18:02 +0300

    Introduces transaction metadata
    Signed-off-by: Egor Ivkov <e.o.ivkov@gmail.com>

--- scripts/setup_docker_test_env.sh
+++ scripts/setup_docker_test_env.sh
@@ -2,5 +2,6 @@
 mkdir test_docker
 cp ./target/debug/iroha_client_cli test_docker
 cp ./iroha_client/config.json test_docker
+cp ./scripts/metadata.json test_docker
 docker-compose up -d --build --force-recreate
 sleep 10
