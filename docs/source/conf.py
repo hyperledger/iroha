dfe82b9f4cf79e793089164dff79011f5241e3ec
Sara <lira.lemur@gmail.com>
2021-03-22 03:25:10 +0300

    configuration information+small fixes+change in m2r extension
    Signed-off-by: Sara <lira.lemur@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('.'))
 #
 # needs_sphinx = '1.0'
 
-# Add any Sphinx extension module names here, as strings. They can be
+# Add any Sphinx extension module names here, as strm2rngs. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
@@ -29,7 +29,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'm2r',
+    'm2r2',
     'sphinx_extensions.iroha_permissions'
 ]
 
