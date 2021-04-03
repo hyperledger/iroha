354b9f6961415efea8535581e0f8492d08feab9c
Ivan Kuvaldin <kuvaldin@soramitsu.co.jp>
2021-04-03 23:32:22 +0300

    Integrate kOnBlock to storage_impl.cpp
    Signed-off-by: Ivan Kuvaldin <kuvaldin@soramitsu.co.jp>

--- irohad/main/application.hpp
+++ irohad/main/application.hpp
@@ -215,6 +215,7 @@ class Irohad {
   virtual RunResult initWsvRestorer();
 
   // constructor dependencies
+  std::shared_ptr<iroha::Subscription> se_;
   IrohadConfig config_;
   const std::string listen_ip_;
   boost::optional<shared_model::crypto::Keypair> keypair_;
