63e55269000b989849bc12b72a8c325abea3cf20
iceseer <iceseer@gmail.com>
2021-04-27 11:08:01 +0300

    added schedulers bind/unbind additional schedulers to dispatcher
    Signed-off-by: Alexander Lednev <57529355+iceseer@users.noreply.github.com>
    
    Signed-off-by: iceseer <iceseer@gmail.com>

---
+++ irohad/subscription/scheduler.hpp
@@ -0,0 +1,35 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef IROHA_SUBSCRIPTION_SCHEDULER_HPP
+#define IROHA_SUBSCRIPTION_SCHEDULER_HPP
+
+#include <functional>
+
+#include "subscription/common.hpp"
+
+namespace iroha::subscription {
+
+  class IScheduler {
+   public:
+    using Task = std::function<void()>;
+    virtual ~IScheduler() {}
+
+    /// Stops sheduler work and tasks execution
+    virtual void dispose(bool wait_for_release = true) = 0;
+
+    /// Checks if current scheduler executes task
+    virtual bool isBusy() const = 0;
+
+    /// Adds task to execution queue
+    virtual void add(Task &&t) = 0;
+
+    /// Adds delayed task to execution queue
+    virtual void addDelayed(std::chrono::microseconds timeout, Task &&t) = 0;
+  };
+
+}  // namespace iroha::subscription
+
+#endif  // IROHA_SUBSCRIPTION_SCHEDULER_HPP
