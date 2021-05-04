8389d70928c96e469b7641056a360b30d80b1588
Ivan 'kuvaldini' Kuvaldin <ivan@kuvaldini.pro>
2021-05-04 09:35:22 +0300

    [GHActions] WIP 15 make-workflows
    Signed-off-by: Ivan 'kuvaldini' Kuvaldin <ivan@kuvaldini.pro>

--- .github/make-workflows.sh
+++ .github/make-workflows.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
-set -xeuo pipefail
+set -euo pipefail
 
 script_dir=$(dirname $(realpath "$0"))
 cd $script_dir
 
 edited=
 for f in $(git status -s -- \*.src.yml | sed 's,^.. ,,') ;do
-    out=$(echo $f | sed s,.src.yml\$,.yml,)
-    wout=workflows/$out
-    tempout=$(mktemp)
-    yq eval 'explode(.)' $f >$tempout
+    readonly out=$(echo $f | sed s,.src.yml\$,.yml,)
+    readonly wout=workflows/$out
+    readonly tempout=$(mktemp)
+    trap "rm -f $tempout" EXIT
+    echo >>$tempout "## DO NOT EDIT"
+    echo >>$tempout "## Generated from $f with $(basename $0)"
+    echo >>$tempout ""
+    yq eval 'explode(.)' $f >>$tempout
     if ! diff -q $wout $tempout ;then
         mv $tempout $wout
-    else
-        rm -f $tempout
+        edited+="'$out' "
     fi
-    edited+="$out "
 done
 
-echo >&2 "make-workflows: created/edited $edited"
+if [[ -n "$edited" ]] 
+then echo >&2 "make-workflows: these files were edited: $edited"
+else echo >&2 "make-workflows: everything is up to date"
+fi
