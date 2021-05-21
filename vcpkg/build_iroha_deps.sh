3c368885304b18c7af5d57e6206a51c5de569a1c
kuvaldini <ivan@kuvaldini.pro>
2021-05-21 16:13:13 +0300

    [GHActions] vcpkg/build_iroha_deps.sh
    Signed-off-by: kuvaldini <ivan@kuvaldini.pro>

--- vcpkg/build_iroha_deps.sh
+++ vcpkg/build_iroha_deps.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-vcpkg_path="${1:-$(pwd)/vcpkg}"
+vcpkg_path="${1:-$(pwd)/vcpkg-build}"
 script_dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
 VCPKG_COMMIT_SHA=${VCPKG_COMMIT_SHA:-$(cat "$script_dir/VCPKG_COMMIT_SHA")}
 VCPKG_COMMIT_SHA=${VCPKG_COMMIT_SHA:-da9defc3bddbba39edd9c7e04d4f2bc4bca3f6d4}
 build_dir=${2:-${build_dir:-$(dirname $script_dir)/build}}
 
-git -C $vcpkg_path fetch origin || 
+git -C $vcpkg_path fetch origin ||
    git clone https://github.com/microsoft/vcpkg $vcpkg_path
 
 git -C $vcpkg_path checkout -f $VCPKG_COMMIT_SHA
 git -C $vcpkg_path/ports clean -fdx
 
 ## Apply patches, bypass if already applied, stop on fail
-for i in "$script_dir"/patches/*.patch; do 
+for i in "$script_dir"/patches/*.patch; do
    if git -C $vcpkg_path apply --reverse --check --ignore-whitespace $i &>/dev/null
       then continue ;fi
    git -C $vcpkg_path apply --ignore-whitespace $i
@@ -24,14 +24,14 @@ done
 # cp -r "$script_dir"/ports $vcpkg_path/
 
 MANIFEST_ROOT=$(realpath $script_dir/..)
-BINARYCACHE_PATH=$vcpkg_path/binarycache
+BINARYCACHE_PATH=$(realpath $vcpkg_path)/binarycache
 INSTALL_ROOT=$vcpkg_path/installed
 
 ## Every time clean build of vcpkgtool takes 43 seconds on MacBook 2016 i7 2.8GHz
 ##                                 and takes 3 minutes on default GitHub runner
 ## ToDo try reuse existing vcpkg_tool
 case $(uname | tr '[:upper:]' '[:lower:]') in
-   windows*|msys*|mingw*) 
+   windows*|msys*|mingw*)
       bootstrap(){
          $vcpkg_path/bootstrap-vcpkg.bat -disableMetrics
       }
@@ -60,7 +60,7 @@ $vcpkg_path/vcpkg install \
 ## PROFILING
 ## system macos big sur on macbook pro 2016 i7 2.8GHz
 ## Installation of already built packages takes about 43 sec
-## 
+##
 ## CMake configure
 ## time cmake -Bbuild -DCMAKE_TOOLCHAIN_FILE=$HOME/devel/vcpkg2/scripts/buildsystems/vcpkg.cmake
 ## Executed in    7,56 secs
