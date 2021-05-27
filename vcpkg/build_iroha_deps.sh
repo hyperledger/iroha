ae0366f2c6182d87eec9023b76345066490b0f4e
kuvaldini <ivan@kuvaldini.pro>
2021-05-27 08:02:39 +0300

    [GHActions] revert ci to sucessful build 76d72ab6
    Signed-off-by: kuvaldini <ivan@kuvaldini.pro>

--- vcpkg/build_iroha_deps.sh
+++ vcpkg/build_iroha_deps.sh
@@ -3,13 +3,14 @@ set -xeuo pipefail
 
 vcpkg_path="${1:-$(pwd)/vcpkg-build}"
 script_dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
-VCPKG_REF=${VCPKG_REF:-$(head -1 "$script_dir/VCPKG_COMMIT_SHA")}
-VCPKG_REF=${VCPKG_REF:-2021.05.12}
+VCPKG_COMMIT_SHA=${VCPKG_COMMIT_SHA:-$(cat "$script_dir/VCPKG_COMMIT_SHA")}
+VCPKG_COMMIT_SHA=${VCPKG_COMMIT_SHA:-da9defc3bddbba39edd9c7e04d4f2bc4bca3f6d4}
+build_dir=${2:-${build_dir:-$(dirname $script_dir)/build}}
 
 git -C $vcpkg_path fetch origin ||
    git clone https://github.com/microsoft/vcpkg $vcpkg_path
 
-git -C $vcpkg_path checkout -f $VCPKG_REF
+git -C $vcpkg_path checkout -f $VCPKG_COMMIT_SHA
 git -C $vcpkg_path/ports clean -fdx
 
 ## Apply patches, bypass if already applied, stop on fail
@@ -22,8 +23,7 @@ done
 ## maybe todo - simpler way to add and patch custom ports
 # cp -r "$script_dir"/ports $vcpkg_path/
 
-# MANIFEST_ROOT=$(realpath $script_dir/..)
-MANIFEST_ROOT=$(git -C $script_dir rev-parse --show-toplevel)
+MANIFEST_ROOT=$(realpath $script_dir/..)
 BINARYCACHE_PATH=$(realpath $vcpkg_path)/binarycache
 INSTALL_ROOT=$vcpkg_path/installed
 
@@ -47,31 +47,15 @@ case $(uname | tr '[:upper:]' '[:lower:]') in
 esac
 
 bootstrap
-# ## Do not use `bootstrap` because it is slow and old and has too much logic overhead
-# vcpkg_tool_path=$vcpkg_path/vcpkg-tool
-# VCPKG_TOOL_REF=2021-05-05-9f849c4c43e50d1b16186ae76681c27b0c1be9d9  #2021-02-24-d67989bce1043b98092ac45996a8230a059a2d7e #
-# (
-# git -C $vcpkg_tool_path fetch origin ||
-#    git clone https://github.com/microsoft/vcpkg-tool.git $vcpkg_tool_path
-# cd $vcpkg_tool_path
-# git checkout $VCPKG_TOOL_REF
-# cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -GNinja -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=OFF -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-# cmake --build build
-# cp build/vcpkg $vcpkg_path/vcpkg
-# )
 
-# $vcpkg_path/vcpkg install \
-#    --x-install-root=$INSTALL_ROOT \
-#    --feature-flags=manifests
-# #   --x-manifest-root=. \
-# #   --binarysource=files,$BINARYCACHE_PATH,readwrite \
+#todo use --x-manifest-root=$(git -C $script_dir rev-parse --show-toplevel)
+$vcpkg_path/vcpkg install \
+   --x-manifest-root=$MANIFEST_ROOT \
+   --binarysource=files,$BINARYCACHE_PATH,readwrite \
+   --x-install-root=$INSTALL_ROOT \
 
-## The old lamp way to install
-( #cd /tmp
-cat $script_dir/VCPKG_DEPS_LIST | while read pkgspec ;do
-   $vcpkg_path/vcpkg install --feature-flags=-manifests $pkgspec
-done
-)
+#   --x-install-root=$build_dir/vcpkg_installed \
+#   --x-install-root=$vcpkg_path/installed \  #default for manifest mode is PWD/vcpkg_installed
 
 ## PROFILING
 ## system macos big sur on macbook pro 2016 i7 2.8GHz
