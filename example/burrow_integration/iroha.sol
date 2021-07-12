84fa926ed35df96d94c42b3a5adb60357cb2e9e4
Ayush Jalan <ayushjalan19@gmail.com>
2021-07-12 12:52:55 +0530

    Update iroha.sol

--- example/burrow_integration/iroha.sol
+++ example/burrow_integration/iroha.sol
@@ -16,16 +16,13 @@ contract Iroha {
     }
 
     // Creates an iroha ccount
-    function createAccount(string memory name, string memory domain,
-                           string memory key) public
-                    returns (bytes memory result) {
+    function createAccount(string memory name, string memory domain, string memory key) public  returns (bytes memory result) {
         bytes memory payload = abi.encodeWithSignature(
             "createAccount(string,string,string)",
             name,
             domain,
             key);
-        (bool success, bytes memory ret) =
-            address(serviceContractAddress).delegatecall(payload);
+        (bool success, bytes memory ret) = address(serviceContractAddress).delegatecall(payload);
         require(success, "Error calling service contract function");
 
         emit Created(name, domain);
@@ -33,45 +30,38 @@ contract Iroha {
     }
 
         //Transfers asset from one iroha account to another
-    function transferAsset(string memory src, string memory dst,
-                           string memory asset, string memory amount) public
-                    returns (bytes memory result) {
+    function transferAsset(string memory src, string memory dst, string memory asset, string memory amount) public returns (bytes memory result) {
         bytes memory payload = abi.encodeWithSignature(
             "transferAsset(string,string,string,string)",
             src,
             dst,
             asset,
             amount);
-        (bool success, bytes memory ret) =
-            address(serviceContractAddress).delegatecall(payload);
+        (bool success, bytes memory ret) = address(serviceContractAddress).delegatecall(payload);
         require(success, "Error calling service contract function");
 
         emit Transferred(src, dst, amount);
         result = ret;
     }
     // Adds asset to iroha account
-    function addAsset(string memory asset, string memory amount) public
-                    returns (bytes memory result) {
+    function addAsset(string memory asset, string memory amount) public  returns (bytes memory result) {
         bytes memory payload = abi.encodeWithSignature(
             "addAsset(string,string)",
             asset,
             amount);
-        (bool success, bytes memory ret) =
-            address(serviceContractAddress).delegatecall(payload);
+        (bool success, bytes memory ret) = address(serviceContractAddress).delegatecall(payload);
         require(success, "Error calling service contract function");
 
         emit Added(asset, amount);
         result = ret;
     }
     //Queries balance of an iroha account
-    function queryBalance(string memory _account, string memory _asset) public
-                    returns (bytes memory result) {
+    function queryBalance(string memory _account, string memory _asset) public returns (bytes memory result) {
         bytes memory payload = abi.encodeWithSignature(
             "getAssetBalance(string,string)",
             _account,
             _asset);
-        (bool success, bytes memory ret) =
-            address(serviceContractAddress).delegatecall(payload);
+        (bool success, bytes memory ret) = address(serviceContractAddress).delegatecall(payload);
         require(success,"Error calling service contract function ");
         result = ret;
     }
