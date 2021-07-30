1cc52a0824937ff020a0aa448c20469fefdcebb8
Piotr Pawłowski <68233055+Pawlak00@users.noreply.github.com>
2021-07-30 15:44:43 +0200

    [client-api] extending TxPaginationMeta in GetAccountTransactions, GetAccountAssetsTransactions (#1092)
    Signed-off-by: Piotr Pawlowski <ppiotru@gmail.com>
    Signed-off-by: Piotr Pawłowski <68233055+Pawlak00@users.noreply.github.com>

--- shared_model/backend/protobuf/queries/impl/proto_tx_pagination_meta.cpp
+++ shared_model/backend/protobuf/queries/impl/proto_tx_pagination_meta.cpp
@@ -5,9 +5,11 @@
 
 #include "backend/protobuf/queries/proto_tx_pagination_meta.hpp"
 
-#include <optional>
-#include "cryptography/hash.hpp"
+#include <google/protobuf/util/time_util.h>
 
+#include <optional>
+
+#include "cryptography/hash.hpp"
 namespace types = shared_model::interface::types;
 
 using namespace shared_model::proto;
@@ -26,12 +28,49 @@ types::TransactionsNumberType TxPaginationMeta::pageSize() const {
 std::optional<types::HashType> TxPaginationMeta::firstTxHash() const {
   if (meta_.opt_first_tx_hash_case()
       == iroha::protocol::TxPaginationMeta::OptFirstTxHashCase::
-             OPT_FIRST_TX_HASH_NOT_SET) {
+          OPT_FIRST_TX_HASH_NOT_SET) {
     return std::nullopt;
   }
   return types::HashType::fromHexString(meta_.first_tx_hash());
 }
-
 shared_model::interface::Ordering const &TxPaginationMeta::ordering() const {
   return ordering_;
+}
+// first_tx_time
+std::optional<types::TimestampType> TxPaginationMeta::firstTxTime() const {
+  if (meta_.opt_first_tx_time_case()
+      == iroha::protocol::TxPaginationMeta::OptFirstTxTimeCase::
+          OPT_FIRST_TX_TIME_NOT_SET) {
+    return std::nullopt;
+  }
+  return google::protobuf::util::TimeUtil::TimestampToMilliseconds(
+      meta_.first_tx_time());
+}
+// last_tx_time
+std::optional<types::TimestampType> TxPaginationMeta::lastTxTime() const {
+  if (meta_.opt_last_tx_time_case()
+      == iroha::protocol::TxPaginationMeta::OptLastTxTimeCase::
+          OPT_LAST_TX_TIME_NOT_SET) {
+    return std::nullopt;
+  }
+  return google::protobuf::util::TimeUtil::TimestampToMilliseconds(
+      meta_.last_tx_time());
+}
+// first tx height
+std::optional<types::HeightType> TxPaginationMeta::firstTxHeight() const {
+  if (meta_.opt_first_tx_height_case()
+      == iroha::protocol::TxPaginationMeta::OptFirstTxHeightCase::
+          OPT_FIRST_TX_HEIGHT_NOT_SET) {
+    return std::nullopt;
+  }
+  return types::HeightType(meta_.first_tx_height());
+}
+// last tx height
+std::optional<types::HeightType> TxPaginationMeta::lastTxHeight() const {
+  if (meta_.opt_last_tx_height_case()
+      == iroha::protocol::TxPaginationMeta::OptLastTxHeightCase::
+          OPT_LAST_TX_HEIGHT_NOT_SET) {
+    return std::nullopt;
+  }
+  return types::HeightType(meta_.last_tx_height());
 }
