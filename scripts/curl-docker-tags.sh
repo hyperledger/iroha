e90a5f3471d1dc074ea15a2d364054515e6fa6d6
kuvaldini <ivan@kuvaldini.pro>
2021-09-01 17:13:37 +0300

    Update curl-docker-tags.sh
    /build all
    
    Signed-off-by: kuvaldini <ivan@kuvaldini.pro>

--- scripts/curl-docker-tags.sh
+++ scripts/curl-docker-tags.sh
@@ -8,9 +8,24 @@ repo=${1#*/}
 org=${org:-hyperledger}
 repo=${repo:-iroha}
 
-curl -fsSL "https://registry.hub.docker.com/v2/repositories/$org/$repo/tags/?page_size=9999" |
+cd $(mktemp -d)
+trap "rm -rf $PWD" EXIT
+
+declare -i page=1
+
+while true ;do
+    curl -fsSL "https://registry.hub.docker.com/v2/repositories/$org/$repo/tags/?page_size=9999&page=$page" >page$page.json
+    if cat page$page.json | jq '.next==null' --exit-status >/dev/null
+    then break
+    fi
+    ((page++))
+done
+
+jq -n '{ results: [ inputs.results ] | add }' page*.json >results.json
+
+cat results.json |
     jq -r '.results |
-        map(. | {name:.name, digest0:.images[0].digest})
+        map(. | {name:.name, last_updated:.last_updated, digest0:.images[0].digest})
         | group_by(.digest0)
-        | map(. | {digest:.[0].digest0, names:map(.|.name)})[]
-        | .digest + " <- " + (.names|@csv)  '
+        | map(. | {digest:.[0].digest0, last_updated:.[0].last_updated, names:map(.|.name)} )[]
+        | .last_updated +" -- "+ .digest +" <- "+ (.names|@csv)'
