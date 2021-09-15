a547d422f05b899e6d1c1e845d57e0b6d0b15f61
kuvaldini <ivan@kuvaldini.pro>
2021-09-16 00:04:53 +0300

    Irohad reuse torii and internal ports
    /build all
    
    Signed-off-by: kuvaldini <ivan@kuvaldini.pro>

--- irohad/main/application.cpp
+++ irohad/main/application.cpp
@@ -1033,13 +1033,13 @@ Irohad::RunResult Irohad::run() {
   torii_server = std::make_unique<ServerRunner>(
       listen_ip_ + ":" + std::to_string(config_.torii_port),
       log_manager_->getChild("ToriiServerRunner")->getLogger(),
-      false);
+      true);
 
   // Initializing internal server
   internal_server = std::make_unique<ServerRunner>(
       listen_ip_ + ":" + std::to_string(config_.internal_port),
       log_manager_->getChild("InternalServerRunner")->getLogger(),
-      false);
+      true);
 
   // Run torii server
   IROHA_EXPECTED_TRY_GET_VALUE(torii_port,
