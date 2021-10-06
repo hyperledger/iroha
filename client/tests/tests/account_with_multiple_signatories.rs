cbca4486a42f21a3650be65c767aab89523da4a3
s8sato <49983831+s8sato@users.noreply.github.com>
2021-10-06 19:40:20 +0900

    Trim the path of every member of the workspace
    Signed-off-by: s8sato <49983831+s8sato@users.noreply.github.com>

---
+++ client/tests/tests/account_with_multiple_signatories.rs
@@ -0,0 +1,51 @@
+#![allow(clippy::restriction)]
+
+use std::thread;
+
+use eyre::Result;
+use iroha_client::client::{self, Client};
+use iroha_core::{config::Configuration, prelude::*};
+use iroha_data_model::prelude::*;
+use test_network::{Peer as TestPeer, *};
+
+#[test]
+fn transaction_signed_by_new_signatory_of_account_should_pass() -> Result<()> {
+    let (_rt, peer, mut iroha_client) = <TestPeer>::start_test_with_runtime();
+    let pipeline_time = Configuration::pipeline_time();
+    thread::sleep(pipeline_time);
+
+    // Given
+    let account_id = AccountId::new("alice", "wonderland");
+    let asset_definition_id = AssetDefinitionId::new("xor", "wonderland");
+    let create_asset = RegisterBox::new(IdentifiableBox::AssetDefinition(
+        AssetDefinition::new_quantity(asset_definition_id.clone()).into(),
+    ));
+    let key_pair = KeyPair::generate()?;
+    let add_signatory = MintBox::new(
+        key_pair.public_key.clone(),
+        IdBox::AccountId(account_id.clone()),
+    );
+
+    iroha_client.submit_all(vec![create_asset.into(), add_signatory.into()])?;
+    thread::sleep(pipeline_time * 2);
+    //When
+    let quantity: u32 = 200;
+    let mint_asset = MintBox::new(
+        Value::U32(quantity),
+        IdBox::AssetId(AssetId::new(
+            asset_definition_id.clone(),
+            account_id.clone(),
+        )),
+    );
+    Client::test_with_key(&peer.api_address, key_pair).submit_till(
+        mint_asset,
+        client::asset::by_account_id(account_id),
+        |result| {
+            result.iter().any(|asset| {
+                asset.id.definition_id == asset_definition_id
+                    && asset.value == AssetValue::Quantity(quantity)
+            })
+        },
+    );
+    Ok(())
+}
---
+++ client/tests/tests/add_asset_should_propagate_to_another_peer.rs
@@ -0,0 +1,57 @@
+#![allow(clippy::restriction)]
+
+use std::thread;
+
+use eyre::Result;
+use iroha_client::client::{self, Client};
+use iroha_core::{config::Configuration, prelude::*};
+use iroha_data_model::prelude::*;
+use test_network::{Network, *};
+
+#[test]
+fn client_add_asset_quantity_to_existing_asset_should_increase_asset_amount_on_another_peer(
+) -> Result<()> {
+    // Given
+    let (_rt, network, mut iroha_client) = <Network>::start_test_with_runtime(4, 1);
+    let pipeline_time = Configuration::pipeline_time();
+
+    thread::sleep(pipeline_time * 5);
+
+    let create_domain = RegisterBox::new(IdentifiableBox::Domain(Domain::new("domain").into()));
+    let account_id = AccountId::new("account", "domain");
+    let create_account = RegisterBox::new(IdentifiableBox::NewAccount(
+        NewAccount::with_signatory(account_id.clone(), KeyPair::generate()?.public_key).into(),
+    ));
+    let asset_definition_id = AssetDefinitionId::new("xor", "domain");
+    let create_asset = RegisterBox::new(IdentifiableBox::AssetDefinition(
+        AssetDefinition::new_quantity(asset_definition_id.clone()).into(),
+    ));
+    iroha_client.submit_all(vec![
+        create_domain.into(),
+        create_account.into(),
+        create_asset.into(),
+    ])?;
+    thread::sleep(pipeline_time * 4);
+    //When
+    let quantity: u32 = 200;
+    iroha_client.submit(MintBox::new(
+        Value::U32(quantity),
+        IdBox::AssetId(AssetId::new(
+            asset_definition_id.clone(),
+            account_id.clone(),
+        )),
+    ))?;
+    thread::sleep(pipeline_time);
+
+    //Then
+    Client::test(&network.peers.last().unwrap().api_address).poll_request(
+        client::asset::by_account_id(account_id),
+        |result| {
+            result.iter().any(|asset| {
+                asset.id.definition_id == asset_definition_id
+                    && asset.value == AssetValue::Quantity(quantity)
+            })
+        },
+    );
+    Ok(())
+}
