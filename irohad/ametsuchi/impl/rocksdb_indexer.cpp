851ee40d0dd3dbcd54529fffa652880ccd1e863e
kuvaldini <ivan@kuvaldini.pro>
2021-10-28 21:04:49 +0300

    [GHA] revert rocksdb_indexer.cpp
    /build all
    
    Signed-off-by: kuvaldini <ivan@kuvaldini.pro>

--- irohad/ametsuchi/impl/rocksdb_indexer.cpp
+++ irohad/ametsuchi/impl/rocksdb_indexer.cpp
@@ -22,8 +22,7 @@ void RocksDBIndexer::txHashStatus(const TxPosition &position,
                                   const HashType &tx_hash,
                                   bool is_committed) {
   RocksDbCommon common(db_context_);
-  common.valueBuffer() =
-      is_committed ? "TRUE" : "FALSE";  // TODO USE ONLY ONE BYTE T OR F
+  common.valueBuffer() = is_committed ? "TRUE" : "FALSE";
   common.valueBuffer() += '#';
   common.valueBuffer() += std::to_string(position.height);
   common.valueBuffer() += '#';
@@ -95,5 +94,8 @@ void RocksDBIndexer::txPositions(
 }
 
 iroha::expected::Result<void, std::string> RocksDBIndexer::flush() {
+  RocksDbCommon common(db_context_);
+  if (!common.commit().ok())
+    return expected::makeError("Unable to flush transactions data.");
   return {};
 }
