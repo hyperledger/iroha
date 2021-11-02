b817543d85ece0c19bc6fa4f788fe373f2bc8cf3
Alexey Kalita <kalita.alexey@outlook.com>
2021-11-02 12:21:33 +0300

    Reconnect telemetry
    Signed-off-by: Alexey Kalita <kalita.alexey@outlook.com>

--- telemetry/src/config.rs
+++ telemetry/src/config.rs
@@ -5,9 +5,12 @@ use iroha_config::derive::Configurable;
 use serde::{Deserialize, Serialize};
 use url::Url;
 
+use crate::retry_period::RetryPeriod;
+
 /// Configuration parameters container
-#[derive(Clone, Default, Deserialize, Serialize, Debug, Configurable, PartialEq, Eq)]
+#[derive(Clone, Deserialize, Serialize, Debug, Configurable, PartialEq, Eq)]
 #[serde(rename_all = "UPPERCASE")]
+#[serde(default)]
 #[config(env_prefix = "TELEMETRY_")]
 pub struct Configuration {
     /// The node's name to be seen on the telemetry
@@ -16,8 +19,35 @@ pub struct Configuration {
     /// The url of the telemetry, e.g., ws://127.0.0.1:8001/submit
     #[config(serde_as_str)]
     pub url: Option<Url>,
+    /// The minimum period of time in seconds to wait before reconnecting
+    #[serde(default = "default_min_period")]
+    pub min_period: u64,
+    /// The maximum exponent of 2 that is used for increasing delay between reconnections
+    #[serde(default = "default_max_exponent")]
+    pub max_exponent: u8,
     /// The filepath that to write dev-telemetry to
     #[cfg(feature = "dev-telemetry")]
     #[config(serde_as_str)]
     pub file: Option<PathBuf>,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Self {
+            name: None,
+            url: None,
+            min_period: RetryPeriod::DEFAULT_MIN_PERIOD,
+            max_exponent: RetryPeriod::DEFAULT_MAX_EXPONENT,
+            #[cfg(feature = "dev-telemetry")]
+            file: None,
+        }
+    }
+}
+
+fn default_min_period() -> u64 {
+    RetryPeriod::DEFAULT_MIN_PERIOD
+}
+
+fn default_max_exponent() -> u8 {
+    RetryPeriod::DEFAULT_MAX_EXPONENT
 }
