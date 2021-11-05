6a719bc967f5b88098413d167eed607b874473ac
kuvaldini <ivan@kuvaldini.pro>
2021-11-05 19:43:52 +0200

    irohad --version reports ursa burrow
    Signed-off-by: kuvaldini <ivan@kuvaldini.pro>

--- irohad/main/irohad.cpp
+++ irohad/main/irohad.cpp
@@ -207,8 +207,45 @@ getCommonObjectsFactory() {
       shared_model::validation::FieldValidator>>(validators_config);
 }
 
+
+#include <array>
+#include <string_view>
+
+template <std::string_view const&... Strs>
+struct join
+{
+    // Join all strings into a single std::array of chars
+    static constexpr auto impl() noexcept
+    {
+        constexpr std::size_t len = (Strs.size() + ...);
+        std::array<char, len + 1> arr{};
+        auto append = [i = 0, &arr](auto const& s) mutable {
+            for (auto c : s) arr[i++] = c;
+        };
+        (append(Strs), ...);
+        arr[len] = 0;
+        return arr;
+    }
+    // Give the joined string static storage
+    static constexpr auto arr = impl();
+    // View as a std::string_view
+    static constexpr std::string_view value {arr.data(), arr.size() - 1};
+};
+// Helper to get the value out
+template <std::string_view const&... Strs>
+static constexpr auto join_v = join<Strs...>::value;
+
+using namespace std::string_view_literals;
+
 int main(int argc, char *argv[]) {
-  gflags::SetVersionString(iroha::kGitPrettyVersion);
+  auto version = std::string(iroha::kGitPrettyVersion);
+#if defined(USE_BURROW)
+  version += " burrow";
+#endif
+#if defined(USE_LIBURSA)
+  version += " ursa";
+#endif
+  gflags::SetVersionString(version);
 
   // Parsing command line arguments
   gflags::ParseCommandLineFlags(&argc, &argv, true);
