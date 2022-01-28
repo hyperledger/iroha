255ab6a5ae73b090420393dcab0932030bb1d96d
Piotr Pawlowski <ppiotru@gmail.com>
2022-01-28 22:24:35 +0100

    application fix
    Signed-off-by: Piotr Pawlowski <ppiotru@gmail.com>

--- irohad/main/application.cpp
+++ irohad/main/application.cpp
@@ -331,8 +331,8 @@ Irohad::RunResult Irohad::initStorage(
       cache->addCacheblePath(RDB_ROOT /**/ RDB_WSV /**/ RDB_ROLES);
       cache->addCacheblePath(RDB_ROOT /**/ RDB_WSV /**/ RDB_DOMAIN);
 
-      db_context_ =
-          std::make_shared<ametsuchi::RocksDBContext>(std::move(rdb_port));
+      db_context_ = std::make_shared<ametsuchi::RocksDBContext>(
+          std::move(rdb_port), std::move(cache));
     } break;
 
     default:
