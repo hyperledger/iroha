71e73f95f7dd51ba7a2ff8ba884b84f281c226e3
Piotr Pawlowski <ppiotru@gmail.com>
2022-01-31 14:01:31 +0100

    burrow storage
    Signed-off-by: Piotr Pawlowski <ppiotru@gmail.com>

--- irohad/main/application.hpp
+++ irohad/main/application.hpp
@@ -23,6 +23,7 @@
 #include "main/subscription_fwd.hpp"
 #include "multi_sig_transactions/gossip_propagation_strategy_params.hpp"
 #include "torii/tls_params.hpp"
+#include "ametsuchi/impl/postgres_burrow_storage.hpp"
 
 namespace google::protobuf {
   class Empty;
@@ -110,6 +111,7 @@ namespace shared_model {
 
 class Irohad {
  public:
+  
   using RunResult = iroha::expected::Result<void, std::string>;
 
   /**
@@ -171,6 +173,7 @@ class Irohad {
    * @return void value on success, error message otherwise
    */
   RunResult run();
+
 
   virtual ~Irohad();
 
@@ -260,7 +263,11 @@ class Irohad {
       query_response_factory_;
 
   // ------------------------| internal dependencies |-------------------------
+
+  std::optional<std::shared_ptr<iroha::ametsuchi::PostgresBurrowStorage>>burrow_storage_;
+
   std::optional<std::unique_ptr<iroha::ametsuchi::VmCaller>> vm_caller_;
+
   std::shared_ptr<iroha::ametsuchi::RocksDBContext> db_context_;
 
  public:
