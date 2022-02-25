e2d1fc43322115cc582a2162e33a2c9441d9ff35
Daniil Polyakov <arjentix@gmail.com>
2022-02-25 13:58:08 +0300

    Rebase via merge. 'refactor_event_emission' rebased on 'iroha2-dev'.
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- core/src/smartcontracts/isi/domain.rs
+++ core/src/smartcontracts/isi/domain.rs
@@ -291,7 +291,7 @@ pub mod query {
                 .key
                 .evaluate(wsv, &Context::default())
                 .wrap_err("Failed to get key")
-                .map_err(Error::Evaluate)?;
+                .map_err(|e| Error::Evaluate(e.to_string()))?;
             wsv.map_domain(&id, |domain| {
                 Ok(domain.metadata.get(&key).map(Clone::clone))
             })?
