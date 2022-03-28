419ed3403f646ec7099ca295a445bc92b024c7d5
Aleksandr <a-p-petrosyan@yandex.ru>
2022-03-28 10:20:13 +0400

    [bump]: pre-release preparations.
    Signed-off-by: Aleksandr <a-p-petrosyan@yandex.ru>

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -245,13 +245,13 @@ mod tests {
     }
 
     fn get_test_instruction() -> Instruction {
-        let new_account_id = AccountId::test("mad_hatter", "wonderland");
+        let new_account_id = AccountId::new("mad_hatter", "wonderland").expect("Valid");
         let register_isi = RegisterBox::new(NewAccount::new(new_account_id));
 
         Instruction::Register(register_isi)
     }
     fn get_test_query() -> QueryBox {
-        let account_id = AccountId::test("alice", "wonderland");
+        let account_id = AccountId::new("alice", "wonderland").expect("Valid");
         FindAccountById::new(account_id).into()
     }
 
