f552707fe0dcbf24fbebb70d90ab547e48331890
Daniil Polyakov <arjentix@gmail.com>
2022-03-29 21:02:20 +0300

    Add test with changing asset metadata after 1 sec
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- client/tests/integration/triggers/time_trigger.rs
+++ client/tests/integration/triggers/time_trigger.rs
@@ -40,8 +40,16 @@ fn time_trigger_execution_count_error_should_be_less_than_10_percent() -> Result
 
     let schedule =
         TimeSchedule::starting_at(start_time).with_period(Duration::from_millis(PERIOD_MS));
-    let register_trigger =
-        build_register_mint_rose_trigger_isi(asset_id.clone(), ExecutionTime::Schedule(schedule));
+    let instruction = MintBox::new(1_u32, asset_id.clone());
+    let register_trigger = RegisterBox::new(IdentifiableBox::from(Trigger::new(
+        "mint_rose",
+        Action::new(
+            Executable::from(vec![instruction.into()]),
+            Repeats::Indefinitely,
+            account_id.clone(),
+            EventFilter::Time(TimeEventFilter(schedule)),
+        ),
+    )?));
     test_client.submit(register_trigger)?;
 
     submit_sample_isi_on_every_block_commit(&mut test_client, &account_id, 3)?;
@@ -67,74 +75,42 @@ fn time_trigger_execution_count_error_should_be_less_than_10_percent() -> Result
 }
 
 #[test]
-fn pre_commit_trigger_should_be_executed() -> Result<()> {
-    const CHECKS_COUNT: usize = 5;
+fn change_asset_metadata_after_1_sec() -> Result<()> {
+    const PERIOD_MS: u64 = 1000;
 
     let (_rt, _peer, mut test_client) = <TestPeer>::start_test_with_runtime();
     wait_for_genesis_committed(&vec![test_client.clone()], 0);
+    let start_time = current_time();
 
-    let asset_definition_id = "rose#wonderland".parse().expect("Valid");
-    let account_id: AccountId = "alice@wonderland".parse().expect("Valid");
-    let asset_id = AssetId::new(asset_definition_id, account_id.clone());
+    let asset_definition_id = AssetDefinitionId::new("rose", "wonderland").expect("Valid");
+    let account_id = AccountId::new("alice", "wonderland").expect("Valid");
+    let key = Name::new("petal")?;
 
-    let mut prev_value = get_asset_value(&mut test_client, asset_id.clone())?;
-
-    let register_trigger =
-        build_register_mint_rose_trigger_isi(asset_id.clone(), ExecutionTime::PreCommit);
-    test_client.submit(register_trigger)?;
-
-    let block_filter =
-        EventFilter::Pipeline(PipelineEventFilter::by_entity(PipelineEntityType::Block));
-    for _ in test_client
-        .listen_for_events(block_filter)?
-        .filter(|event| {
-            if let Ok(Event::Pipeline(event)) = event {
-                if event.status == PipelineStatus::Committed {
-                    return true;
-                }
-            }
-            false
-        })
-        .take(CHECKS_COUNT)
-    {
-        let new_value = get_asset_value(&mut test_client, asset_id.clone())?;
-        assert_eq!(new_value, prev_value + 1);
-        prev_value = new_value;
-
-        // ISI just to create a new block
-        let sample_isi = SetKeyValueBox::new(
+    let schedule = TimeSchedule::starting_at(start_time + Duration::from_millis(PERIOD_MS));
+    let instruction = SetKeyValueBox::new(asset_definition_id.clone(), key.clone(), Value::U32(3));
+    let register_trigger = RegisterBox::new(IdentifiableBox::from(Trigger::new(
+        "change_rose_metadata",
+        Action::new(
+            Executable::from(vec![instruction.into()]),
+            Repeats::Exactly(1),
             account_id.clone(),
-            "key".parse::<Name>()?,
-            String::from("value"),
-        );
-        test_client.submit(sample_isi)?;
-    }
+            EventFilter::Time(TimeEventFilter(schedule)),
+        ),
+    )?));
+    test_client.submit(register_trigger)?;
+    submit_sample_isi_on_every_block_commit(
+        &mut test_client,
+        &account_id,
+        usize::try_from(PERIOD_MS / DEFAULT_CONSENSUS_ESTIMATION_MS + 1)?,
+    )?;
+
+    let value = test_client.request(FindAssetDefinitionKeyValueByIdAndKey {
+        id: asset_definition_id.into(),
+        key: key.into(),
+    })?;
+    assert!(matches!(value, Value::U32(3)));
 
     Ok(())
-}
-
-/// Get asset numeric value
-fn get_asset_value(client: &mut Client, asset_id: AssetId) -> Result<u32> {
-    let asset = client.request(client::asset::by_id(asset_id))?;
-    Ok(*TryAsRef::<u32>::try_as_ref(asset.value())?)
-}
-
-/// Build register ISI for trigger which mints roses
-fn build_register_mint_rose_trigger_isi(
-    asset_id: AssetId,
-    execution_time: ExecutionTime,
-) -> RegisterBox {
-    let instruction = MintBox::new(1_u32, asset_id.clone());
-
-    RegisterBox::new(Trigger::new(
-        "mint_rose".parse().expect("Valid"),
-        Action::new(
-            Executable::from([instruction.into()]),
-            Repeats::Indefinitely,
-            asset_id.account_id,
-            EventFilter::Time(TimeEventFilter(execution_time)),
-        ),
-    ))
 }
 
 /// Submit some sample ISIs to create new blocks
