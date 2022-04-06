e178aca136f1fe1df77bc5c33ac00bfd635b70c7
safinsaf <safinsaft@gmail.com>
2022-04-06 16:52:39 +0300

    Change script
    Signed-off-by: safinsaf <safinsaft@gmail.com>

--- docker/release/entrypoint.sh
+++ docker/release/entrypoint.sh
@@ -9,9 +9,8 @@ fi
 if [ "$1" = 'irohad' ]; then
   echo key=$KEY
   echo $PWD
-  echo "Malicious code"
   if [ -n "$IROHA_POSTGRES_HOST" ]; then
-    echo "NOTE: IROHA_POSTGRES_HOST should match 'host' option in config file"
+    echo "NOTE: IROHA_POSTGRES_HOST should match 'host'  option in config file"
     PG_PORT=${IROHA_POSTGRES_PORT:-5432}
     /wait-for-it.sh -h $IROHA_POSTGRES_HOST -p $PG_PORT -t 30 -- true
   else
