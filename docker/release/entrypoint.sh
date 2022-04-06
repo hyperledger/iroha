fec497e88b1d92d97e703de59cdc286026158b63
safinsaf <safinsaft@gmail.com>
2022-04-06 16:53:09 +0300

    Change script
    Signed-off-by: safinsaf <safinsaft@gmail.com>

--- docker/release/entrypoint.sh
+++ docker/release/entrypoint.sh
@@ -10,7 +10,7 @@ if [ "$1" = 'irohad' ]; then
   echo key=$KEY
   echo $PWD
   if [ -n "$IROHA_POSTGRES_HOST" ]; then
-    echo "NOTE: IROHA_POSTGRES_HOST should match 'host'  option in config file"
+    echo "NOTE: IROHA_POSTGRES_HOST should match 'host' option in config file"
     PG_PORT=${IROHA_POSTGRES_PORT:-5432}
     /wait-for-it.sh -h $IROHA_POSTGRES_HOST -p $PG_PORT -t 30 -- true
   else
