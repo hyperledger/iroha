33d9bd640726a96b3abf0a9a713f305a54aba3a3
Daniil Polyakov <arjentix@gmail.com>
2022-04-06 16:43:04 +0300

    Move FromVariant tests to the separate folder
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- macro/derive/tests/ui.rs
+++ macro/derive/tests/ui.rs
@@ -1,8 +1,10 @@
 use trybuild::TestCases;
 
 #[test]
-fn ui() {
+fn from_variant_ui() {
     let test_cases = TestCases::new();
-    test_cases.pass("tests/ui_pass/*.rs");
-    test_cases.compile_fail("tests/ui_fail/*.rs");
+    test_cases.pass("tests/ui_pass/from_variant/*.rs");
+    test_cases.compile_fail("tests/ui_fail/from_variant/*.rs");
+}
+
 }
