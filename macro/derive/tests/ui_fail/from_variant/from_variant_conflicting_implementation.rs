3c8ca55eca0c1d5a922b713461e2e58f82ef1cb4
Daniil Polyakov <arjentix@gmail.com>
2022-04-07 20:29:36 +0300

    Move ui_fail for FromVariant into separate folder
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- macro/derive/tests/ui_fail/from_variant/from_variant_conflicting_implementation.rs
+++ macro/derive/tests/ui_fail/from_variant/from_variant_conflicting_implementation.rs
@@ -1,4 +1,4 @@
-include!("../ui_pass/enum_from_variant.rs");
+include!("../../ui_pass/from_variant/enum_from_variant.rs");
 
 impl From<Variant1> for Enum {
     fn from(variant: Variant1) -> Self {
