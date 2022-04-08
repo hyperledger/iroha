2997d7049c388467adef039886e95ef40dda8fcc
Daniil Polyakov <arjentix@gmail.com>
2022-04-08 20:46:55 +0300

    Add list-types subcommand
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- tools/parity_scale_decoder/src/main.rs
+++ tools/parity_scale_decoder/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::print_stdout, clippy::use_debug, clippy::unnecessary_wraps)]
 
-use std::{fs, io, path::PathBuf};
+use std::{collections::BTreeSet, fs, io, path::PathBuf};
 
 use clap::Parser;
 use eyre::{eyre, Result};
@@ -10,7 +10,15 @@ use iroha_macro::{get_dump_decoded_map, DumpDecodedMap};
 /// Parity Scale decoder tool for Iroha structs
 #[derive(Debug, Parser)]
 #[clap(version, about, author)]
-struct Args {
+enum Args {
+    /// Show all available types
+    ListTypes,
+    /// Decode type from binary
+    Decode(Decode),
+}
+
+#[derive(Debug, clap::Args)]
+struct Decode {
     /// Path to the binary with encoded Iroha structure
     binary: PathBuf,
     /// Type that is expected to be encoded in binary.
@@ -22,17 +30,23 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let bytes = fs::read(args.binary)?;
+    let map = get_dump_decoded_map!();
     let stdout = io::stdout();
     let mut writer = io::BufWriter::new(stdout.lock());
 
-    let map = get_dump_decoded_map!();
+    match args {
+        Args::Decode(decode_args) => decode(decode_args, map, &mut writer),
+        Args::ListTypes => list_types(map, &mut writer),
+    }
+}
+
+fn decode<W: io::Write>(args: Decode, map: &DumpDecodedMap, writer: &mut W) -> Result<()> {
+    let bytes = fs::read(args.binary)?;
 
     if let Some(type_id) = args.type_id {
-        return decode_by_type(map, &type_id, &bytes, &mut writer);
+        return decode_by_type(map, &type_id, &bytes, writer);
     }
-
-    decode_by_guess(map, &bytes, &mut writer)
+    decode_by_guess(map, &bytes, writer)
 }
 
 fn decode_by_type<W: io::Write>(
@@ -58,4 +72,18 @@ fn decode_by_guess<W: io::Write>(map: &DumpDecodedMap, bytes: &[u8], writer: &mu
         return writeln!(writer, "1 compatible type found").map_err(Into::into);
     }
     writeln!(writer, "{count} compatible types found").map_err(Into::into)
+}
+
+fn list_types<W: io::Write>(map: &DumpDecodedMap, writer: &mut W) -> Result<()> {
+    let sorted = map.keys().collect::<BTreeSet<_>>();
+    for key in sorted {
+        writeln!(writer, "{key}")?;
+    }
+
+    match map.len() {
+        0 => writeln!(writer, "No type is supported"),
+        1 => writeln!(writer, "\n1 type is supported"),
+        n => writeln!(writer, "\n{n} types are supported"),
+    }
+    .map_err(Into::into)
 }
