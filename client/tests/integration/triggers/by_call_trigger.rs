55cd9499a0a8ab2952e276ff05961528dd7d591b
Daniil Polyakov <arjentix@gmail.com>
2022-05-05 15:42:14 +0300

    Add test to check behaviour when trigger fails
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- client/tests/integration/triggers/by_call_trigger.rs
+++ client/tests/integration/triggers/by_call_trigger.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::restriction)]
 
-use std::{sync::mpsc, thread, time::Duration};
+use std::{str::FromStr as _, sync::mpsc, thread, time::Duration};
 
 use eyre::{eyre, Result, WrapErr};
 use iroha_client::client::{self, Client};
@@ -92,6 +92,60 @@ fn infinite_recursion_should_produce_one_call_per_block() -> Result<()> {
     let new_value = get_asset_value(&mut test_client, asset_id)?;
     assert_eq!(new_value, prev_value + 1);
 
+    Ok(())
+}
+
+#[test]
+fn trigger_failure_should_not_cancel_other_triggers_execution() -> Result<()> {
+    let (_rt, _peer, mut test_client) = <TestPeer>::start_test_with_runtime();
+    wait_for_genesis_committed(&vec![test_client.clone()], 0);
+
+    let asset_definition_id = "rose#wonderland".parse()?;
+    let account_id = AccountId::from_str("alice@wonderland")?;
+    let asset_id = AssetId::new(asset_definition_id, account_id.clone());
+
+    // Registering trigger that should fail on execution
+    let bad_trigger_id = <Trigger<FilterBox> as Identifiable>::Id::from_str("bad_trigger")?;
+    // Invalid instruction
+    let bad_trigger_instructions = vec![MintBox::new(1_u32, account_id.clone()).into()];
+    let register_bad_trigger = RegisterBox::new(Trigger::new(
+        bad_trigger_id.clone(),
+        Action::new(
+            Executable::from(bad_trigger_instructions),
+            Repeats::Indefinitely,
+            account_id.clone(),
+            FilterBox::ExecuteTrigger(ExecuteTriggerEventFilter::new(
+                bad_trigger_id.clone(),
+                account_id.clone(),
+            )),
+        ),
+    ));
+    test_client.submit(register_bad_trigger)?;
+
+    // Registering normal trigger
+    let trigger_id = <Trigger<FilterBox> as Identifiable>::Id::from_str(TRIGGER_NAME)?;
+    let trigger_instructions = vec![MintBox::new(1_u32, asset_id.clone()).into()];
+    let register_trigger = RegisterBox::new(Trigger::new(
+        trigger_id,
+        Action::new(
+            Executable::from(trigger_instructions),
+            Repeats::Indefinitely,
+            account_id,
+            // Time-triggers (which are Pre-commit triggers) will be executed last
+            FilterBox::Time(TimeEventFilter(ExecutionTime::PreCommit)),
+        ),
+    ));
+    test_client.submit_blocking(register_trigger)?;
+
+    // Saving current asset value
+    let prev_asset_value = get_asset_value(&mut test_client, asset_id.clone())?;
+
+    // Executing bad trigger
+    test_client.submit_blocking(ExecuteTriggerBox::new(bad_trigger_id))?;
+
+    // Checking results
+    let new_asset_value = get_asset_value(&mut test_client, asset_id)?;
+    assert_eq!(new_asset_value, prev_asset_value + 1);
     Ok(())
 }
 
