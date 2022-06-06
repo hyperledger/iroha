0817682bab5a46b52d600277b606db23883968cb
Sam H. Smith <sam.henning.smith@protonmail.com>
2022-06-06 19:38:03 +0200

    [fix] #2294: Add flamegraph generation to oneshot.rs
    Signed-off-by: Sam H. Smith <sam.henning.smith@protonmail.com>

--- client/benches/tps/oneshot.rs
+++ client/benches/tps/oneshot.rs
@@ -2,10 +2,41 @@
 
 mod lib;
 
-#[allow(clippy::expect_used)]
+use std::{fs::File, io::BufWriter};
+
+use tracing_flame::{FlameLayer, FlushGuard};
+use tracing_subscriber::prelude::*;
+
+#[allow(clippy::expect_used, clippy::print_stdout, clippy::use_debug)]
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut flush_guard: Option<FlushGuard<BufWriter<File>>> = None;
+
+    if args.len() >= 2 {
+        let file = File::create(&args[1]).expect("valid path");
+
+        let flame_layer = FlameLayer::new(BufWriter::new(file))
+            .with_threads_collapsed(true)
+            .with_empty_samples(true);
+        flush_guard = Some(flame_layer.flush_on_drop());
+
+        tracing_subscriber::registry().with(flame_layer).init();
+        iroha_logger::disable_logger();
+    }
+
     let config = lib::Config::from_path("benches/tps/config.json").expect("Failed to configure");
     let tps = config.measure().expect("Failed to measure");
-    iroha_logger::info!(?config);
-    iroha_logger::info!(%tps);
+
+    match flush_guard {
+        Some(guard) => {
+            guard.flush().expect("Flushed data without errors");
+            println!("Tracing data outputted to file: {}", &args[1]);
+            println!("TPS was {}", tps);
+            println!("Config was {:?}", config);
+        }
+        None => {
+            iroha_logger::info!(?config);
+            iroha_logger::info!(%tps);
+        }
+    }
 }
