f61480cefd9bafa090eef08f6f1c8343b64196a6
Daniil Polyakov <arjentix@gmail.com>
2022-06-08 02:58:06 +0300

    Fix mock feature flag
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- core/src/wsv.rs
+++ core/src/wsv.rs
@@ -40,7 +40,7 @@ mod __world {
     /// The global entity consisting of `domains`, `triggers` and etc.
     /// For example registration of domain, will have this as an ISI target.
     #[derive(Debug, Default, Clone, Getters)]
-    #[cfg_attr(feature = "mock", allow(dead_code))]
+    #[cfg_attr(feature = "mock_world", allow(dead_code))]
     #[getset(get = "pub")]
     pub struct World {
         /// Iroha parameters.
@@ -80,6 +80,7 @@ mod __world {
         }
     }
 
+    #[cfg(feature = "mock_world")]
     mockall::mock! {
         pub World {
             /// Creates an empty `World`.
@@ -119,7 +120,7 @@ mod __world {
 }
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "mock")] {
+    if #[cfg(feature = "mock_world")] {
         pub use __world::MockWorld as World;
         pub use __world::World as OriginalWorld;
     } else {
--- core/test_network/tests/integration/mod.rs
+++ core/test_network/tests/integration/mod.rs
@@ -1,1 +1,2 @@
+#[cfg(feature = "mock")]
 mod sumeragi_with_mock;
