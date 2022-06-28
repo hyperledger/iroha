85f7e521a682def3583fb3be5583c8d9e795a9a0
Daniil Polyakov <arjentix@gmail.com>
2022-06-28 22:21:58 +0300

    Update has_token.rs
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- core/src/smartcontracts/isi/permissions/has_token.rs
+++ core/src/smartcontracts/isi/permissions/has_token.rs
@@ -24,29 +24,51 @@ pub trait HasToken: Debug {
     ) -> std::result::Result<PermissionToken, String>;
 }
 
-impl IsAllowed<Instruction> for HasTokenBoxed {
+impl GetValidatorType for HasTokenBoxed {
+    fn get_validator_type(&self) -> ValidatorType {
+        ValidatorType::Instruction
+    }
+}
+
+impl IsAllowed for HasTokenBoxed {
+    type Operation = Instruction;
+
     fn check(
         &self,
         authority: &AccountId,
         instruction: &Instruction,
         wsv: &WorldStateView,
-    ) -> Result<()> {
-        let permission_token = self
-            .token(authority, instruction, wsv)
-            .map_err(|err| format!("Unable to identify corresponding permission token: {}", err))?;
-        let contain = wsv
-            .map_account(authority, |account| {
-                account.contains_permission(&permission_token)
-            })
-            .map_err(|e| e.to_string())?;
+    ) -> ValidatorVerdict {
+        let permission_token = match self.token(authority, instruction, wsv) {
+            Ok(permission_token) => permission_token,
+            Err(err) => {
+                return ValidatorVerdict::Deny(
+                    format!("Unable to identify corresponding permission token: {}", err).into(),
+                );
+            }
+        };
+
+        let contain = match wsv.map_account(authority, |account| {
+            account.contains_permission(&permission_token)
+        }) {
+            Ok(contain) => contain,
+            Err(err) => {
+                return ValidatorVerdict::Deny(
+                    format!("Unable to check if account has permission token: {}", err).into(),
+                );
+            }
+        };
+
         if contain {
-            Ok(())
+            ValidatorVerdict::Allow
         } else {
-            Err(format!(
-                "Account does not have the needed permission token: {:?}.",
-                permission_token
+            ValidatorVerdict::Deny(
+                format!(
+                    "Account does not have the needed permission token: {:?}.",
+                    permission_token
+                )
+                .into(),
             )
-            .into())
         }
     }
 }
