bf8f99db54bd604b143ba253ee83fbf6bb03ad0f
Shunkichi Sato <49983831+s8sato@users.noreply.github.com>
2022-08-22 23:20:00 +0900

    Fix `iroha_telemetry::ws::tests`: Use `chrono::Utc` instead of `chrono::Local`
    Signed-off-by: Shunkichi Sato <49983831+s8sato@users.noreply.github.com>

--- telemetry/src/ws.rs
+++ telemetry/src/ws.rs
@@ -2,7 +2,7 @@
 #![allow(clippy::std_instead_of_core, clippy::std_instead_of_alloc)]
 use std::time::Duration;
 
-use chrono::Local;
+use chrono::Utc;
 use eyre::{eyre, Result};
 use futures::{stream::SplitSink, Sink, SinkExt, StreamExt};
 use iroha_logger::Telemetry;
@@ -214,13 +214,13 @@ fn prepare_message(name: &str, telemetry: Telemetry) -> Result<(Message, Option<
         payload.insert("authority".into(), false.into());
         payload.insert(
             "startup_time".into(),
-            Local::now().timestamp_millis().to_string().into(),
+            Utc::now().timestamp_millis().to_string().into(),
         );
         payload.insert("network_id".into(), "".into());
     }
     let mut map = Map::new();
     map.insert("id".into(), 0_i32.into());
-    map.insert("ts".into(), Local::now().to_rfc3339().into());
+    map.insert("ts".into(), Utc::now().to_rfc3339().into());
     map.insert("payload".into(), payload.into());
     let msg = Message::Binary(serde_json::to_vec(&map)?);
     Ok((msg, msg_kind))
@@ -568,7 +568,7 @@ mod tests {
         // The message is sent
         fail_send.store(false, Ordering::Release);
         tokio::time::sleep(Duration::from_secs(1)).await;
-        message_receiver.try_next().unwrap_err();
+        message_receiver.try_next().unwrap();
     }
 
     macro_rules! test_with_suite {
