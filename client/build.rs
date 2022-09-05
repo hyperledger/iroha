0ce154cc1d2247bfedff20d059e1ab4afb6b5bc7
Daniil Polyakov <arjentix@gmail.com>
2022-09-05 16:20:59 +0300

    Rebase via merge. 'wasm_validators' rebased on 'origin/iroha2-dev'.
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- client/build.rs
+++ client/build.rs
@@ -8,6 +8,7 @@ use core::str::from_utf8;
 use std::{
     env,
     ffi::OsStr,
+    fs,
     path::{Path, PathBuf},
     process::Command,
 };
@@ -17,10 +18,6 @@ use eyre::{eyre, Context as _, Result};
 #[allow(clippy::expect_used)]
 fn main() {
     const TEST_SMARTCONTRACTS_DIR: &str = "tests/integration/smartcontracts";
-    const TEST_SMARTCONTRACTS: [&str; 2] = [
-        "create_nft_for_every_user_smartcontract",
-        "deny_new_validators_validator",
-    ];
 
     println!("cargo:rerun-if-changed={TEST_SMARTCONTRACTS_DIR}");
     // TODO: check if this was causing the recursive loop.
@@ -39,20 +36,41 @@ fn main() {
         .expect("Garbage in `rustc --version` output")
         .contains("nightly")
     {
-        let manifest_dir = env::var("CARGO_MANIFEST_DIR")
-            .expect("Expected `CARGO_MANIFEST_DIR` environment variable");
-        let out_dir = env::var_os("OUT_DIR").expect("Expected `OUT_DIR` environment variable");
-
-        for local_path_str in &TEST_SMARTCONTRACTS {
-            let smartcontract_path = Path::new(&manifest_dir)
-                .join(TEST_SMARTCONTRACTS_DIR)
-                .join(local_path_str);
-            format_smartcontract(smartcontract_path.clone())
-                .expect("Failed to format the smartcontract");
-            build_smartcontract(&smartcontract_path, &out_dir)
-                .expect("Failed to build the smartcontract");
-        }
+        build_all_smartcontracts(TEST_SMARTCONTRACTS_DIR)
+            .expect("Failed to build smartcontracts in directory: {TEST_SMARTCONTRACTS_DIR}");
     }
+}
+
+fn build_all_smartcontracts(path: impl AsRef<Path>) -> Result<()> {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR")
+        .wrap_err("Expected `CARGO_MANIFEST_DIR` environment variable")?;
+    let out_dir =
+        env::var_os("OUT_DIR").ok_or_else(|| eyre!("Expected `OUT_DIR` environment variable"))?;
+
+    let full_smartcontracts_path = Path::new(&manifest_dir).join(path.as_ref());
+    for entry in fs::read_dir(full_smartcontracts_path)
+        .wrap_err("Failed to read test smartcontracts directory: {full_smartcontracts_path}")?
+    {
+        let smartcontract_dir =
+            entry.wrap_err("Failed to read test smartcontracts directory entry")?;
+        if !smartcontract_dir
+            .file_type()
+            .wrap_err(format!(
+                "Failed to get file type of entry: {}",
+                smartcontract_dir.file_name().to_string_lossy()
+            ))?
+            .is_dir()
+        {
+            continue;
+        }
+
+        format_smartcontract(smartcontract_dir.path())
+            .wrap_err("Failed to format the smartcontract")?;
+        build_smartcontract(&smartcontract_dir.path(), &out_dir)
+            .wrap_err("Failed to build the smartcontract")?;
+    }
+
+    Ok(())
 }
 
 fn format_smartcontract(smartcontract_path: PathBuf) -> Result<()> {
