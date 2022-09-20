fe8796b316823c8729151f050f879a81fed4587c
Daniil Polyakov <arjentix@gmail.com>
2022-09-20 17:48:12 +0300

    Improve (de-)serialization of Ipfs and DomainId
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- data_model/src/domain.rs
+++ data_model/src/domain.rs
@@ -21,6 +21,7 @@ use iroha_primitives::conststr::ConstString;
 use iroha_schema::IntoSchema;
 use parity_scale_codec::{Decode, Encode, Input};
 use serde::{Deserialize, Serialize};
+use serde_with::{DeserializeFromStr, SerializeDisplay};
 
 use crate::{
     account::{Account, AccountsMap},
@@ -339,6 +340,7 @@ impl FromIterator<Domain> for crate::Value {
 /// Construct using [`FromStr::from_str`] method.
 #[derive(
     Debug,
+    Display,
     Clone,
     PartialEq,
     Eq,
@@ -346,7 +348,8 @@ impl FromIterator<Domain> for crate::Value {
     Ord,
     Hash,
     Encode,
-    Serialize,
+    SerializeDisplay,
+    DeserializeFromStr,
     IntoFfi,
     TryFromReprC,
     IntoSchema,
@@ -413,23 +416,6 @@ impl IpfsPath {
     }
 }
 
-impl<'de> Deserialize<'de> for IpfsPath {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        #[cfg(not(feature = "std"))]
-        use alloc::borrow::Cow;
-        #[cfg(feature = "std")]
-        use std::borrow::Cow;
-
-        use serde::de::Error as _;
-
-        let name = <Cow<str>>::deserialize(deserializer)?;
-        Self::from_str(&name).map_err(D::Error::custom)
-    }
-}
-
 impl Decode for IpfsPath {
     fn decode<I: Input>(input: &mut I) -> Result<Self, parity_scale_codec::Error> {
         let name = ConstString::decode(input)?;
@@ -450,8 +436,8 @@ impl Decode for IpfsPath {
     Hash,
     Decode,
     Encode,
-    Deserialize,
-    Serialize,
+    DeserializeFromStr,
+    SerializeDisplay,
     IntoFfi,
     TryFromReprC,
     IntoSchema,
