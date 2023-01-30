8b3514591c1bc9053ccfd98bdb78bec7380195f5
Daniil Polyakov <arjentix@gmail.com>
2023-01-31 01:41:26 +0300

    [feature] #3207: Add `main` validator
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

---
+++ permission_validators/runtime/main/src/lib.rs
@@ -0,0 +1,63 @@
+//! Main and default Iroha instruction validator.
+
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use iroha_wasm::validator::prelude::*;
+
+/// Validate any [`Instruction`](iroha_wasm::data_model::isi::Instruction).
+//
+// TODO: Not exhaustive list. Add more validators here.
+#[entrypoint(params = "[authority, instruction]")]
+pub fn validate(authority: <Account as Identifiable>::Id, instruction: Instruction) -> Verdict {
+    iroha_asset_burn_validator::validate(authority.clone(), instruction.clone())
+        .and_then(|| iroha_asset_mint_validator::validate(authority.clone(), instruction.clone()))
+        .and_then(|| {
+            iroha_asset_set_key_value_validator::validate(authority.clone(), instruction.clone())
+        })
+        .and_then(|| {
+            iroha_asset_remove_key_value_validator::validate(authority.clone(), instruction.clone())
+        })
+        .and_then(|| {
+            iroha_asset_transfer_validator::validate(authority.clone(), instruction.clone())
+        })
+        .and_then(|| {
+            iroha_asset_unregister_validator::validate(authority.clone(), instruction.clone())
+        })
+        .and_then(|| {
+            iroha_asset_definition_set_key_value_validator::validate(
+                authority.clone(),
+                instruction.clone(),
+            )
+        })
+        .and_then(|| {
+            iroha_asset_definition_remove_key_value_validator::validate(
+                authority.clone(),
+                instruction.clone(),
+            )
+        })
+        .and_then(|| {
+            iroha_asset_definition_unregister_validator::validate(
+                authority.clone(),
+                instruction.clone(),
+            )
+        })
+        .and_then(|| {
+            iroha_asset_definition_transfer_validator::validate(
+                authority.clone(),
+                instruction.clone(),
+            )
+        })
+        .and_then(|| {
+            iroha_account_set_key_value_validator::validate(authority.clone(), instruction.clone())
+        })
+        .and_then(|| {
+            iroha_account_remove_key_value_validator::validate(
+                authority.clone(),
+                instruction.clone(),
+            )
+        })
+        .and_then(|| iroha_parameter_set_validator::validate(authority, instruction))
+}
