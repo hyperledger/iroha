3058a38bc22c0a7f35700742ff617039788a61e3
Daniil Polyakov <arjentix@gmail.com>
2023-05-03 21:53:45 +0300

    [fix] #3408: Fix `public_keys_cannot_be_burned_to_nothing` test
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- client/tests/integration/burn_public_keys.rs
+++ client/tests/integration/burn_public_keys.rs
@@ -1,30 +1,27 @@
 #![allow(clippy::pedantic, clippy::restriction)]
 
-use std::thread;
-
 use iroha_client::client::{account, transaction, Client};
 use iroha_crypto::{KeyPair, PublicKey};
 use iroha_data_model::prelude::*;
 use test_network::*;
-
-use super::Configuration;
 
 fn submit_and_get(
     client: &mut Client,
     instructions: impl IntoIterator<Item = InstructionBox>,
     submitter: Option<(AccountId, KeyPair)>,
 ) -> TransactionValue {
-    let hash = if let Some((account_id, keypair)) = submitter {
-        let tx = TransactionBuilder::new(account_id, Vec::from_iter(instructions), 100_000)
-            .sign(keypair)
-            .unwrap();
-
-        client.submit_transaction(tx).unwrap()
+    let tx = if let Some((account_id, keypair)) = submitter {
+        let tx = TransactionBuilder::new(account_id, Vec::from_iter(instructions), 100_000);
+        tx.sign(keypair).unwrap()
     } else {
-        client.submit_all(instructions).unwrap()
+        let tx = client
+            .build_transaction(instructions, UnlimitedMetadata::default())
+            .unwrap();
+        client.sign_transaction(tx).unwrap()
     };
 
-    thread::sleep(Configuration::pipeline_time() * 2);
+    let hash = tx.hash();
+    let _ = client.submit_transaction_blocking(tx);
 
     client.request(transaction::by_hash(*hash)).unwrap()
 }
@@ -36,7 +33,6 @@ fn account_keys_count(client: &mut Client, account_id: AccountId) -> usize {
 }
 
 #[test]
-#[ignore = "TODO (#3408): Fix this flaky test. For some reason Iroha sometimes just ignores last transaction sometimes"]
 fn public_keys_cannot_be_burned_to_nothing() {
     const KEYS_COUNT: usize = 3;
     let charlie_id: AccountId = "charlie@wonderland".parse().expect("Valid");
@@ -72,7 +68,11 @@ fn public_keys_cannot_be_burned_to_nothing() {
     let charlie = client.request(account::by_id(charlie_id.clone())).unwrap();
     let mut keys = charlie.signatories();
     let burn = |key: PublicKey| InstructionBox::from(BurnBox::new(key, charlie_id.clone()));
-    let burn_keys_leaving_one = keys.by_ref().take(KEYS_COUNT - 1).cloned().map(burn);
+    let burn_keys_leaving_one = keys
+        .by_ref()
+        .filter(|pub_key| pub_key != &charlie_initial_keypair.public_key())
+        .cloned()
+        .map(burn);
 
     let mut committed_txn = submit_and_get(
         &mut client,
@@ -83,11 +83,11 @@ fn public_keys_cannot_be_burned_to_nothing() {
     assert_eq!(keys_count, 1);
     assert!(matches!(committed_txn, TransactionValue::Transaction(_)));
 
-    let burn_the_last_key = keys.cloned().map(burn);
+    let burn_the_last_key = burn(charlie_initial_keypair.public_key().clone());
 
     committed_txn = submit_and_get(
         &mut client,
-        burn_the_last_key,
+        std::iter::once(burn_the_last_key),
         Some((charlie_id.clone(), charlie_initial_keypair)),
     );
     keys_count = charlie_keys_count(&mut client);
