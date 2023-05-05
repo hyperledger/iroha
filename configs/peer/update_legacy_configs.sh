38540ebfc7c85311e65f5be7aaa88729c6e0a7b2
Marin Veršić <marin.versic101@gmail.com>
2023-05-05 16:36:45 +0300

    [bug] #3454: make LTS the default
    Signed-off-by: Marin Veršić <marin.versic101@gmail.com>

--- configs/peer/update_legacy_configs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This script is intended for release updates,
-# when LTS and Stable branch configurations may change.
-# It downloads the previous configurations from raw.githubusercontent.com.
-wget https://raw.githubusercontent.com/hyperledger/iroha/iroha2-stable/configs/peer/config.json -O "$(dirname "${BASH_SOURCE[0]}")/legacy_stable/config.json"
-wget https://raw.githubusercontent.com/hyperledger/iroha/iroha2-stable/configs/peer/genesis.json -O "$(dirname "${BASH_SOURCE[0]}")/legacy_stable/genesis.json"
-wget https://raw.githubusercontent.com/hyperledger/iroha/iroha2-lts/configs/peer/config.json -O "$(dirname "${BASH_SOURCE[0]}")/legacy_lts/config.json"
-wget https://raw.githubusercontent.com/hyperledger/iroha/iroha2-lts/configs/peer/genesis.json -O "$(dirname "${BASH_SOURCE[0]}")/legacy_lts/genesis.json"
