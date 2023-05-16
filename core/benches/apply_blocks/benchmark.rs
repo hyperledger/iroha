e902d2354952926f0c68e09bbce2ee6ea7c54f80
Shanin Roman <shanin1000@yandex.ru>
2023-05-16 19:48:17 +0300

    [refactor]: Add wsv benchmarks
    Signed-off-by: Shanin Roman <shanin1000@yandex.ru>

---
+++ core/benches/apply_blocks/benchmark.rs
@@ -0,0 +1,22 @@
+#![allow(missing_docs, clippy::restriction)]
+
+mod apply_blocks;
+
+use apply_blocks::WsvApplyBlocks;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn apply_blocks(c: &mut Criterion) {
+    let bench = WsvApplyBlocks::setup().expect("Failed to setup benchmark");
+
+    let mut group = c.benchmark_group("apply_blocks");
+    group.significance_level(0.1).sample_size(10);
+    group.bench_function("apply_blocks", |b| {
+        b.iter(|| {
+            WsvApplyBlocks::measure(black_box(&bench)).expect("Failed to execute benchmark");
+        });
+    });
+    group.finish();
+}
+
+criterion_group!(wsv, apply_blocks);
+criterion_main!(wsv);
