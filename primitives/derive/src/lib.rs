ae70450d264986cfe55aa4bd8ba638585575585c
Nikita Strygin <DCNick3@users.noreply.github.com>
2023-07-04 16:32:04 +0300

    [feature] #3383: Fix small stuff after review
    - rename macro to socket_addr (for consistency with already existing decl macro)
    - more backticks
    - fix `SocketAddr` variant names (whoops)
    - re-export the macro from `iroha_primitives` under a temporary name
    
    Signed-off-by: Nikita Strygin <DCNick3@users.noreply.github.com>

--- primitives/derive/src/lib.rs
+++ primitives/derive/src/lib.rs
@@ -1,6 +1,15 @@
-mod socket;
+mod socket_addr;
 
+/// Convenience macro to concisely construct a [`SocketAddr`]
+///
+/// # Examples
+/// ```
+/// # use iroha_primitives_derive::socket_addr;
+///
+/// let localhost = socket_addr!(127.0.0.1:8080);
+/// let remote = socket_addr!([2001:db8::1]:8080);
+/// ```
 #[proc_macro]
-pub fn socket(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    socket::socket_impl(input.into()).into()
+pub fn socket_addr(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    socket_addr::socket_addr_impl(input.into()).into()
 }
