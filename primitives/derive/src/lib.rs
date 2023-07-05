3b3a020298ca6589a3b46e77dab314257910f109
Nikita Strygin <DCNick3@users.noreply.github.com>
2023-07-05 12:42:28 +0300

    [feature] #3383: Allow using an expression in place of a port
    - keep the "stringify and parse" approach for the IP part, but parse the rest using syn's parsing framework
    - special-case brackets: they are used to wrap the IPv6 addresses
    - use colon as a terminator for IPv4 address
    
    Signed-off-by: Nikita Strygin <DCNick3@users.noreply.github.com>

--- primitives/derive/src/lib.rs
+++ primitives/derive/src/lib.rs
@@ -1,6 +1,6 @@
 mod socket_addr;
 
-/// Convenience macro to concisely construct a [`SocketAddr`]
+/// Convenience macro to concisely construct a `SocketAddr`
 ///
 /// # Examples
 /// ```
@@ -8,6 +8,16 @@ mod socket_addr;
 ///
 /// let localhost = socket_addr!(127.0.0.1:8080);
 /// let remote = socket_addr!([2001:db8::1]:8080);
+/// ```
+///
+/// It is also possible to use an expression in port position:
+///
+/// ```
+/// # use iroha_primitives_derive::socket_addr;
+///
+/// let port = 8080;
+///
+/// let localhost = socket_addr!(127.0.0.1:port);
 /// ```
 #[proc_macro]
 pub fn socket_addr(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
