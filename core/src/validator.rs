8abf64b82b7b9b78c464010f1533b3b4170e8265
Daniil Polyakov <arjentix@gmail.com>
2023-07-31 19:22:41 +0300

    [refactor] #3752: Replace `MockValidator` with `Initial`
    Signed-off-by: Daniil Polyakov <arjentix@gmail.com>

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -1,29 +1,25 @@
 //! Structures and impls related to *runtime* `Validator`s processing.
 
 use derive_more::DebugCustom;
-#[cfg(test)]
-use iroha_data_model::transaction::Executable;
 use iroha_data_model::{
     account::AccountId,
     isi::InstructionBox,
     query::QueryBox,
-    transaction::VersionedSignedTransaction,
-    validator::{self as data_model_validator, MigrationResult},
-    ValidationFail,
+    transaction::{Executable, VersionedSignedTransaction},
+    validator as data_model_validator, ValidationFail,
 };
 use iroha_logger::trace;
 
-use super::wsv::WorldStateView;
-use crate::smartcontracts::wasm::{
-    self,
-    error::{Error, ExportFnCallError},
+use super::{
+    smartcontracts::{wasm, Execute as _},
+    wsv::WorldStateView,
 };
 
-impl From<Error> for ValidationFail {
-    fn from(err: Error) -> Self {
+impl From<wasm::error::Error> for ValidationFail {
+    fn from(err: wasm::error::Error) -> Self {
         match err {
-            Error::ExportFnCall(call_error) => {
-                use ExportFnCallError::*;
+            wasm::error::Error::ExportFnCall(call_error) => {
+                use wasm::error::ExportFnCallError::*;
 
                 match call_error {
                     ExecutionLimitsExceeded(_) => Self::TooComplex,
@@ -35,35 +31,39 @@ impl From<Error> for ValidationFail {
     }
 }
 
-/// Validator that verifies the operation is valid.
-///
-/// Can be upgraded with [`Upgrade`](iroha_data_model::isi::Upgrade) instruction.
-#[derive(DebugCustom, Clone)]
-#[debug(
-    fmt = "Validator {{ loaded_validator: {0:?}, engine: <Engine is truncated> }}",
-    "&self.loaded_validator"
-)]
-pub struct Validator {
-    /// Pre-loaded validator.
-    /// Can be set with [`update()`](Validator::update).
-    loaded_validator: LoadedValidator,
+/// Error used in [`migrate()`](Validator::migrate).
+#[derive(Debug, thiserror::Error)]
+pub enum MigrationError {
+    /// Error during WASM blob loading or runtime preparation.
+    #[error("WASM error: {0}")]
+    Wasm(#[from] wasm::error::Error),
+    /// Error returned by entrypoint during execution.
+    #[error("Entrypoint returned error: {0}")]
+    EntrypointExecution(data_model_validator::MigrationError),
 }
 
-impl Validator {
-    /// Create new [`Validator`] from raw validator.
-    ///
-    /// # Errors
-    ///
-    /// Fails if failed to load wasm blob.
-    pub fn new(
-        raw_validator: data_model_validator::Validator,
-        engine: &wasmtime::Engine,
-    ) -> Result<Self, wasm::error::Error> {
-        Ok(Self {
-            loaded_validator: LoadedValidator::load(engine, raw_validator)?,
-        })
-    }
+/// Validator that verifies that operation is valid and executes it.
+///
+/// Executing is done in order to verify dependent instructions in transaction.
+/// So in fact it's more like an **Executor**, and it probably will be renamed soon.
+///
+/// Can be upgraded with [`Upgrade`](iroha_data_model::isi::Upgrade) instruction.
+#[derive(Debug, Default, Clone)]
+pub enum Validator {
+    /// Initial validator that allows all operations and performs no permission checking.
+    #[default]
+    Initial,
+    /// User-provided validator with arbitrary logic.
+    UserProvided(UserProvidedValidator),
+}
 
+/// Validator provided by user.
+///
+/// Used to not to leak private data to the user.
+#[derive(Debug, Clone)]
+pub struct UserProvidedValidator(LoadedValidator);
+
+impl Validator {
     /// Validate [`VersionedSignedTransaction`].
     ///
     /// # Errors
@@ -77,18 +77,33 @@ impl Validator {
         authority: &AccountId,
         transaction: VersionedSignedTransaction,
     ) -> Result<(), ValidationFail> {
-        let runtime = wasm::RuntimeBuilder::<wasm::state::validator::ValidateTransaction>::new()
-            .with_engine(wsv.engine.clone()) // Cloning engine is cheap, see [`wasmtime::Engine`] docs
-            .with_configuration(wsv.config.wasm_runtime_config)
-            .build()?;
-
         trace!("Running transaction validation");
-        runtime.execute_validator_validate_transaction(
-            wsv,
-            authority,
-            &self.loaded_validator.module,
-            transaction,
-        )?
+
+        match self {
+            Self::Initial => {
+                let (_authority, Executable::Instructions(instructions)) = transaction.into() else {
+                    return Ok(());
+                };
+                for isi in instructions {
+                    isi.execute(authority, wsv)?
+                }
+                Ok(())
+            }
+            Self::UserProvided(UserProvidedValidator(loaded_validator)) => {
+                let runtime =
+                    wasm::RuntimeBuilder::<wasm::state::validator::ValidateTransaction>::new()
+                    .with_engine(wsv.engine.clone()) // Cloning engine is cheap, see [`wasmtime::Engine`] docs
+                    .with_configuration(wsv.config.wasm_runtime_config)
+                    .build()?;
+
+                runtime.execute_validator_validate_transaction(
+                    wsv,
+                    authority,
+                    &loaded_validator.module,
+                    transaction,
+                )?
+            }
+        }
     }
 
     /// Validate [`InstructionBox`].
@@ -104,18 +119,25 @@ impl Validator {
         authority: &AccountId,
         instruction: InstructionBox,
     ) -> Result<(), ValidationFail> {
-        let runtime = wasm::RuntimeBuilder::<wasm::state::validator::ValidateInstruction>::new()
-            .with_engine(wsv.engine.clone()) // Cloning engine is cheap, see [`wasmtime::Engine`] docs
-            .with_configuration(wsv.config.wasm_runtime_config)
-            .build()?;
-
         trace!("Running instruction validation");
-        runtime.execute_validator_validate_instruction(
-            wsv,
-            authority,
-            &self.loaded_validator.module,
-            instruction,
-        )?
+
+        match self {
+            Self::Initial => instruction.execute(authority, wsv).map_err(Into::into),
+            Self::UserProvided(UserProvidedValidator(loaded_validator)) => {
+                let runtime =
+                    wasm::RuntimeBuilder::<wasm::state::validator::ValidateInstruction>::new()
+                    .with_engine(wsv.engine.clone()) // Cloning engine is cheap, see [`wasmtime::Engine`] docs
+                    .with_configuration(wsv.config.wasm_runtime_config)
+                    .build()?;
+
+                runtime.execute_validator_validate_instruction(
+                    wsv,
+                    authority,
+                    &loaded_validator.module,
+                    instruction,
+                )?
+            }
+        }
     }
 
     /// Validate [`QueryBox`].
@@ -131,158 +153,67 @@ impl Validator {
         authority: &AccountId,
         query: QueryBox,
     ) -> Result<(), ValidationFail> {
-        let runtime = wasm::RuntimeBuilder::<wasm::state::validator::ValidateQuery>::new()
-            .with_engine(wsv.engine.clone()) // Cloning engine is cheap, see [`wasmtime::Engine`] docs
-            .with_configuration(wsv.config.wasm_runtime_config)
-            .build()?;
-
         trace!("Running query validation");
-        runtime.execute_validator_validate_query(
-            wsv,
-            authority,
-            &self.loaded_validator.module,
-            query,
-        )?
+
+        match self {
+            Self::Initial => Ok(()),
+            Self::UserProvided(UserProvidedValidator(loaded_validator)) => {
+                let runtime = wasm::RuntimeBuilder::<wasm::state::validator::ValidateQuery>::new()
+                    .with_engine(wsv.engine.clone()) // Cloning engine is cheap, see [`wasmtime::Engine`] docs
+                    .with_configuration(wsv.config.wasm_runtime_config)
+                    .build()?;
+
+                runtime.execute_validator_validate_query(
+                    wsv,
+                    authority,
+                    &loaded_validator.module,
+                    query,
+                )?
+            }
+        }
     }
 
-    /// Run migration.
+    /// Migrate validator to a new user-provided one.
+    ///
+    /// Execute `migrate()` entrypoint of the `raw_validator` and set `self` to
+    /// [`UserProvided`](Validator::UserProvided) with `raw_validator`.
     ///
     /// # Errors
     ///
+    /// - Failed to load `raw_validator`;
     /// - Failed to prepare runtime for WASM execution;
     /// - Failed to execute entrypoint of the WASM blob.
     pub fn migrate(
-        &self,
+        &mut self,
+        raw_validator: data_model_validator::Validator,
         wsv: &mut WorldStateView,
         authority: &AccountId,
-    ) -> Result<MigrationResult, wasm::error::Error> {
+    ) -> Result<(), MigrationError> {
+        trace!("Running validator migration");
+
+        let loaded_validator = LoadedValidator::load(&wsv.engine, raw_validator)?;
+
         let runtime = wasm::RuntimeBuilder::<wasm::state::validator::Migrate>::new()
             .with_engine(wsv.engine.clone()) // Cloning engine is cheap, see [`wasmtime::Engine`] docs
             .with_configuration(wsv.config.wasm_runtime_config)
             .build()?;
 
         runtime
-            .execute_validator_migration(wsv, authority, &self.loaded_validator.module)
-            .map_err(Into::into)
-    }
-}
+            .execute_validator_migration(wsv, authority, &loaded_validator.module)?
+            .map_err(MigrationError::EntrypointExecution)?;
 
-/// Mock of validator for unit tests of `iroha_core`.
-///
-/// We can't use real validator because WASM for it is produced in runtime from outside world.
-#[cfg(test)]
-#[derive(Default, Debug, Copy, Clone)]
-pub struct MockValidator;
-
-#[cfg(test)]
-#[allow(
-    clippy::unused_self,
-    clippy::unnecessary_wraps,
-    clippy::trivially_copy_pass_by_ref,
-    clippy::needless_pass_by_value
-)]
-impl MockValidator {
-    /// Mock for creating new validator from raw validator.
-    ///
-    /// # Errors
-    ///
-    /// Never fails with [`Err`].
-    ///
-    /// # Panics
-    ///
-    /// Will immediately panic, because you shouldn't call it in tests.
-    #[allow(clippy::needless_pass_by_value)]
-    pub fn new(
-        _raw_validator: data_model_validator::Validator,
-        _engine: &wasmtime::Engine,
-    ) -> Result<Self, wasm::error::Error> {
-        panic!("You probably don't need this method in tests")
-    }
-
-    /// Mock for transaction validation.
-    /// Will just execute instructions if there are some.
-    ///
-    /// Without this step invalid transactions won't be marked as rejected in
-    /// [`ChainedBlock::validate`].
-    ///
-    /// # Errors
-    ///
-    /// Fails if instruction execution fails
-    pub fn validate_transaction(
-        &self,
-        wsv: &mut WorldStateView,
-        authority: &AccountId,
-        transaction: VersionedSignedTransaction,
-    ) -> Result<(), ValidationFail> {
-        let (_authority, Executable::Instructions(instructions)) = transaction.into() else {
-            return Ok(());
-        };
-        for isi in instructions {
-            Self::execute_instruction(wsv, authority, isi)?;
-        }
+        *self = Self::UserProvided(UserProvidedValidator(loaded_validator));
         Ok(())
-    }
-
-    /// Mock for instruction validation. Will just execute instruction.
-    ///
-    /// # Errors
-    ///
-    /// Fails if instruction execution fails
-    pub fn validate_instruction(
-        &self,
-        wsv: &mut WorldStateView,
-        authority: &AccountId,
-        instruction: InstructionBox,
-    ) -> Result<(), ValidationFail> {
-        Self::execute_instruction(wsv, authority, instruction)
-    }
-
-    /// Mock for query validation.
-    ///
-    /// # Errors
-    ///
-    /// Never fails.
-    pub fn validate_query(
-        &self,
-        _wsv: &WorldStateView,
-        _authority: &AccountId,
-        _query: QueryBox,
-    ) -> Result<(), ValidationFail> {
-        Ok(())
-    }
-
-    /// Mock for validator migration.
-    ///
-    /// # Errors
-    ///
-    /// Never fails.
-    pub fn migrate(
-        &self,
-        _wsv: &mut WorldStateView,
-        _authority: &AccountId,
-    ) -> Result<MigrationResult, wasm::error::Error> {
-        Ok(Ok(()))
-    }
-
-    fn execute_instruction(
-        wsv: &mut WorldStateView,
-        authority: &AccountId,
-        instruction: InstructionBox,
-    ) -> Result<(), ValidationFail> {
-        use super::smartcontracts::Execute as _;
-
-        instruction.execute(authority, wsv).map_err(Into::into)
     }
 }
 
 /// [`Validator`] with [`Module`](wasmtime::Module) for execution.
 ///
-/// Creating [`Module`] is expensive, so we do it once on [`upgrade()`](Validator::upgrade)
-/// step and reuse it on [`validate()`](Validator::validate) step.
+/// Creating a [`wasmtime::Module`] is expensive, so we do it once on [`migrate()`](Validator::migrate)
+/// step and reuse it later on validating steps.
 #[derive(DebugCustom, Clone)]
 #[debug(fmt = "LoadedValidator {{ module: <Module is truncated> }}")]
 struct LoadedValidator {
-    #[cfg_attr(test, allow(dead_code))]
     module: wasmtime::Module,
 }
 
