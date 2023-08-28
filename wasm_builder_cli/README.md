f20b6cd4a2588251930ce12ae81a76e0e2754d1c
Dmitry Balashov <a.marcius26@gmail.com>
2023-08-28 11:19:56 +0700

    [feat]: produce workable `iroha_wasm_builder_cli`
    Signed-off-by: Dmitry Balashov <a.marcius26@gmail.com>

---
+++ wasm_builder_cli/README.md
@@ -0,0 +1,23 @@
+# `iroha_wasm_builder_cli`
+
+A CLI around [`iroha_wasm_builder`](../wasm_builder) crate.
+
+## Usage
+
+**Check the smartcontract:**
+
+```bash
+iroha_wasm_builder_cli check path/to/project
+```
+
+**Build the smartcontract:**
+
+```bash
+iroha_wasm_builder_cli build path/to/project
+```
+
+**Build with options:**
+
+```bash
+iroha_wasm_builder_cli build path/to/project --optimize --format
+```
--- wasm_builder_cli/src/main.rs
+++ wasm_builder_cli/src/main.rs
@@ -1,15 +1,73 @@
-struct Args {
-    /// Path to the smartcontract
-    path: String,
+use std::{
+    io::{stdout, BufWriter, Write},
+    path::PathBuf,
+};
+
+use clap::{Args, Parser};
+use color_eyre::eyre::Context;
+use iroha_wasm_builder::Builder;
+
+#[derive(Parser, Debug)]
+#[command(name = "iroha_wasm_builder_cli", version, author)]
+enum Cli {
     /// Apply `cargo check` to the smartcontract
-    #[arg(long, short)]
-    check: bool,
-    /// Enable smartcontract formatting using `cargo fmt`.
-    #[arg(long, short)]
-    format: bool,
-    #[arg(long, short)]
-    optimize: bool,
-    // TODO: output file? stdout?
+    Check {
+        #[command(flatten)]
+        common: CommonArgs,
+    },
+    /// Build the smartcontract
+    Build {
+        #[command(flatten)]
+        common: CommonArgs,
+        /// Enable smartcontract formatting using `cargo fmt`.
+        // TODO: why it is a part of `build` in wasm_builder?
+        #[arg(long)]
+        format: bool,
+        /// Optimize WASM output.
+        #[arg(long)]
+        optimize: bool,
+    },
 }
 
-fn main() {}
+#[derive(Args, Debug)]
+struct CommonArgs {
+    /// Path to the smartcontract
+    path: PathBuf,
+}
+
+fn main() -> color_eyre::Result<()> {
+    match Cli::parse() {
+        Cli::Check {
+            common: CommonArgs { path },
+        } => {
+            let builder = Builder::new(&path);
+            builder.check()?;
+        }
+        Cli::Build {
+            common: CommonArgs { path },
+            format,
+            optimize,
+        } => {
+            let builder = Builder::new(&path);
+            let builder = if format { builder.format() } else { builder };
+
+            let output = builder.build().wrap_err("Failed to build")?;
+            let output = if optimize {
+                output.optimize().wrap_err("Failed to apply --optimize")?
+            } else {
+                output
+            };
+
+            let bytes = output
+                .into_bytes()
+                .wrap_err("Failed to fetch the bytes of the output smartcontract")?;
+
+            let mut writer = BufWriter::new(stdout());
+            writer
+                .write_all(&bytes)
+                .wrap_err("Failed to output the bytes")?;
+        }
+    }
+
+    Ok(())
+}
