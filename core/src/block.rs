5a72d2e755432d27bf85e4464e49265c8c8b310a
Marin Veršić <marin.versic101@gmail.com>
2023-09-18 15:08:36 +0300

    [documentation]: Improve documentation on block state transitions
    Signed-off-by: Marin Veršić <marin.versic101@gmail.com>

--- core/src/block.rs
+++ core/src/block.rs
@@ -1,5 +1,8 @@
-//! This module contains [`Block`] structures for each state, it's
-//! transitions, implementations and related trait implementations.
+//! This module contains [`Block`] structures for each state. Transitions are modeled as follows:
+//! 1. If a new block is constructed by the node:
+//!     `BlockBuilder<Pending>` -> `BlockBuilder<Chained>` -> `ValidBlock` -> `CommittedBlock`
+//! 2. If a block is received, i.e. deserialized:
+//!     `VersionedSignedBlock` -> `ValidBlock` -> `CommittedBlock`
 //! [`Block`]s are organised into a linear sequence over time (also known as the block chain).
 #![allow(
     clippy::module_name_repetitions,
