fb40bcc46ba4df9fedc18b349f69e7d667ef9e2f
Shanin Roman <shanin1000@yandex.ru>
2023-09-21 08:59:36 +0300

    [fix] #3905: Fix `apply_blocks` and `validate_blocks` benchmark
    Signed-off-by: Shanin Roman <shanin1000@yandex.ru>

---
+++ core/benches/blocks/apply_blocks_oneshot.rs
@@ -0,0 +1,31 @@
+//! Oneshot execution of `apply_blocks` benchmark.
+//! Can be useful to profile using flamegraph.
+//!
+//! ```bash
+//! CARGO_PROFILE_RELEASE_DEBUG=true cargo flamegraph --root --release --example apply_blocks
+//! ```
+
+mod apply_blocks;
+
+use apply_blocks::WsvApplyBlocks;
+use iroha_config::base::proxy::Builder;
+use iroha_data_model::Level;
+use iroha_logger::{Configuration, ConfigurationProxy};
+
+fn main() {
+    let log_config = Configuration {
+        max_log_level: Level::INFO.into(),
+        compact_mode: false,
+        ..ConfigurationProxy::default()
+            .build()
+            .expect("Default logger config should always build")
+    };
+    // Can't use logger because it's failed to initialize.
+    #[allow(clippy::print_stderr)]
+    if let Err(err) = iroha_logger::init(&log_config) {
+        eprintln!("Failed to initialize logger: {err}");
+    }
+    iroha_logger::info!("Starting...");
+    let bench = WsvApplyBlocks::setup().expect("Failed to setup benchmark");
+    WsvApplyBlocks::measure(&bench).expect("Failed to execute bnechmark");
+}
