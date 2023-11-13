6f54d8348931079673ff66aaec994a81d1d125f5
Marin Veršić <marin.versic101@gmail.com>
2023-11-13 16:32:10 +0300

    [BACKPORT] #3856: Proc macro for default executor boilerplate (#4051)
    Co-authored-by: Ilia Churin <churin.ilya@gmail.com>

--- client/tests/integration/smartcontracts/executor_with_custom_token/src/lib.rs
+++ client/tests/integration/smartcontracts/executor_with_custom_token/src/lib.rs
@@ -11,7 +11,6 @@
 //! get access to control all domains. Remember that this is just a test example.
 
 #![no_std]
-#![allow(missing_docs, clippy::missing_errors_doc)]
 
 extern crate alloc;
 #[cfg(not(test))]
@@ -21,11 +20,7 @@ use alloc::{borrow::ToOwned, string::String};
 
 use anyhow::anyhow;
 use iroha_executor::{
-    data_model::evaluate::{EvaluationError, ExpressionEvaluator},
-    default::default_permission_token_schema,
-    permission::Token as _,
-    prelude::*,
-    smart_contract,
+    default::default_permission_token_schema, permission::Token as _, prelude::*, smart_contract,
 };
 use iroha_schema::IntoSchema;
 use lol_alloc::{FreeListAllocator, LockedAllocator};
@@ -59,6 +54,8 @@ mod token {
     pub struct CanControlDomainLives;
 }
 
+#[derive(Constructor, ValidateEntrypoints, ExpressionEvaluator, Validate, Visit)]
+#[visit(custom(visit_register_domain, visit_unregister_domain))]
 struct Executor {
     verdict: Result,
     block_height: u64,
@@ -66,15 +63,6 @@ struct Executor {
 }
 
 impl Executor {
-    /// Construct [`Self`]
-    pub fn new(block_height: u64) -> Self {
-        Self {
-            verdict: Ok(()),
-            block_height,
-            host: smart_contract::Host,
-        }
-    }
-
     fn get_all_accounts_with_can_unregister_domain_permission(
     ) -> Result<Vec<(Account, DomainId)>, MigrationError> {
         let accounts = FindAllAccounts.execute().map_err(|error| {
@@ -172,121 +160,33 @@ impl Executor {
     }
 }
 
-macro_rules! defaults {
-    ( $($executor:ident $(<$param:ident $(: $bound:path)?>)?($operation:ty)),+ $(,)? ) => { $(
-        fn $executor $(<$param $(: $bound)?>)?(&mut self, authority: &AccountId, operation: $operation) {
-            iroha_executor::default::$executor(self, authority, operation)
-        } )+
-    };
+fn visit_register_domain(executor: &mut Executor, authority: &AccountId, _isi: Register<Domain>) {
+    if executor.block_height() == 0 {
+        pass!(executor)
+    }
+    if token::CanControlDomainLives.is_owned_by(authority) {
+        pass!(executor);
+    }
+
+    deny!(
+        executor,
+        "You don't have permission to register a new domain"
+    );
 }
 
-impl Visit for Executor {
-    fn visit_register_domain(&mut self, authority: &AccountId, _isi: Register<Domain>) {
-        if self.block_height() == 0 {
-            pass!(self);
-        }
-        if token::CanControlDomainLives.is_owned_by(authority) {
-            pass!(self);
-        }
-
-        deny!(self, "You don't have permission to register a new domain");
+fn visit_unregister_domain(
+    executor: &mut Executor,
+    authority: &AccountId,
+    _isi: Unregister<Domain>,
+) {
+    if executor.block_height() == 0 {
+        pass!(executor);
+    }
+    if token::CanControlDomainLives.is_owned_by(authority) {
+        pass!(executor);
     }
 
-    fn visit_unregister_domain(&mut self, authority: &AccountId, _isi: Unregister<Domain>) {
-        if self.block_height() == 0 {
-            pass!(self);
-        }
-        if token::CanControlDomainLives.is_owned_by(authority) {
-            pass!(self);
-        }
-
-        deny!(self, "You don't have permission to unregister domain");
-    }
-
-    defaults! {
-        visit_unsupported<T: core::fmt::Debug>(T),
-
-        visit_transaction(&SignedTransaction),
-        visit_instruction(&InstructionExpr),
-        visit_expression<V>(&EvaluatesTo<V>),
-        visit_sequence(&SequenceExpr),
-        visit_if(&ConditionalExpr),
-        visit_pair(&PairExpr),
-
-        // Peer validation
-        visit_unregister_peer(Unregister<Peer>),
-
-        // Domain validation
-        visit_transfer_domain(Transfer<Account, DomainId, Account>),
-        visit_set_domain_key_value(SetKeyValue<Domain>),
-        visit_remove_domain_key_value(RemoveKeyValue<Domain>),
-
-        // Account validation
-        visit_unregister_account(Unregister<Account>),
-        visit_mint_account_public_key(Mint<PublicKey, Account>),
-        visit_burn_account_public_key(Burn<PublicKey, Account>),
-        visit_mint_account_signature_check_condition(Mint<SignatureCheckCondition, Account>),
-        visit_set_account_key_value(SetKeyValue<Account>),
-        visit_remove_account_key_value(RemoveKeyValue<Account>),
-
-        // Asset validation
-        visit_register_asset(Register<Asset>),
-        visit_unregister_asset(Unregister<Asset>),
-        visit_mint_asset(Mint<NumericValue, Asset>),
-        visit_burn_asset(Burn<NumericValue, Asset>),
-        visit_transfer_asset(Transfer<Asset, NumericValue, Account>),
-        visit_set_asset_key_value(SetKeyValue<Asset>),
-        visit_remove_asset_key_value(RemoveKeyValue<Asset>),
-
-        // AssetDefinition validation
-        visit_unregister_asset_definition(Unregister<AssetDefinition>),
-        visit_transfer_asset_definition(Transfer<Account, AssetDefinitionId, Account>),
-        visit_set_asset_definition_key_value(SetKeyValue<AssetDefinition>),
-        visit_remove_asset_definition_key_value(RemoveKeyValue<AssetDefinition>),
-
-        // Permission validation
-        visit_grant_account_permission(Grant<PermissionToken>),
-        visit_revoke_account_permission(Revoke<PermissionToken>),
-
-        // Role validation
-        visit_register_role(Register<Role>),
-        visit_unregister_role(Unregister<Role>),
-        visit_grant_account_role(Grant<RoleId>),
-        visit_revoke_account_role(Revoke<RoleId>),
-
-        // Trigger validation
-        visit_unregister_trigger(Unregister<Trigger<TriggeringFilterBox>>),
-        visit_mint_trigger_repetitions(Mint<u32, Trigger<TriggeringFilterBox>>),
-        visit_burn_trigger_repetitions(Burn<u32, Trigger<TriggeringFilterBox>>),
-        visit_execute_trigger(ExecuteTrigger),
-
-        // Parameter validation
-        visit_set_parameter(SetParameter),
-        visit_new_parameter(NewParameter),
-
-        // Upgrade validation
-        visit_upgrade_executor(Upgrade<iroha_executor::data_model::executor::Executor>),
-    }
-}
-
-impl Validate for Executor {
-    fn verdict(&self) -> &Result {
-        &self.verdict
-    }
-
-    fn block_height(&self) -> u64 {
-        self.block_height
-    }
-
-    fn deny(&mut self, reason: ValidationFail) {
-        self.verdict = Err(reason);
-    }
-}
-
-impl ExpressionEvaluator for Executor {
-    fn evaluate<E: Evaluate>(&self, expression: &E) -> Result<E::Value, EvaluationError> {
-        self.host.evaluate(expression)
-    }
+    deny!(executor, "You don't have permission to unregister domain");
 }
 
 #[entrypoint]
@@ -303,33 +203,4 @@ pub fn migrate(_block_height: u64) -> MigrationResult {
     );
 
     Executor::replace_token(&accounts)
-}
-
-#[entrypoint]
-pub fn validate_transaction(
-    authority: AccountId,
-    transaction: SignedTransaction,
-    block_height: u64,
-) -> Result {
-    let mut executor = Executor::new(block_height);
-    executor.visit_transaction(&authority, &transaction);
-    executor.verdict
-}
-
-#[entrypoint]
-pub fn validate_instruction(
-    authority: AccountId,
-    instruction: InstructionExpr,
-    block_height: u64,
-) -> Result {
-    let mut executor = Executor::new(block_height);
-    executor.visit_instruction(&authority, &instruction);
-    executor.verdict
-}
-
-#[entrypoint]
-pub fn validate_query(authority: AccountId, query: QueryBox, block_height: u64) -> Result {
-    let mut executor = Executor::new(block_height);
-    executor.visit_query(&authority, &query);
-    executor.verdict
 }
--- client/tests/integration/smartcontracts/executor_with_migration_fail/src/lib.rs
+++ client/tests/integration/smartcontracts/executor_with_migration_fail/src/lib.rs
@@ -1,7 +1,6 @@
 //! Runtime Executor which copies default validation logic but forbids any queries and fails to migrate.
 
 #![no_std]
-#![allow(missing_docs, clippy::missing_errors_doc)]
 
 extern crate alloc;
 #[cfg(not(test))]
@@ -10,137 +9,17 @@ extern crate panic_halt;
 use alloc::{borrow::ToOwned as _, format};
 
 use anyhow::anyhow;
-use iroha_executor::{
-    data_model::{
-        evaluate::{EvaluationError, ExpressionEvaluator},
-        ValidationFail,
-    },
-    parse,
-    prelude::*,
-    smart_contract,
-};
+use iroha_executor::{parse, prelude::*, smart_contract};
 use lol_alloc::{FreeListAllocator, LockedAllocator};
 
 #[global_allocator]
 static ALLOC: LockedAllocator<FreeListAllocator> = LockedAllocator::new(FreeListAllocator::new());
 
+#[derive(Constructor, ValidateEntrypoints, ExpressionEvaluator, Validate, Visit)]
 struct Executor {
     verdict: Result,
     block_height: u64,
     host: smart_contract::Host,
-}
-
-impl Executor {
-    /// Construct [`Self`]
-    pub fn new(block_height: u64) -> Self {
-        Self {
-            verdict: Ok(()),
-            block_height,
-            host: smart_contract::Host,
-        }
-    }
-}
-
-macro_rules! defaults {
-    ( $($executor:ident $(<$param:ident $(: $bound:path)?>)?($operation:ty)),+ $(,)? ) => { $(
-        fn $executor $(<$param $(: $bound)?>)?(&mut self, authority: &AccountId, operation: $operation) {
-            iroha_executor::default::$executor(self, authority, operation)
-        } )+
-    };
-}
-
-impl Visit for Executor {
-    fn visit_query(&mut self, _authority: &AccountId, _query: &QueryBox) {
-        self.deny(ValidationFail::NotPermitted(
-            "All queries are forbidden".to_owned(),
-        ));
-    }
-
-    defaults! {
-        visit_unsupported<T: core::fmt::Debug>(T),
-
-        visit_transaction(&SignedTransaction),
-        visit_instruction(&InstructionExpr),
-        visit_expression<V>(&EvaluatesTo<V>),
-        visit_sequence(&SequenceExpr),
-        visit_if(&ConditionalExpr),
-        visit_pair(&PairExpr),
-
-        // Peer validation
-        visit_unregister_peer(Unregister<Peer>),
-
-        // Domain validation
-        visit_unregister_domain(Unregister<Domain>),
-        visit_transfer_domain(Transfer<Account, DomainId, Account>),
-        visit_set_domain_key_value(SetKeyValue<Domain>),
-        visit_remove_domain_key_value(RemoveKeyValue<Domain>),
-
-        // Account validation
-        visit_unregister_account(Unregister<Account>),
-        visit_mint_account_public_key(Mint<PublicKey, Account>),
-        visit_burn_account_public_key(Burn<PublicKey, Account>),
-        visit_mint_account_signature_check_condition(Mint<SignatureCheckCondition, Account>),
-        visit_set_account_key_value(SetKeyValue<Account>),
-        visit_remove_account_key_value(RemoveKeyValue<Account>),
-
-        // Asset validation
-        visit_register_asset(Register<Asset>),
-        visit_unregister_asset(Unregister<Asset>),
-        visit_mint_asset(Mint<NumericValue, Asset>),
-        visit_burn_asset(Burn<NumericValue, Asset>),
-        visit_transfer_asset(Transfer<Asset, NumericValue, Account>),
-        visit_set_asset_key_value(SetKeyValue<Asset>),
-        visit_remove_asset_key_value(RemoveKeyValue<Asset>),
-
-        // AssetDefinition validation
-        visit_unregister_asset_definition(Unregister<AssetDefinition>),
-        visit_transfer_asset_definition(Transfer<Account, AssetDefinitionId, Account>),
-        visit_set_asset_definition_key_value(SetKeyValue<AssetDefinition>),
-        visit_remove_asset_definition_key_value(RemoveKeyValue<AssetDefinition>),
-
-        // Permission validation
-        visit_grant_account_permission(Grant<PermissionToken>),
-        visit_revoke_account_permission(Revoke<PermissionToken>),
-
-        // Role validation
-        visit_register_role(Register<Role>),
-        visit_unregister_role(Unregister<Role>),
-        visit_grant_account_role(Grant<RoleId>),
-        visit_revoke_account_role(Revoke<RoleId>),
-
-        // Trigger validation
-        visit_unregister_trigger(Unregister<Trigger<TriggeringFilterBox>>),
-        visit_mint_trigger_repetitions(Mint<u32, Trigger<TriggeringFilterBox>>),
-        visit_burn_trigger_repetitions(Burn<u32, Trigger<TriggeringFilterBox>>),
-        visit_execute_trigger(ExecuteTrigger),
-
-        // Parameter validation
-        visit_set_parameter(SetParameter),
-        visit_new_parameter(NewParameter),
-
-        // Upgrade validation
-        visit_upgrade_executor(Upgrade<iroha_executor::data_model::executor::Executor>),
-    }
-}
-
-impl Validate for Executor {
-    fn verdict(&self) -> &Result {
-        &self.verdict
-    }
-
-    fn block_height(&self) -> u64 {
-        self.block_height
-    }
-
-    fn deny(&mut self, reason: ValidationFail) {
-        self.verdict = Err(reason);
-    }
-}
-
-impl ExpressionEvaluator for Executor {
-    fn evaluate<E: Evaluate>(&self, expression: &E) -> Result<E::Value, EvaluationError> {
-        self.host.evaluate(expression)
-    }
 }
 
 #[entrypoint]
@@ -159,33 +38,4 @@ pub fn migrate(_block_height: u64) -> MigrationResult {
         })?;
 
     Err("This executor always fails to migrate".to_owned())
-}
-
-#[entrypoint]
-pub fn validate_transaction(
-    authority: AccountId,
-    transaction: SignedTransaction,
-    block_height: u64,
-) -> Result {
-    let mut executor = Executor::new(block_height);
-    executor.visit_transaction(&authority, &transaction);
-    executor.verdict
-}
-
-#[entrypoint]
-pub fn validate_instruction(
-    authority: AccountId,
-    instruction: InstructionExpr,
-    block_height: u64,
-) -> Result {
-    let mut executor = Executor::new(block_height);
-    executor.visit_instruction(&authority, &instruction);
-    executor.verdict
-}
-
-#[entrypoint]
-pub fn validate_query(authority: AccountId, query: QueryBox, block_height: u64) -> Result {
-    let mut executor = Executor::new(block_height);
-    executor.visit_query(&authority, &query);
-    executor.verdict
 }
