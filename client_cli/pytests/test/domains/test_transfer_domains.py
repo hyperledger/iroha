6d75fe9430f775d640a44a635f8d4a6a9e267f0b
alexstroke <111361420+astrokov7@users.noreply.github.com>
2023-11-16 18:26:31 +0400

    [fix] Fix transfer domain pytest
    Signed-off-by: alexstroke <111361420+astrokov7@users.noreply.github.com>

--- client_cli/pytests/test/domains/test_transfer_domains.py
+++ client_cli/pytests/test/domains/test_transfer_domains.py
@@ -1,30 +1,26 @@
 import allure
+import pytest
 
-from src.client_cli import client_cli, iroha
-import json
+from src.client_cli import client_cli, iroha, have
+
+@pytest.fixture(scope="function", autouse=True)
+def story_account_transfers_domain():
+    allure.dynamic.story('Account transfers a domain')
+    allure.dynamic.label('permission', 'no_permission_required')
 
 @allure.label('sdk_test_id', 'transfer_domain_successfully')
-def test_transfer_domain_successfully(
-  GIVEN_currently_authorized_account,
-  GIVEN_new_one_existing_account,
-  GIVEN_new_one_existing_domain,
+def test_transfer_domain(
+        GIVEN_currently_authorized_account,
+        GIVEN_new_one_existing_account,
+        GIVEN_new_one_existing_domain,
 ):
     with allure.step(
-            f'WHEN client_cli transfers this domain from {GIVEN_currently_authorized_account} to {GIVEN_new_one_existing_account}'):
-        client_cli.execute(f'domain transfer --from={GIVEN_currently_authorized_account} --to={GIVEN_new_one_existing_account} --id={GIVEN_new_one_existing_domain.name}')
+            f'WHEN {GIVEN_currently_authorized_account} transfers domains '
+            f'to {GIVEN_new_one_existing_account}'):
+        client_cli.execute(f'domain transfer '
+                           f'--from={GIVEN_currently_authorized_account} '
+                           f'--to={GIVEN_new_one_existing_account} '
+                           f'--id={GIVEN_new_one_existing_domain.name}')
     with allure.step(
-            f'THEN {GIVEN_new_one_existing_account} should own this domain {GIVEN_new_one_existing_domain}'):
-        def condition():
-            owned_by = GIVEN_new_one_existing_account
-            domain_name = GIVEN_new_one_existing_domain.name
-            with allure.step(
-                    f'WHEN client_cli query domains filtered by name "{domain_name}"'):
-                domains = iroha.list_filter(f'{{"Identifiable": {{"Is": "{domain_name}"}}}}').domains()
-            with allure.step(
-                    f'THEN Iroha should return only return domains with "{domain_name}" name'):
-                allure.attach(
-                    json.dumps(domains),
-                    name='domains',
-                    attachment_type=allure.attachment_type.JSON)
-                return len(domains) != 0 and all(domains[key]["id"] == domain_name and domains[key]["owned_by"] == owned_by for key in domains)
-        client_cli.wait_for(condition)
+            f'THEN {GIVEN_new_one_existing_account} should own {GIVEN_new_one_existing_domain}'):
+        iroha.should(have.domain(GIVEN_new_one_existing_domain.name, owned_by=GIVEN_new_one_existing_account))
