aa97d39595e770c111944fca4f4a5697c0a58889
Dmitry Balashov <43530070+0x009922@users.noreply.github.com>
2023-12-05 08:04:48 +0700

    [test]: fix config endpoints test
    Signed-off-by: Dmitry Balashov <43530070+0x009922@users.noreply.github.com>

--- client/tests/integration/config.rs
+++ client/tests/integration/config.rs
@@ -1,24 +1,24 @@
 use iroha_data_model::Level;
 use test_network::*;
 
-#[tokio::test]
-async fn config_endpoints() {
+#[test]
+fn config_endpoints() {
     const NEW_LOG_LEVEL: Level = Level::ERROR;
 
-    // The underscored variables must not be dropped until end of closure.
-    let (_dont_drop, peer, test_client) =
-        <PeerBuilder>::new().with_port(10_685).start_with_runtime();
+    let (rt, peer, test_client) = <PeerBuilder>::new().with_port(10_685).start_with_runtime();
     wait_for_genesis_committed(&vec![test_client.clone()], 0);
-    let init_log_level = peer
-        .iroha
-        .as_ref()
-        .unwrap()
-        .kiso
-        .get_dto()
-        .await
-        .unwrap()
-        .logger
-        .level;
+
+    let init_log_level = rt.block_on(async move {
+        peer.iroha
+            .as_ref()
+            .unwrap()
+            .kiso
+            .get_dto()
+            .await
+            .unwrap()
+            .logger
+            .level
+    });
 
     // Just to be sure this test suite is not useless
     assert_ne!(init_log_level, NEW_LOG_LEVEL);
