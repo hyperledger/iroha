5d4082ffe3c20d1a871b26f76eb50569be53659b
Vladislav <amuzik95@gmail.com>
2024-01-15 09:23:27 +0300

    [refactor]: update iroha_telemetry_derive to use syn2 (#4168)
    Signed-off-by: VAmuzing <amuzik95@gmail.com>

--- telemetry/derive/tests/ui_fail/not_return_result.rs
+++ telemetry/derive/tests/ui_fail/not_return_result.rs
@@ -1,8 +1,11 @@
 use iroha_telemetry_derive::metrics;
+use iroha_core::prelude::WorldStateView;
+
+type MyNotResult = Option<i32>;
 
 #[metrics(+"test_query", "another_test_query_without_timing")]
-fn execute(_wsv: &WorldStateView) -> iroha_core::RESULT {
-    Ok(())
+fn execute(_wsv: &WorldStateView) -> MyNotResult {
+    None
 }
 
 fn main() {
--- telemetry/derive/tests/ui_fail/return_nothing.rs
+++ telemetry/derive/tests/ui_fail/return_nothing.rs
@@ -1,8 +1,9 @@
 use iroha_telemetry_derive::metrics;
+use iroha_core::prelude::WorldStateView;
 
 #[metrics(+"test_query", "another_test_query_without_timing")]
 fn execute(wsv: &WorldStateView) {
-    Ok(())
+    Ok::<(), ()>(());
 }
 
 fn main() {
