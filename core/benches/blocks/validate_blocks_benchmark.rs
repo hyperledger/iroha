260b6482f2faff2c531a9cf15f25786d63c4538a
Shanin Roman <shanin1000@yandex.ru>
2024-01-18 16:07:44 +0300

    [fix] #4211: Fix call blocking methods within async context
    Signed-off-by: Shanin Roman <shanin1000@yandex.ru>

--- core/benches/blocks/validate_blocks_benchmark.rs
+++ core/benches/blocks/validate_blocks_benchmark.rs
@@ -6,7 +6,11 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use validate_blocks::WsvValidateBlocks;
 
 fn validate_blocks(c: &mut Criterion) {
-    let bench = WsvValidateBlocks::setup().expect("Failed to setup benchmark");
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Failed building the Runtime");
+    let bench = WsvValidateBlocks::setup(rt.handle()).expect("Failed to setup benchmark");
 
     let mut group = c.benchmark_group("validate_blocks");
     group.significance_level(0.1).sample_size(10);
