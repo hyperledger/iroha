2b072ba37d7bac41f9c085421ccf78d43984e088
timofeevmd <47450344+timofeevmd@users.noreply.github.com>
2024-02-05 22:31:00 +0400

    Update client/tests/integration/tx_chain_id.rs
    Co-authored-by: Daniil Polyakov <arjentix@gmail.com>
    Signed-off-by: timofeevmd <timofeevmd@gmail.com>

--- client/tests/integration/tx_chain_id.rs
+++ client/tests/integration/tx_chain_id.rs
@@ -4,6 +4,8 @@ use iroha_crypto::KeyPair;
 use iroha_data_model::prelude::*;
 use test_network::*;
 
+use super::asset::asset_id_new;
+
 #[test]
 fn send_tx_with_different_chain_id() {
     let (_rt, _peer, test_client) = <PeerBuilder>::new().with_port(11_240).start_with_runtime();
@@ -18,7 +20,7 @@ fn send_tx_with_different_chain_id() {
         sender_account_id.clone(),
         [sender_keypair.public_key().clone()],
     ));
-    let create_reciver_account = Register::account(Account::new(reciver_account_id.clone(), []));
+    let create_receiver_account = Register::account(Account::new(receiver_account_id.clone(), []));
     let create_asset =
         Register::asset_definition(AssetDefinition::quantity(asset_definition_id.clone()));
     let register_asset: InstructionBox = Register::asset(Asset::new(
@@ -31,8 +33,8 @@ fn send_tx_with_different_chain_id() {
         .submit_blocking(create_sender_accout)
         .expect("Failed to create sender account.");
     test_client
-        .submit_blocking(create_reciver_account)
-        .expect("Failed to create reciver account.");
+        .submit_blocking(create_receiver_account)
+        .expect("Failed to create receiver account.");
     test_client
         .submit_blocking(create_asset)
         .expect("Failed to create asset");
@@ -42,33 +44,23 @@ fn send_tx_with_different_chain_id() {
     let chain_id_0 = ChainId::new("0");
     let chain_id_1 = ChainId::new("1");
 
-    let trasfer_instruction = Transfer::asset_quantity(
+    let transfer_instruction = Transfer::asset_quantity(
         asset_id_new("test_asset", "wonderland", sender_account_id.clone()),
         to_transfer,
-        reciver_account_id.clone(),
+        receiver_account_id.clone(),
     );
     let asset_transfer_tx_0 = TransactionBuilder::new(chain_id_0, sender_account_id.clone())
-        .with_instructions([trasfer_instruction.clone()])
+        .with_instructions([transfer_instruction.clone()])
         .sign(sender_keypair.clone())
         .expect("Failed to sign sender transaction, chainId = 0");
     let asset_transfer_tx_1 = TransactionBuilder::new(chain_id_1, sender_account_id.clone())
-        .with_instructions([trasfer_instruction])
+        .with_instructions([transfer_instruction])
         .sign(sender_keypair)
         .expect("Failed to sign sender transaction, chainId = 1");
     test_client
         .submit_transaction_blocking(&asset_transfer_tx_0)
         .unwrap();
-    test_client
+    let _err = test_client
         .submit_transaction_blocking(&asset_transfer_tx_1)
         .unwrap_err();
-}
-
-fn asset_id_new(definition_name: &str, definition_domain: &str, account_id: AccountId) -> AssetId {
-    AssetId::new(
-        AssetDefinitionId::new(
-            definition_domain.parse().expect("Valid"),
-            definition_name.parse().expect("Valid"),
-        ),
-        account_id,
-    )
 }
