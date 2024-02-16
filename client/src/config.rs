04ee42fd57c103268b72277335d2ad5348b20ce5
Dmitry Balashov <43530070+0x009922@users.noreply.github.com>
2024-02-16 11:07:17 +0900

    [refactor]: update client configs, cover full in tests
    Signed-off-by: Dmitry Balashov <43530070+0x009922@users.noreply.github.com>

--- client/src/config.rs
+++ client/src/config.rs
@@ -97,4 +97,26 @@ mod tests {
     fn web_login_bad() {
         let _err = WebLogin::from_str("alice:wonderland").expect_err("input has `:`");
     }
+
+    #[test]
+    fn parse_full_toml_config() {
+        let _: RootPartial = toml::toml! {
+            chain_id = "00000000-0000-0000-0000-000000000000"
+            torii_url = "http://127.0.0.1:8080/"
+
+            [basic_auth]
+            web_login = "mad_hatter"
+            password = "ilovetea"
+
+            [account]
+            id = "alice@wonderland"
+            public_key = "ed01207233BFC89DCBD68C19FDE6CE6158225298EC1131B6A130D1AEB454C1AB5183C0"
+            private_key = { digest_function = "ed25519", payload = "9ac47abf59b356e0bd7dcbbbb4dec080e302156a48ca907e47cb6aea1d32719e7233bfc89dcbd68c19fde6ce6158225298ec1131b6a130d1aeb454c1ab5183c0" }
+
+            [transaction]
+            time_to_live = 100_000
+            status_timeout = 100_000
+            nonce = false
+        }.try_into().unwrap();
+    }
 }
