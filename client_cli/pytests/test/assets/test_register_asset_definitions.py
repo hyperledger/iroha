1304cfc084d0addc43cd1d8ea476bdf0de48e558
Shanin Roman <shanin1000@yandex.ru>
2024-02-21 14:39:16 +0300

    [refactor] #3660: Adjust pytests
    Signed-off-by: Shanin Roman <shanin1000@yandex.ru>

--- client_cli/pytests/test/assets/test_register_asset_definitions.py
+++ client_cli/pytests/test/assets/test_register_asset_definitions.py
@@ -11,19 +11,19 @@ def story_account_registers_asset_definitions():
     allure.dynamic.label("permission", "no_permission_required")
 
 
-@allure.label("sdk_test_id", "register_asset_definition_with_quantity_value_type")
-def test_register_asset_definition_with_quantity_value_type(
-    GIVEN_fake_asset_name, GIVEN_registered_domain, GIVEN_quantity_value_type
+@allure.label("sdk_test_id", "register_asset_definition_with_numeric_value_type")
+def test_register_asset_definition_with_numeric_value_type(
+    GIVEN_fake_asset_name, GIVEN_registered_domain, GIVEN_numeric_value_type
 ):
     with allure.step(
         f'WHEN client_cli registers the asset_definition "{GIVEN_fake_asset_name}" '
-        f'with "{GIVEN_quantity_value_type}" value type'
+        f'with "{GIVEN_numeric_value_type}" value type'
         f'in the "{GIVEN_registered_domain.name}" domain'
     ):
         client_cli.register().asset().definition(
             asset=GIVEN_fake_asset_name,
             domain=GIVEN_registered_domain.name,
-            value_type=GIVEN_quantity_value_type,
+            value_type=GIVEN_numeric_value_type,
         )
     with allure.step(f'THEN Iroha should have the asset "{GIVEN_fake_asset_name}"'):
         iroha.should(
@@ -35,17 +35,17 @@ def test_register_asset_definition_with_quantity_value_type(
 
 @allure.label("sdk_test_id", "register_asset_definition_with_too_long_name")
 def test_register_asset_definition_with_too_long_name(
-    GIVEN_129_length_name, GIVEN_registered_domain, GIVEN_quantity_value_type
+    GIVEN_129_length_name, GIVEN_registered_domain, GIVEN_numeric_value_type
 ):
     with allure.step(
         f'WHEN client_cli registers the asset_definition "{GIVEN_129_length_name}" '
-        f'with "{GIVEN_quantity_value_type}" value type'
+        f'with "{GIVEN_numeric_value_type}" value type'
         f'in the "{GIVEN_registered_domain.name}" domain'
     ):
         client_cli.register().asset().definition(
             asset=GIVEN_129_length_name,
             domain=GIVEN_registered_domain.name,
-            value_type=GIVEN_quantity_value_type,
+            value_type=GIVEN_numeric_value_type,
         )
     with allure.step(f'THEN Iroha should have the asset "{GIVEN_129_length_name}"'):
         client_cli.should(have.error(Stderr.TOO_LONG.value))
@@ -73,28 +73,6 @@ def test_register_asset_definition_with_store_value_type(
         )
 
 
-@allure.label("sdk_test_id", "register_asset_definition_with_big_quantity_type")
-def test_register_asset_definition_with_big_quantity_type(
-    GIVEN_fake_asset_name, GIVEN_registered_domain, GIVEN_big_quantity_value_type
-):
-    with allure.step(
-        f'WHEN client_cli registers the asset_definition "{GIVEN_fake_asset_name}" '
-        f'with "{GIVEN_big_quantity_value_type}" value type'
-        f'in the "{GIVEN_registered_domain.name}" domain'
-    ):
-        client_cli.register().asset().definition(
-            asset=GIVEN_fake_asset_name,
-            domain=GIVEN_registered_domain.name,
-            value_type=GIVEN_big_quantity_value_type,
-        )
-    with allure.step(f'THEN Iroha should have the asset "{GIVEN_fake_asset_name}"'):
-        iroha.should(
-            have.asset_definition(
-                GIVEN_fake_asset_name + "#" + GIVEN_registered_domain.name
-            )
-        )
-
-
 @allure.label("sdk_test_id", "register_asset_definition_with_metadata")
 @pytest.mark.xfail(reason="TO DO")
 def test_register_asset_definition_with_metadata(
@@ -113,21 +91,21 @@ def test_register_fixed_asset_definition(
 
 @allure.label("sdk_test_id", "register_asset_with_existing_name")
 def test_register_asset_with_existing_name(
-    GIVEN_registered_asset_definition_with_quantity_value_type,
+    GIVEN_registered_asset_definition_with_numeric_value_type,
 ):
     with allure.step(
         f"WHEN account tries to register an asset definition "
-        f'with the same name "{GIVEN_registered_asset_definition_with_quantity_value_type.name}"'
-        f'in the "{GIVEN_registered_asset_definition_with_quantity_value_type.domain}" domain'
+        f'with the same name "{GIVEN_registered_asset_definition_with_numeric_value_type.name}"'
+        f'in the "{GIVEN_registered_asset_definition_with_numeric_value_type.domain}" domain'
     ):
         client_cli.register().asset().definition(
-            asset=GIVEN_registered_asset_definition_with_quantity_value_type.name,
-            domain=GIVEN_registered_asset_definition_with_quantity_value_type.domain,
-            value_type=GIVEN_registered_asset_definition_with_quantity_value_type.value_type,
+            asset=GIVEN_registered_asset_definition_with_numeric_value_type.name,
+            domain=GIVEN_registered_asset_definition_with_numeric_value_type.domain,
+            value_type=GIVEN_registered_asset_definition_with_numeric_value_type.value_type,
         )
     with allure.step(
         f'THEN client_cli should have the asset definition error: "'
-        f'{GIVEN_registered_asset_definition_with_quantity_value_type.__repr__()}"'
+        f'{GIVEN_registered_asset_definition_with_numeric_value_type.__repr__()}"'
     ):
         client_cli.should(have.error(Stderr.REPETITION.value))
 
@@ -139,7 +117,7 @@ def test_register_asset_with_empty_name(GIVEN_registered_domain):
         f'in the "{GIVEN_registered_domain.name}" domain'
     ):
         client_cli.register().asset().definition(
-            asset="", domain=GIVEN_registered_domain.name, value_type="Quantity"
+            asset="", domain=GIVEN_registered_domain.name, value_type="Numeric"
         )
     with allure.step(
         f'THEN сlient_cli should have the asset error: "{Stderr.CANNOT_BE_EMPTY}"'
@@ -149,7 +127,7 @@ def test_register_asset_with_empty_name(GIVEN_registered_domain):
 
 @allure.label("sdk_test_id", "register_asset_with_not_existing_domain")
 def test_register_asset_with_not_existing_domain(
-    GIVEN_not_existing_name, GIVEN_quantity_value_type, GIVEN_fake_asset_name
+    GIVEN_not_existing_name, GIVEN_numeric_value_type, GIVEN_fake_asset_name
 ):
     with allure.step(
         "WHEN client_cli tries to register an asset definition with not existing domain"
@@ -157,7 +135,7 @@ def test_register_asset_with_not_existing_domain(
         client_cli.register().asset().definition(
             asset=GIVEN_fake_asset_name,
             domain=GIVEN_not_existing_name,
-            value_type=GIVEN_quantity_value_type,
+            value_type=GIVEN_numeric_value_type,
         )
     with allure.step("THEN client_cli should have the error"):
         client_cli.should(have.error(Stderr.FAILED_TO_FIND_DOMAIN.value))
