0a936c3d455d411fd09c8786f85c52f7fa818e73
Dmitry Murzin <diralik@yandex.ru>
2024-05-01 12:46:48 +0300

    [fix] #4331: Revoke removed `PermissionToken`s on `Upgrade<Executor>` (#4503)
    Signed-off-by: Dmitry Murzin <diralik@yandex.ru>

---
+++ client/tests/integration/smartcontracts/executor_remove_token/src/lib.rs
@@ -0,0 +1,37 @@
+//! Runtime Executor which removes [`token::CanControlDomainLives`] permission token.
+//! Needed for tests.
+
+#![no_std]
+
+extern crate alloc;
+#[cfg(not(test))]
+extern crate panic_halt;
+
+use iroha_executor::{default::default_permission_token_schema, prelude::*};
+use lol_alloc::{FreeListAllocator, LockedAllocator};
+
+#[global_allocator]
+static ALLOC: LockedAllocator<FreeListAllocator> = LockedAllocator::new(FreeListAllocator::new());
+
+getrandom::register_custom_getrandom!(iroha_executor::stub_getrandom);
+
+#[derive(Constructor, ValidateEntrypoints, Validate, Visit)]
+struct Executor {
+    verdict: Result,
+    block_height: u64,
+}
+
+#[entrypoint]
+pub fn migrate(_block_height: u64) -> MigrationResult {
+    // Note that actually migration will reset token schema to default (minus `CanUnregisterDomain`)
+    // So any added custom permission tokens will be also removed
+    let mut schema = default_permission_token_schema();
+    schema.remove::<iroha_executor::default::tokens::domain::CanUnregisterDomain>();
+
+    let (token_ids, schema_str) = schema.serialize();
+    iroha_executor::set_permission_token_schema(
+        &iroha_executor::data_model::permission::PermissionTokenSchema::new(token_ids, schema_str),
+    );
+
+    Ok(())
+}
