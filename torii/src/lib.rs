09d288fa1434a178e795d34c6511073bf64a0e2b
0x009922 <43530070+0x009922@users.noreply.github.com>
2024-06-12 14:45:37 +0900

    refactor: simplify supervisor implementation
    Signed-off-by: 0x009922 <43530070+0x009922@users.noreply.github.com>

--- torii/src/lib.rs
+++ torii/src/lib.rs
@@ -49,7 +49,6 @@ pub struct Torii {
     kiso: KisoHandle,
     queue: Arc<Queue>,
     events: EventsSender,
-    // shutdown_cancel_token: CancellationToken,
     query_service: LiveQueryStoreHandle,
     kura: Arc<Kura>,
     transaction_max_content_len: Bytes<u64>,
