95eab6d6d4c420340da608effda255920fa0f2c7
Nurzhan Saken <nurzhan.sakenov@gmail.com>
2024-06-25 11:04:58 +0400

    feat(swarm): reimplement, optimize compose config and its generation (#4719)
    * feat(swarm): reimplement, optimize compose config and its generation
    * feat(swarm): include args in banner, add option to print config to stdout
    * test(swarm): add tests, fix errors
    * docs(swarm): add note about config structure
    * fix(swarm): fix peer startup, don't use builder on pull, add local image to CI
    * refactor(swarm): clean up schema types
    * build(swarm): pass `GENESIS_PRIVATE_KEY` env var to containers
    * feat(swarm): update leader command, include full topology in leader's env
    ---------
    Signed-off-by: Nurzhan Sakén <nurzhan.sakenov@gmail.com>

---
+++ tools/swarm/src/peer.rs
@@ -0,0 +1,65 @@
+//! Peer utils.
+
+pub type PeerInfo = (PeerName, P2pApiPorts, ExposedKeyPair);
+pub type PeerName = String;
+pub type P2pApiPorts = [u16; 2];
+pub type ExposedKeyPair = (iroha_crypto::PublicKey, iroha_crypto::ExposedPrivateKey);
+pub type ExposedKeyRefPair<'a> = (
+    &'a iroha_crypto::PublicKey,
+    &'a iroha_crypto::ExposedPrivateKey,
+);
+
+pub const SERVICE_NAME: &str = "irohad";
+
+pub fn generate_key_pair(base_seed: Option<&[u8]>, extra_seed: &[u8]) -> ExposedKeyPair {
+    let (public_key, private_key) = base_seed
+        .map_or_else(iroha_crypto::KeyPair::random, |seed| {
+            iroha_crypto::KeyPair::from_seed(
+                seed.iter().chain(extra_seed).copied().collect::<Vec<_>>(),
+                iroha_crypto::Algorithm::default(),
+            )
+        })
+        .into_parts();
+    (public_key, iroha_crypto::ExposedPrivateKey(private_key))
+}
+
+pub fn network(count: u16, key_seed: Option<&[u8]>) -> std::collections::BTreeMap<u16, PeerInfo> {
+    (0..count)
+        .map(|nth| {
+            let name = format!("{SERVICE_NAME}{nth}");
+            let ports = [super::BASE_PORT_P2P + nth, super::BASE_PORT_API + nth];
+            let key_pair = generate_key_pair(key_seed, &nth.to_be_bytes());
+            (nth, (name, ports, key_pair))
+        })
+        .collect()
+}
+
+pub fn chain() -> iroha_data_model::ChainId {
+    iroha_data_model::ChainId::from(crate::CHAIN_ID)
+}
+
+pub fn peer_id(
+    name: &str,
+    port: u16,
+    public_key: iroha_crypto::PublicKey,
+) -> iroha_data_model::peer::PeerId {
+    iroha_data_model::peer::PeerId::new(
+        iroha_primitives::addr::SocketAddrHost {
+            host: name.to_owned().into(),
+            port,
+        }
+        .into(),
+        public_key,
+    )
+}
+
+#[allow(single_use_lifetimes)]
+pub fn topology<'a>(
+    peers: impl Iterator<Item = &'a PeerInfo>,
+) -> std::collections::BTreeSet<iroha_data_model::peer::PeerId> {
+    peers
+        .map(|(service_name, [port_p2p, _], (public_key, _))| {
+            peer_id(service_name, *port_p2p, public_key.clone())
+        })
+        .collect()
+}
