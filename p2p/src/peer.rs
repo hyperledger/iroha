7a8ba1bdbcd070647ffec78d09236b19aae0adda
Shanin Roman <40040452+Erigara@users.noreply.github.com>
2024-09-05 11:56:47 +0300

    refacrot(p2p): add clarification comment for `message_sender.send()`
    Co-authored-by: ⭐️NINIKA⭐️ <DCNick3@users.noreply.github.com>
    Signed-off-by: Shanin Roman <40040452+Erigara@users.noreply.github.com>

--- p2p/src/peer.rs
+++ p2p/src/peer.rs
@@ -261,6 +261,11 @@ mod run {
                         idle_interval.reset();
                         ping_interval.reset();
                     }
+                    // `message_sender.send()` is safe to be cancelled, it won't advance the queue or write anything if another branch completes first.
+                    // 
+                    // We need to conditionally disable it in case there is no data is to be sent, otherwise `message_sender.send()` will complete immediately
+                    //
+                    // The only source of data to be sent is other branches of this loop, so we do not need any async waiting mechanism for waiting for readiness.
                     result = message_sender.send(), if message_sender.ready() => {
                         if let Err(error) = result {
                             iroha_logger::error!(%error, "Failed to send message to peer.");
