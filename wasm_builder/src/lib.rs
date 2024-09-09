448344e5775c9fb8b38c1464a28a5e3c7c5cfbc2
BAStos525 <jungle.vas@yandex.ru>
2024-09-09 16:55:40 +0300

    Update wasm_builder to nightly-2024-09-09
    Signed-off-by: BAStos525 <jungle.vas@yandex.ru>

--- wasm_builder/src/lib.rs
+++ wasm_builder/src/lib.rs
@@ -14,7 +14,7 @@ use eyre::{bail, eyre, Context as _, Result};
 use path_absolutize::Absolutize;
 
 /// Current toolchain used to build smartcontracts
-const TOOLCHAIN: &str = "+nightly-2024-04-18";
+const TOOLCHAIN: &str = "+nightly-2024-09-09";
 
 /// WASM Builder for smartcontracts (e.g. triggers and executors).
 ///
