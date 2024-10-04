8bb3b190916a27085814c5721b123e76a28758e7
0x009922 <43530070+0x009922@users.noreply.github.com>
2024-10-04 16:48:06 +0900

    build: select build targets by `cdylib` target
    Signed-off-by: 0x009922 <43530070+0x009922@users.noreply.github.com>

--- scripts/build_wasm_samples.sh
+++ scripts/build_wasm_samples.sh
@@ -3,10 +3,9 @@ set -e;
 SAMPLES_DIR="wasm_samples"
 TARGET_DIR="$SAMPLES_DIR/target/prebuilt"
 mkdir -p "$TARGET_DIR"
-# FIXME: `executor_custom_data_model` doesn't compile: https://github.com/hyperledger/iroha/issues/5114
 for dir in $(
   cargo metadata --no-deps --manifest-path ./wasm_samples/Cargo.toml --format-version=1 |
-  jq -r '.packages[].manifest_path | rtrimstr("/Cargo.toml") | split("/") | last | select(. != "executor_custom_data_model")'
+  jq '.packages | map(select(.targets[].kind | contains(["cdylib"]))) | map(.manifest_path | split("/") | .[-2]) | .[]' -r
 ); do
   out_file="$TARGET_DIR/$dir.wasm"
   cargo run --bin iroha_wasm_builder -- build "$SAMPLES_DIR/$dir" --optimize --out-file "$out_file"
