5e45adea6250bee70ce1f063704a28192bd70b2e
0x009922 <43530070+0x009922@users.noreply.github.com>
2024-10-08 05:47:25 +0000

    refactor(iroha_torii): spawn a single server task with only one `TcpListener` (#5122)
    Signed-off-by: 0x009922 <43530070+0x009922@users.noreply.github.com>

--- crates/irohad/src/lib.rs
+++ crates/irohad/src/lib.rs
@@ -10,6 +10,7 @@ use std::{
     future::Future,
     path::{Path, PathBuf},
     sync::Arc,
+    time::Duration,
 };
 
 use clap::Parser;
@@ -34,7 +35,7 @@ use iroha_core::{
     IrohaNetwork,
 };
 use iroha_data_model::{block::SignedBlock, prelude::*};
-use iroha_futures::supervisor::{ShutdownSignal, Supervisor};
+use iroha_futures::supervisor::{Child, OnShutdown, ShutdownSignal, Supervisor};
 use iroha_genesis::GenesisBlock;
 use iroha_logger::{actor::LoggerHandle, InitConfig as LoggerInitConfig};
 use iroha_primitives::addr::SocketAddr;
@@ -323,10 +324,18 @@ impl Iroha {
             #[cfg(feature = "telemetry")]
             metrics_reporter,
         )
-        .start(&supervisor.shutdown_signal())
-        .await
-        .map_err(|report| report.change_context(StartError::StartTorii))?;
-        supervisor.monitor(tokio::spawn(torii_run));
+        .start(supervisor.shutdown_signal());
+        supervisor.monitor(Child::new(
+            tokio::spawn(async move {
+                if let Err(err) = torii_run.await {
+                    iroha_logger::error!(?err, "Torii failed to terminate gracefully");
+                    // TODO: produce non-zero exit code or something
+                } else {
+                    iroha_logger::debug!("Torii exited normally");
+                };
+            }),
+            OnShutdown::Wait(Duration::from_secs(5)),
+        ));
 
         supervisor.monitor(tokio::task::spawn(config_updates_relay(kiso, logger)));
 
