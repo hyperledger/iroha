435387c3b02d7fe0213cde2db88e874f7c7e2929
0x009922 <43530070+0x009922@users.noreply.github.com>
2024-10-18 09:35:06 +0900

    chore: use `expect`
    Co-authored-by: Marin Veršić <marin.versic101@gmail.com>
    Signed-off-by: 0x009922 <43530070+0x009922@users.noreply.github.com>

--- crates/iroha_torii/src/utils.rs
+++ crates/iroha_torii/src/utils.rs
@@ -64,7 +64,7 @@ pub mod extractors {
     }
 
     /// Extractor of Accept header
-    #[cfg_attr(not(feature = "telemetry"), allow(unused))]
+    #[cfg_attr(not(feature = "telemetry"), expect(unused))]
     pub struct ExtractAccept(pub HeaderValue);
 
     #[async_trait]
