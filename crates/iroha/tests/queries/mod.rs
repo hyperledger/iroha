f03ebe14da117a5ec4f08b1af9b793e130fc3a44
Dmitry Murzin <diralik@yandex.ru>
2024-10-23 22:58:12 +0300

    feat: Sort descending in `FindTransactions` query
    Signed-off-by: Dmitry Murzin <diralik@yandex.ru>

--- crates/iroha/tests/queries/mod.rs
+++ crates/iroha/tests/queries/mod.rs
@@ -31,3 +31,44 @@ fn too_big_fetch_size_is_not_allowed() {
         ))
     ));
 }
+
+#[test]
+fn find_blocks_reversed() -> eyre::Result<()> {
+    let (network, _rt) = NetworkBuilder::new().start_blocking()?;
+    let client = network.client();
+
+    client.submit_blocking(Register::domain(Domain::new("domain1".parse()?)))?;
+
+    let blocks = client.query(FindBlocks).execute_all()?;
+    assert_eq!(blocks.len(), 2);
+    assert_eq!(blocks[1].header().prev_block_hash, None);
+    assert_eq!(
+        blocks[0].header().prev_block_hash,
+        Some(blocks[1].header().hash())
+    );
+
+    Ok(())
+}
+
+#[test]
+fn find_transactions_reversed() -> eyre::Result<()> {
+    let (network, _rt) = NetworkBuilder::new().start_blocking()?;
+    let client = network.client();
+
+    let register_domain = Register::domain(Domain::new("domain1".parse()?));
+    client.submit_blocking(register_domain.clone())?;
+
+    let txs = client.query(FindTransactions).execute_all()?;
+
+    // check that latest transaction is register domain
+    let Executable::Instructions(instructions) = txs[0].transaction.value.instructions() else {
+        panic!("Expected instructions");
+    };
+    assert_eq!(instructions.len(), 1);
+    assert_eq!(
+        instructions[0],
+        InstructionBox::Register(register_domain.into())
+    );
+
+    Ok(())
+}
